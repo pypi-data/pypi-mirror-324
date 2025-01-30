803f1bdaf4c19854caf8e15680a82d00b452b586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 16:05:10 +0000

    Add package pymagba/0.1.0/pymagba-0.1.0-cp312-cp312-macosx_10_12_x86_64.whl

---
+++ packages/pymagba/pymagba-0.1.0-cp312-cp312-macosx_10_12_x86_64.whl/pymagba/sources/__init__.py
@@ -0,0 +1,4 @@
+# PyMagba is licensed under The 3-Clause BSD, see LICENSE.
+# Copyright 2025 Sira Pornsiriprasert <code@psira.me>
+
+from ._sources import CylinderMagnet, SourceCollection, SourceType
