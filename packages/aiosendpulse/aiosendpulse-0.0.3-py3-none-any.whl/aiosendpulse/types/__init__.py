b7caa119c6a1f35237ff66aa839e124b505299ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 16:04:48 +0000

    Add package aiosendpulse/0.0.3/aiosendpulse-0.0.3-py3-none-any.whl

---
+++ packages/aiosendpulse/aiosendpulse-0.0.3-py3-none-any.whl/aiosendpulse/types/__init__.py
@@ -0,0 +1,28 @@
+from .addressbook import Addressbook, AddressbookId
+from .addressbook_variable import AddressbookVariable
+from .email import EmailDetail
+from .recipient import Recipient
+from .result import CreateTemplateResult, Result
+from .sender import Sender
+from .template import Template, TemplateMeta
+from .template_category import TemplateCategory
+from .token import Token
+
+
+Template.model_rebuild()
+
+
+__all__ = [
+    "Addressbook",
+    "AddressbookId",
+    "AddressbookVariable",
+    "EmailDetail",
+    "Recipient",
+    "CreateTemplateResult",
+    "Result",
+    "Sender",
+    "Template",
+    "TemplateMeta",
+    "TemplateCategory",
+    "Token",
+]
