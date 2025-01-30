03c61d9c8601b1f35f216e811116a304127aa261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 04:05:04 +0000

    Add package compas-invocations2/0.3.2/compas_invocations2-0.3.2-py3-none-any.whl

---
+++ packages/compas-invocations2/compas_invocations2-0.3.2-py3-none-any.whl/compas_invocations2/style.py
@@ -0,0 +1,37 @@
+import invoke
+
+from compas_invocations2.console import chdir
+
+
+@invoke.task()
+def lint(ctx):
+    """Check the consistency of coding style."""
+
+    print("\nRunning ruff linter...")
+    ctx.run("ruff check --fix src tests")
+
+    print("\nRunning black linter...")
+    ctx.run("black --check --diff --color src tests")
+
+    print("\nAll linting is done!")
+
+
+@invoke.task()
+def format(ctx):
+    """Reformat the code base using black."""
+
+    print("\nRunning ruff formatter...")
+    ctx.run("ruff format src tests")
+
+    print("\nRunning black formatter...")
+    ctx.run("black src tests")
+
+    print("\nAll formatting is done!")
+
+
+@invoke.task()
+def check(ctx):
+    """Check the consistency of documentation, coding style and a few other things."""
+
+    with chdir(ctx.base_folder):
+        lint(ctx)
