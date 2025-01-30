c5e7939ae445344da9da10a2d3c310b649564f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:33 +0000

    Add package docketanalyzer/0.3.18/docketanalyzer-0.3.18-py3-none-any.whl

---
+++ packages/docketanalyzer/docketanalyzer-0.3.18-py3-none-any.whl/docketanalyzer/__init__.py
@@ -0,0 +1,15 @@
+from docketanalyzer._version import __version__
+from docketanalyzer.utils import *
+from docketanalyzer.env import env
+env.add_to_namespace(globals())
+from docketanalyzer.core import *
+from docketanalyzer.agents import *
+from docketanalyzer.agents import *
+from docketanalyzer.docket import *
+from docketanalyzer.tasks import *
+from docketanalyzer.cli import cli
+
+
+if dev_available():
+    from dev import patch
+    patch(globals())
