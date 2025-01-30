233b5b1cbb38d2ec457744b7da067eb0270adce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:24 +0000

    Add package dataset-format-benchmark/0.2.0/dataset_format_benchmark-0.2.0.tar.gz

---
+++ packages/dataset-format-benchmark/dataset_format_benchmark-0.2.0.tar.gz/dataset_format_benchmark-0.2.0/src/dataset_format_benchmark/datasets/__init__.py
@@ -0,0 +1,69 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Iterator, Generator, Sequence
+
+import torch
+import torch.utils.data
+
+from dataset_format_benchmark.storages import ImageFileStorage
+
+USE_CUDA = torch.cuda.is_available()
+
+
+class BaseDataset(torch.utils.data.Dataset, ABC):
+    DATASET_DIR_NAME = None
+    METADATA_FILE_NAME = None
+
+    def __init__(self, root: Path, transform=None, target_transform=None):
+        super().__init__()
+        self.device = torch.device('cuda' if USE_CUDA else 'cpu')
+        self.data_length = 0
+        self.dataset_root_path = Path(root, self.DATASET_DIR_NAME)
+        self.metadata_file_path = Path(self.dataset_root_path, 'metadata_fs.json')
+        self.filenames: Sequence[str] = []
+        self.storages: list[ImageFileStorage] = []
+
+    def add_storage(self, storage: ImageFileStorage):
+        self.storages.append(storage)
+
+    def get_storages(self) -> list[ImageFileStorage]:
+        return self.storages
+
+    def iter_files(self, root: Path, recursive: bool = True) -> Generator[Path, None, None]:
+        for item in root.iterdir():
+            if item.is_file():
+                yield item
+            elif item.is_dir() and recursive and not item.name.startswith('.'):
+                yield from self.iter_files(item)
+
+    @abstractmethod
+    def iter_images(self, root: Path) -> Iterator:
+        pass
+
+    def _download(self, force: bool = False):
+        pass
+
+    @abstractmethod
+    def _prepare(self, force: bool = False):
+        pass
+
+    def load(self, force_download: bool = False, force_prepare: bool = False):
+        self._download(force_download or force_prepare)
+        self._prepare(force_prepare)
+
+    def __len__(self):
+        return self.data_length
+
+
+class PyTorchDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset: BaseDataset, storage: ImageFileStorage, transform=None, target_transform=None):
+        super().__init__()
+        self.device = torch.device('cuda' if USE_CUDA else 'cpu')
+        self.dataset = dataset
+        self.storage = storage
+
+    def __getitem__(self, index):
+        return self.storage[self.dataset[index]]
+
+    def __len__(self):
+        return len(self.dataset)
---
+++ packages/dataset-format-benchmark/dataset_format_benchmark-0.2.0.tar.gz/dataset_format_benchmark-0.2.0/src/dataset_format_benchmark/datasets/own/transport.py
@@ -0,0 +1,73 @@
+import concurrent.futures
+import logging
+import multiprocessing
+from pathlib import Path
+from typing import Generator, Sequence
+
+import numpy as np
+import rawpy
+from rawpy._rawpy import Params
+
+from dataset_format_benchmark.datasets import BaseDataset
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class OwnTransportDataset(BaseDataset):
+    DATASET_DIR_NAME = 'own_transport'
+    IMAGE_HEIGHT = 6336
+    IMAGE_WIDTH = 9504
+    METADATA_FILE_NAME = 'metadata.json'
+    LABELS = {
+        0: 'Car',
+        1: 'Truck',
+        2: 'Airplane',
+        3: 'Train',
+        4: 'Bicycle',
+        5: 'Scooter',
+        6: 'Ship',
+    }
+
+    def iter_images(self, root: Path) -> Generator[Path, None, None]:
+        return (
+            file_path
+            for file_path in self.iter_files(root)
+            if file_path.name.lower().endswith('.arw')
+        )
+
+    def _convert_raw(self, raw_image_path: Path):
+        raw_image = rawpy.imread(str(raw_image_path))
+
+        for storage in self.storages:
+            for color_space in storage.color_spaces:
+                for bits in storage.SUPPORTED_BPS:
+                    params = Params(
+                        # demosaic_algorithm=DemosaicAlgorithm.DCB, dcb_iterations=1, dcb_enhance=True,
+                        median_filter_passes=0, use_camera_wb=True, output_color=color_space, output_bps=bits,
+                        no_auto_bright=True
+                    )
+                    processed_image = np.asarray(raw_image.postprocess(params))
+                    storage_dir_name = storage.IMAGE_FILE_EXTENSION
+                    color_space_name = str(color_space).split('.')[-1]
+
+                    logger.info(f'Converting {str(raw_image_path)} to {storage_dir_name}')
+
+                    storage.save_image(raw_image_path.parent, raw_image_path.name, bits, color_space_name,
+                                       processed_image)
+
+    def _convert_raws(self, root: Path) -> Sequence[str]:
+        filenames = []
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+            # semaphore = multiprocessing.Semaphore(multiprocessing.cpu_count())
+
+            for image_path in self.iter_images(root):
+                executor.submit(self._convert_raw, image_path)
+                filenames.append(image_path.name)
+
+        return filenames
+
+    def _prepare(self, force: bool = True):
+        if force or not self.metadata_file_path.exists():
+            self.filenames = self._convert_raws(self.dataset_root_path)
