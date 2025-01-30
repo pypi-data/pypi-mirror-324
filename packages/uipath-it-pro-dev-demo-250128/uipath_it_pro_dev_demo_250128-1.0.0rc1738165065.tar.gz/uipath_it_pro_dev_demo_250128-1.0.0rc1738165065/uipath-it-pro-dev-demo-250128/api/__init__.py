ae9265d991771c84ca87e017cddba9e149a5849d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 04:04:02 +0000

    Add package uipath-it-pro-dev-demo-250128/1.0.0rc1738165065/uipath_it_pro_dev_demo_250128-1.0.0rc1738165065.tar.gz

---
+++ packages/uipath-it-pro-dev-demo-250128/uipath_it_pro_dev_demo_250128-1.0.0rc1738165065.tar.gz/uipath_it_pro_dev_demo_250128-1.0.0rc1738165065/uipath-it-pro-dev-demo-250128/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from uipath-it-pro-dev-demo-250128.api.assets_api import AssetsApi
+from uipath-it-pro-dev-demo-250128.api.automations_api import AutomationsApi
+
