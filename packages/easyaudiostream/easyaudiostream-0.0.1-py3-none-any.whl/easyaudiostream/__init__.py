14004f1c9d4cf2b3da495c662dfbdd8668928c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:03:55 +0000

    Add package easyaudiostream/0.0.1/easyaudiostream-0.0.1-py3-none-any.whl

---
+++ packages/easyaudiostream/easyaudiostream-0.0.1-py3-none-any.whl/easyaudiostream/__init__.py
@@ -0,0 +1,3 @@
+from ._version import __version__
+from .audio import play_audio, play_raw_audio, play_raw_stream, play_stream
+from .mic import get_mic_stream, get_mic_stream_async, list_mics
---
+++ packages/easyaudiostream/easyaudiostream-0.0.1-py3-none-any.whl/easyaudiostream/_capabilities.py
@@ -0,0 +1,12 @@
+import subprocess
+
+has_ffmpeg = (
+    subprocess.run(["ffplay", "-version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0
+)
+
+try:
+    import pyaudio
+except ImportError:
+    has_pyaudio = False
+else:
+    has_pyaudio = True
