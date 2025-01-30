6f942b65c3aeb0096ede7eadf6b3b15dfd4f38af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 15:05:14 +0000

    Add package q3dviewer/1.0.7/q3dviewer-1.0.7.tar.gz

---
+++ packages/q3dviewer/q3dviewer-1.0.7.tar.gz/q3dviewer-1.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='q3dviewer',
+    version='1.0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'q3dviewer': ['shaders/*.glsl'],
+    },
+    install_requires=[
+        'numpy',
+        'pyside6',
+        'PyOpenGL',
+        'pillow',
+        'meshio',
+        'pypcd4',
+        'pye57',
+        'laspy',
+        ],
+    entry_points={
+        'console_scripts': [
+            'cloud_viewer = q3dviewer.tools.cloud_viewer:main',
+            'ros_viewer = q3dviewer.tools.ros_viewer:main',
+            'mesh_viewer = q3dviewer.tools.mesh_viewer:main',
+            'gaussian_viewer = q3dviewer.tools.gaussian_viewer:main',
+            'lidar_cam_calib = q3dviewer.tools.lidar_cam_calib:main',
+            'lidar_calib = q3dviewer.tools.lidar_calib:main',
+        ],
+    },
+)
