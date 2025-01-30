4c72baf08294e071af41796fb7ae0d407e59d37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 15:06:31 +0000

    Add package py-deepseek-api/1.0.2/py_deepseek_api-1.0.2-py3-none-any.whl

---
+++ packages/py-deepseek-api/py_deepseek_api-1.0.2-py3-none-any.whl/deepseek/__init__.py
@@ -0,0 +1,12 @@
+from ._api   import DeepSeekApi
+from ._chat  import Conversation
+from ._const import FileStatus
+__all__ = [
+    "DeepSeekApi",
+    "Conversation",
+    "FileStatus"
+]
+
+
+__version__ = "1.0.2"
+
