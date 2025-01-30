2f2b0d6a6c3dda5c0ada41302a987bf29acb1aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:46 +0000

    Add package sprite-splitter/0.0.2/sprite_splitter-0.0.2-py3-none-any.whl

---
+++ packages/sprite-splitter/sprite_splitter-0.0.2-py3-none-any.whl/sprite_splitter/__init__.py
@@ -0,0 +1,5 @@
+from .sprite_splitter import AlphaSpriteSplitter
+from .box import Box
+from .edge_finder import AlphaSpriteEdgeStartFinder
+
+__all__ = ['AlphaSpriteSplitter', 'Box', 'AlphaSpriteEdgeStartFinder']
