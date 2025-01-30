ef0ab3ce10e68b5b5aa33e13397148f9928bd08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:04:11 +0000

    Add package places-intel/0.2.2/places_intel-0.2.2-py3-none-any.whl

---
+++ packages/places-intel/places_intel-0.2.2-py3-none-any.whl/places_intel/__init__.py
@@ -0,0 +1,4 @@
+from .main import multiple_place_intel, single_place_intel, create_kml, hotel_intel
+from .outscraper_reviews import OutscraperInterface
+
+__all__ = ['multiple_place_intel', 'single_place_intel', 'OutscraperInterface']
