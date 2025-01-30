e8d1e6f2f0bb9047d93ce1243dd07307b2301cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:34 +0000

    Add package SFSGL/0.1.8/sfsgl-0.1.8.tar.gz

---
+++ packages/SFSGL/sfsgl-0.1.8.tar.gz/sfsgl-0.1.8/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    author='Hasan Ali Özkan',
+    description='Simple File Sharing and Gathering Library',
+    name='SFSGL',
+    version='0.1.8',
+    url='https://github.com/hasanaliozkan-dev/SFSGL/',
+    project_urls={
+        "Source Code": "https://github.com/hasanaliozkan-dev/SFSGL/",
+        "Bug Tracker": "https://github.com/hasanaliozkan-dev/SFSGL/issues",
+        "Documentation": "https://github.com/hasanaliozkan-dev/SFSGL/blob/main/README.md"
+    },
+    packages=find_packages(),
+    install_requires=['flask', 'werkzeug'],
+    package_data={
+        '': ['*.html', '*.py'],
+        'SFSGL': ['static/*', 'templates/*'],
+    },
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
