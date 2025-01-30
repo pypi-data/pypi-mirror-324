91070d95d0e0be1fa81a50b3c4a032b6f4429e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:13 +0000

    Add package temu-captcha-solver/0.4.3/temu_captcha_solver-0.4.3.tar.gz

---
+++ packages/temu-captcha-solver/temu_captcha_solver-0.4.3.tar.gz/temu_captcha_solver-0.4.3/src/temu_captcha_solver/captchatype.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class CaptchaType(Enum):
+    ARCED_SLIDE = 0
+    PUZZLE = 1
+    SEMANTIC_SHAPES = 2
+    THREE_BY_THREE = 3
---
+++ packages/temu-captcha-solver/temu_captcha_solver-0.4.3.tar.gz/temu_captcha_solver-0.4.3/src/temu_captcha_solver/selectors.py
@@ -0,0 +1,35 @@
+ARCED_SLIDE_PUZZLE_IMAGE_SELECTOR = "#slider > img"
+ARCED_SLIDE_PIECE_CONTAINER_SELECTOR = "#img-button"
+ARCED_SLIDE_PIECE_IMAGE_SELECTOR = "#img-button > img"
+ARCED_SLIDE_BUTTON_SELECTOR = "#slide-button"
+ARCED_SLIDE_UNIQUE_IDENTIFIERS = [".handleBar-vT4I5", ".vT4I57cQ", "div[style=\"width: 414px;\"] #slider", "div[style=\"width: 410px;\"] #slider"]
+
+PUZZLE_BUTTON_SELECTOR = "#slide-button"
+PUZZLE_PUZZLE_IMAGE_SELECTOR = "#slider > img"
+PUZZLE_PIECE_IMAGE_SELECTOR = "#img-button > img"
+PUZZLE_UNIQUE_IDENTIFIERS = ["#Slider"]
+
+SEMANTIC_SHAPES_IFRAME = "iframe"
+SEMANTIC_SHAPES_CHALLENGE_ROOT_ELE = "#Picture"
+SEMANTIC_SHAPES_CHALLENGE_TEXT = "div[class^='picture-text'], div._2Alt0zsN"
+SEMANTIC_SHAPES_IMAGE = "#captchaImg"
+SEMANTIC_SHAPES_REFRESH_BUTTON = ".refresh-27d6x, .ZVIQM964"
+SEMANTIC_SHAPES_UNIQUE_IDENTIFIERS = [SEMANTIC_SHAPES_IFRAME, ".iframe-3eaNR", ".iframe-8Vtge", "#captchaImg"]
+
+THREE_BY_THREE_IMAGE = "img.loaded"
+THREE_BY_THREE_TEXT = ".verifyDialog div[role=dialog]"
+THREE_BY_THREE_CONFIRM_BUTTON = ".verifyDialog div[role=button]:has(span)"
+THREE_BY_THREE_UNIQUE_IDENTIFIERS = ["#imageSemantics img.loaded"]
+
+# Occassionally Temu shows a familiar challenge but it's in an iframe
+# Need a way to account for occasions when other challenges are nested in iframe
+
+CAPTCHA_PRESENCE_INDICATORS = [
+    "#imageSemantics img.loaded",
+    "#slide-button",
+    "#Slider",
+    "#slider",
+    SEMANTIC_SHAPES_IFRAME,
+    "img._1fsu8Elk",
+    "#captchaImg"
+]
