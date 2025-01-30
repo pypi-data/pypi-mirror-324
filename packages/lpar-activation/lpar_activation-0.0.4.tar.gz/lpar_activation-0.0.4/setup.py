c9cba5120de31fa60ef3a4a942facc75507329a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 03:04:47 +0000

    Add package lpar-activation/0.0.4/lpar_activation-0.0.4.tar.gz

---
+++ packages/lpar-activation/lpar_activation-0.0.4.tar.gz/lpar_activation-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="lpar_activation",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=["pytest==8.3.4",
+                      "gitdb==4.0.12",
+                      "GitPython==3.1.44",
+                      "pytest==8.3.4",
+                      "setuptools==75.8.0",
+                      ],
+)
