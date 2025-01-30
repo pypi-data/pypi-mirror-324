20111b3c80aeb2aa7451e63fe0be370667799b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:05:08 +0000

    Add package fameio/3.1.0/fameio-3.1.0-py3-none-any.whl

---
+++ packages/fameio/fameio-3.1.0-py3-none-any.whl/fameio/cli/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2024 German Aerospace Center <fame@dlr.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from fameio.cli.parser import update_default_config
---
+++ packages/fameio/fameio-3.1.0-py3-none-any.whl/fameio/input/schema/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024 German Aerospace Center <fame@dlr.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .agenttype import AgentType
+from .attribute import AttributeSpecs, AttributeType
+from .java_packages import JavaPackages
+from .schema import Schema
