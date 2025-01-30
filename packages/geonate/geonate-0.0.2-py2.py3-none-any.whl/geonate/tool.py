4103f445ff7dfde73eb0d9de2fa26d2f61260bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:43 +0000

    Add package geonate/0.0.2/geonate-0.0.2-py2.py3-none-any.whl

---
+++ packages/geonate/geonate-0.0.2-py2.py3-none-any.whl/geonate/tool.py
@@ -0,0 +1,216 @@
+"""
+The common module contains common functions and classes used by the other modules.
+
+"""
+# import common packages 
+
+from typing import AnyStr, Dict, Optional
+
+##############################################################################################
+#                                                                                                                                                                                                          #
+#                       Main functions                                                                                                                                                         #
+#                                                                                                                                                                                                           #
+##############################################################################################
+
+'''
+TOOL FUNCTIONS
+
+1. list_files
+2. empty_dataframe
+2. getExtent
+
+
+'''
+# =========================================================================================== #
+#               Create an empty dataframe                                                                                                                                           #
+# =========================================================================================== #
+def dataframe(nrows, ncols, value='NA', name=None):
+    '''
+    Create an empty dataframe
+
+    Parameters:
+        nrows: numeric, numbers of rows
+        ncols: numeric, number of columns
+        value: input value in all cells, default is NA
+        name: vector, names of columns, if not given, it will return default as number of column
+        
+    Example:
+        data = tools.dataframe(nrows= 5, ncols= 2, name= ['Col1', 'Col2'])
+
+    '''
+    import pandas as pd
+    import numpy as np
+    
+    # Check validity of column name
+    if name is None:
+        column_names = [f'Col_{i+1}' for i in range(ncols)]
+    elif len(name) == ncols:
+        column_names = name
+    else:
+        raise ValueError("Length of column names vector must match numbers of columns")
+
+    # check input value
+    try: 
+        if isinstance(value, int):
+            val = value
+        elif isinstance(value, float):
+            val = value
+        else:
+            val = np.nan
+    except ValueError:
+        val = np.nan
+    
+    # Create data and parse it into dataframe 
+    data = [[val] * ncols for _ in range(nrows)]
+    dataframe = pd.DataFrame(data, columns= column_names)
+    
+    return dataframe
+
+
+# =========================================================================================== #
+#               Find all files in folder with specific pattern                                                                                                                  #
+# =========================================================================================== #
+def listFiles(path: AnyStr, pattern: AnyStr, search_type: AnyStr = 'pattern', full_name: bool=True):
+    '''
+    List all files with specific pattern within a folder path
+
+    Parameters:
+        path: string, folder path where files stored
+        pattern: string, search pattern of files (e.g., '*.tif')
+        search_type: string, search type whether by "extension" or name "pattern"
+        full_name: boolean, whether returning full name with path detail or not
+        
+    Example:
+        files = tools.list_files(path= './Sample_data/shapefile/', pattern='*shp')
+
+    '''
+    import os
+    import fnmatch
+
+    # Create empty list to store list of files
+    files_list = []
+
+    # Check search type
+    if (search_type.upper() == 'EXTENSION') or (search_type.upper() == 'E'):
+        if '*' in pattern:
+            raise ValueError("Do not use '*' in the pattern of extension search")
+        else:
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    if file.lower().endswith(pattern):
+                        if full_name is True:
+                            files_list.append(os.path.join(root, file))
+                        else:
+                            files_list.append(file)    
+    
+    elif (search_type.upper() == 'PATTERN') or (search_type.upper() == 'P'):
+        if '*' not in pattern:
+            raise ValueError("Pattern search requires '*' in pattern")
+        else:
+            for root, dirs, files in os.walk(path):
+                for file in fnmatch.filter(files, pattern):
+                    if full_name is True:
+                        files_list.append(os.path.join(root, file))
+                    else:
+                        files_list.append(file)
+    
+    else:
+        raise ValueError('Search pattern must be one of these types (pattern, p, extension, e)')
+
+    return files_list
+
+
+# =========================================================================================== #
+#               Get general extent                                                                                                                                                           #
+# =========================================================================================== #
+def extent(input: AnyStr, poly: bool= True):
+    '''
+    Get spatial extent of geotif image from a list or local variable
+
+    Parameters:
+        input: string, an input as a list of geotif files or local image/shapefile
+        poly: bool, whether returns the extent polygon as geopandas object
+        
+    Example:
+        files = tools.list_files('./landsat_multi/merge/', 'tif')
+        ext, poly = tools.extent(files)
+
+    '''
+    import rasterio
+    import geopandas as gpd
+    from shapely.geometry import Polygon
+
+    general_extent = None
+
+    # get extent for raster files store in folder
+    if (isinstance(input, list)) or (isinstance(input, str)):
+        for file in input:
+            with rasterio.open(file) as src:
+                ext = src.bounds
+                crs = src.crs
+
+                if general_extent is None:
+                    general_extent = ext
+                else:
+                    general_extent =  (
+                        min(general_extent[0], ext[0]),
+                        min(general_extent[1], ext[1]),
+                        max(general_extent[2], ext[2]),
+                        max(general_extent[3], ext[3])
+                        )
+                    
+    # get extent for local read shapefile    
+    elif isinstance(input, gpd.GeoDataFrame):
+        ext = input.bounds
+        crs = input.crs
+        general_extent = (ext['minx'], ext['miny'],
+                                        ext['maxx'], ext['maxy'])
+    
+    # get extent for local read geotif 
+    else:
+        general_extent = input.bounds
+        crs = input.crs
+    
+    # return rectangle of extennt
+    if poly is True:
+        poly_geom = Polygon([
+            (general_extent[0], general_extent[1]), 
+            (general_extent[2], general_extent[1]), 
+            (general_extent[2], general_extent[3]), 
+            (general_extent[0], general_extent[3])
+            ])
+        poly = gpd.GeoDataFrame(index=[0], geometry=[poly_geom])
+        poly.crs = {'init': crs}
+    else: 
+        poly = None
+
+    return general_extent, poly
+    
+    
+# =========================================================================================== #
+#              Convert meter to acr-degree based on latitude
+# =========================================================================================== #
+def meter2degree(input, latitude):
+    '''
+    Convert image resolution from meter to acr-degree depending on location of latitude
+
+    Parameters:
+        input: number, input resolution of distance
+        latitude: number, latitude presents location 
+    
+    Example:
+       degree = raster.meter2degree(30, 10.25)   
+
+    '''
+    import numpy as np
+
+    if latitude is None:
+        # Equator location
+        degree = input / (111320 * np.cos(np.radians(0.0)))
+    else:
+        degree = input / (111320 * np.cos(np.radians(latitude)))
+    
+    return degree
+
+
+
