11f622ab41accb721db8d7d8c3bbc8fddbd86220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 15:05:41 +0000

    Add package hilda/2.0.16/hilda-2.0.16-py3-none-any.whl

---
+++ packages/hilda/hilda-2.0.16-py3-none-any.whl/hilda/ipython_extensions/keybindings.py
@@ -0,0 +1,24 @@
+from prompt_toolkit.enums import DEFAULT_BUFFER
+from prompt_toolkit.filters import EmacsInsertMode, HasFocus, HasSelection, ViInsertMode
+from prompt_toolkit.keys import Keys
+
+
+def load_ipython_extension(ipython):
+    def register_keybindings():
+        hilda = ipython.user_ns['p']
+        keys_mapping = {Keys.F1: hilda.ui_manager.show,
+                        Keys.F2: hilda.toggle_enable_stdout_stderr,
+                        Keys.F7: hilda.step_into,
+                        Keys.F8: hilda.step_over,
+                        Keys.F9: lambda _: (hilda.log_info('Sending continue'), hilda.cont()),
+                        Keys.F10: lambda _: (hilda.log_info('Sending stop'), hilda.stop())}
+
+        insert_mode = ViInsertMode() | EmacsInsertMode()
+        registry = ipython.pt_app.key_bindings
+
+        for key, callback in keys_mapping.items():
+            registry.add_binding(key, filter=(HasFocus(DEFAULT_BUFFER) & ~HasSelection() & insert_mode))(
+                callback)
+
+    register_keybindings()
+    ipython.events.register('shell_initialized', register_keybindings)
