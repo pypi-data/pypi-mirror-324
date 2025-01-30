a24eeb65799213849bdfb1421a839f820cddeaf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:05:00 +0000

    Add package monarchmoney/0.1.15/monarchmoney-0.1.15-py2.py3-none-any.whl

---
+++ packages/monarchmoney/monarchmoney-0.1.15-py2.py3-none-any.whl/monarchmoney/__init__.py
@@ -0,0 +1,16 @@
+"""
+monarchmoney
+
+A Python API for interacting with MonarchMoney.
+"""
+
+from .monarchmoney import (
+    LoginFailedException,
+    MonarchMoneyEndpoints,
+    MonarchMoney,
+    RequireMFAException,
+    RequestFailedException,
+)
+
+__version__ = "0.1.15"
+__author__ = "hammem"
