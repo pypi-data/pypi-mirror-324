66f202b2cca8a73f55baaa085ac67aaffc46765a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:26 +0000

    Add package uniserde/0.4.0rc4/uniserde-0.4.0rc4.tar.gz

---
+++ packages/uniserde/uniserde-0.4.0rc4.tar.gz/uniserde-0.4.0rc4/devel.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+
+import uniserde
+import uniserde.compat
+from datetime import datetime, timezone
+import typing as t
+import dataclasses
+from bson import ObjectId
+import tests.models as models
+
+
+
+serde = uniserde.BsonSerde(lazy=False)
+
+value_fresh = models.ChildClass.create_child_variant_1()
+
+value_bson = serde.as_bson(value_fresh)
+assert value_bson['type'] == 'ChildClass'
+
+value_round_trip = serde.from_bson(value_bson, as_type=models.ParentClass)
+
+assert isinstance(value_round_trip, models.ChildClass)
+assert value_fresh == value_round_trip
+
+raise SystemExit()
+
+
+from dataclasses import dataclass
+import uniserde
+import uniserde.codegen
+import uuid
+import tests.models
+import enum
+
+
+original = tests.models.ClassWithStaticmethodOverrides.create()
+
+
+print(f"Original: {original}")
+
+serialized = uniserde.as_bson(original)
+
+print(f"Serialized: {serialized}")
+
+deserialized = uniserde.from_bson(serialized, type(original))
+
+print(f"Deserialized: {deserialized}")
