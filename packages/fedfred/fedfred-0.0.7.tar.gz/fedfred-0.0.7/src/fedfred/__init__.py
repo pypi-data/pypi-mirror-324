0d61a9f10adc3bc2ce6f804bb1142c2f3fd35591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:42 +0000

    Add package fedfred/0.0.7/fedfred-0.0.7.tar.gz

---
+++ packages/fedfred/fedfred-0.0.7.tar.gz/fedfred-0.0.7/src/fedfred/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module initializes the Binance US API package.
+
+Imports:
+    BinanceRestAPI: A class that provides methods to interact with the Binance US REST API.
+"""
+from.fedfred import FredAPI
+from.fedfred import FredMapsAPI
