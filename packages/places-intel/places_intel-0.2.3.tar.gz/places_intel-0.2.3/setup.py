082e7e3f7d470cd4c7228a1cfeda798a8a26115b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:04:12 +0000

    Add package places-intel/0.2.3/places_intel-0.2.3.tar.gz

---
+++ packages/places-intel/places_intel-0.2.3.tar.gz/places_intel-0.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="places_intel",  # Package name
+    version="0.2.3",  # Initial version
+    author="Blake Jennings",
+    author_email="blake@workerbeetech.com",
+    description="A library for fetching and processing place data with polygons using Outscraper and Overpass APIs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # Automatically find sub-packages
+    install_requires=[
+        "outscraper",
+        'requests',
+        "overpy",
+        "pyspark",
+        "pandas",
+        "spark"
+    ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
