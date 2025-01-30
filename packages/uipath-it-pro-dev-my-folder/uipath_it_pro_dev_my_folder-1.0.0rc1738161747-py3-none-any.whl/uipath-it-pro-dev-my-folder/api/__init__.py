c103dc2ee8c44cf98bbb78703d78df91722ddb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 03:06:00 +0000

    Add package uipath-it-pro-dev-my-folder/1.0.0rc1738161747/uipath_it_pro_dev_my_folder-1.0.0rc1738161747-py3-none-any.whl

---
+++ packages/uipath-it-pro-dev-my-folder/uipath_it_pro_dev_my_folder-1.0.0rc1738161747-py3-none-any.whl/uipath-it-pro-dev-my-folder/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from uipath-it-pro-dev-my-folder.api.assets_api import AssetsApi
+from uipath-it-pro-dev-my-folder.api.buckets_api import BucketsApi
+
