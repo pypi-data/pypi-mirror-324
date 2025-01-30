3f59de60b13a0c1a29ee24cfcaffad90355ea61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:35 +0000

    Add package micrOSDevToolKit/2.9.4/micrOSDevToolKit-2.9.4-py3-none-any.whl

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-2.9.4-py3-none-any.whl/toolkit/simulator_lib/uasyncio.py
@@ -0,0 +1,42 @@
+import asyncio
+
+
+def open_connection(host, port, ssl=False):
+    return asyncio.open_connection(host, port, ssl=ssl)
+
+
+class Lock(asyncio.Lock):
+    pass
+
+
+class Event(asyncio.Event):
+    pass
+
+
+async def sleep(sec):
+    await asyncio.sleep(sec)
+
+
+async def sleep_ms(ms):
+    await asyncio.sleep(ms/1000)
+
+
+def get_event_loop():
+    return asyncio.get_event_loop()
+
+
+class Event:
+
+    def __new__(cls):
+        return asyncio.Event()
+
+
+def start_server(awaitable, host, port, backlog):
+    return asyncio.start_server(awaitable, host, port)
+
+
+
+
+
+
+
