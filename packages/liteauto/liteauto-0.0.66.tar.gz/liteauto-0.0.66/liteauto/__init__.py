f3f5ded33310387fc9154cf0ae9b689a87ee8cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:04:09 +0000

    Add package liteauto/0.0.66/liteauto-0.0.66.tar.gz

---
+++ packages/liteauto/liteauto-0.0.66.tar.gz/liteauto-0.0.66/liteauto/__init__.py
@@ -0,0 +1,12 @@
+from .searchlite import google
+from .parselite import parse
+from .visionlite import wlsplit,wlanswer,wltopk,wlsimchunks
+
+
+from .common import web,compress,get_summaries,compress_sequential,summary
+
+from .gmaillite import GmailAutomation,gmail,automail
+
+from .project_to_prompt import ProjectToPrompt
+
+from .arxiv_extractor import get_todays_arxiv_papers
