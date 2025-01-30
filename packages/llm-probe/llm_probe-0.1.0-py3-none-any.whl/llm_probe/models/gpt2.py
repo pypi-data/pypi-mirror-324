dcd986e2f0d0da0306dcd401fd940ee5e7dd2e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 04:03:36 +0000

    Add package llm-probe/0.1.0/llm_probe-0.1.0-py3-none-any.whl

---
+++ packages/llm-probe/llm_probe-0.1.0-py3-none-any.whl/llm_probe/models/gpt2.py
@@ -0,0 +1,28 @@
+##########################################################################
+##  GPT2 Model Information
+##
+##  Authors:  Junsoo    Kim   ( js.kim@hyperaccel.ai        )
+##  Version:  0.0.10
+##  Date:     2025-01-15      ( v0.0.10, init               )
+##
+##########################################################################
+
+##########################################################################
+## Configuration Table
+##########################################################################
+
+cfg = {
+    "architecture": "architectures",
+    "bos_token_id": "bos_token_id",
+    "eos_token_id": "eos_token_id",
+    "hidden_act": "activation_function",
+    "hidden_size": "n_embd",
+    "intermediate_size": "n_inner",
+    "max_length": "n_positions",
+    "num_attention_heads": "n_head",
+    "num_hidden_layers": "n_layer",
+    "norm_eps": "layer_norm_epsilon",
+    "vocab_size": "vocab_size",
+}
+
+##########################################################################
