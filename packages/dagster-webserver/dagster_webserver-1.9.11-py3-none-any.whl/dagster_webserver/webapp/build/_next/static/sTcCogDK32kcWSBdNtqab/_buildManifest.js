50daf48826712772350deb4ba22082360082ccb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:03:43 +0000

    Add package dagster-webserver/1.9.11/dagster_webserver-1.9.11-py3-none-any.whl

---
+++ packages/dagster-webserver/dagster_webserver-1.9.11-py3-none-any.whl/dagster_webserver/webapp/build/_next/static/sTcCogDK32kcWSBdNtqab/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d042268e57c67672.js"],"/_error":["static/chunks/pages/_error-27536e07fe299122.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
