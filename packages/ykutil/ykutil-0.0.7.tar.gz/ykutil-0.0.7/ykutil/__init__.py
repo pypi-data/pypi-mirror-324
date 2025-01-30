2de50ecaefd3b0207984bfe0e8c58e69ab8a588d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 03:04:55 +0000

    Add package ykutil/0.0.7/ykutil-0.0.7.tar.gz

---
+++ packages/ykutil/ykutil-0.0.7.tar.gz/ykutil-0.0.7/ykutil/__init__.py
@@ -0,0 +1,131 @@
+import pkgutil
+from importlib.util import find_spec
+
+from .constants import DEFAULT_PAD_TOKEN, IGNORE_INDEX, SPACE_TOKENIZERS
+from .data_model import (
+    Serializable,
+    sortedtuple,
+    stringify_tuple_keys,
+    summed_stat_dc,
+    undefaultdict,
+)
+from .log_util import add_file_handler, log
+from .multiproc import apply_args_and_kwargs, run_in_parallel, starmap_with_kwargs
+from .sql import merge_databases
+from .python import (
+    all_equal,
+    anyin,
+    approx_list_split,
+    approx_number_split,
+    check_if_in_other_list,
+    chunk_list,
+    count_sublist_occurrences,
+    dict_percentages,
+    dict_without,
+    flatten,
+    identity,
+    index_of_sublist_match,
+    list_find_new,
+    list_flip,
+    list_in_list,
+    list_multiply,
+    list_rindex,
+    list_split,
+    list_split_at_value,
+    list_squeeze,
+    make_list_unique,
+    multify_text,
+    naive_regex_escape,
+    nth_index,
+    re_line_matches,
+    recursed_dict_percentages,
+    recursed_merge_percent_stats,
+    recursed_sum_up_stats,
+    remove_until,
+    removesuffixes,
+    shortest_common_supersequence,
+    split_multi,
+    str_find_all,
+    transpose_li_of_dict,
+    unique_n_times,
+    update_running_avg,
+)
+from .statistics import Statlogger, Welfords
+from .tools import bulk_rename
+from .types_util import T, describe_type
+from .os_tools import search_file_in_folder
+
+if find_spec("torch") is not None and find_spec("numpy") is not None:
+    from .print_tools import describe_array, describe_list
+
+if find_spec("pandera") is not None:
+    from .pandera_util import empty_dataframe_from_model
+
+if find_spec("openai") is not None and find_spec("pydantic") is not None:
+    from .llm_api import (
+        AzureModelWrapper,
+        ModelWrapper,
+        human_readable_parse,
+        local_image_to_data_url,
+    )
+
+if find_spec("dacite") is not None and find_spec("yaml") is not None:
+    from .configuration import from_file
+
+if find_spec("torch") is not None:
+    from .torch_helpers import (
+        deserialize_tensor,
+        disable_gradients,
+        find_all_subarray_poses,
+        free_cuda_memory,
+        pad_along_dimension,
+        print_memory_info,
+        rolling_window,
+        serialize_tensor,
+        tensor_in,
+    )
+
+    if find_spec("accelerate") is not None:
+        from .accelerate_tools import gather_dict
+
+    if find_spec("transformers") is not None:
+        from .model import (
+            find_all_linear_names,
+            get_max_memory_and_device_map,
+            print_parameters_by_dtype,
+            print_trainable_parameters,
+            smart_tokenizer_and_embedding_resize,
+        )
+        from .training import train_eval_and_get_metrics
+        from .transformer import (
+            DataCollatorWithPadding,
+            TokenStoppingCriteria,
+            batch_tokenization,
+            compute_seq_log_probability,
+            dict_from_chat_template,
+            find_tokens_with_str,
+            flat_encode,
+            generate_different_sequences,
+            load_tk_with_pad_tk,
+            obtain_offsets,
+            regex_tokens_using_offsets,
+            tokenize,
+            tokenize_instances,
+            transform_with_offsets,
+            untokenize,
+        )
+
+        if find_spec("datasets") is not None:
+            from .dataset import colorcode_dataset, colorcode_entry, describe_dataset
+
+        if find_spec("transformer_heads") is not None:
+            if find_spec("peft") is not None:
+                from .peft_util import load_maybe_peft_model_tokenizer
+            from .evaluation import (
+                EvaluateFirstStepCallback,
+                compute_classification_head_metrics,
+                compute_metric,
+                compute_metrics_functions,
+            )
+
+__version__ = "0.0.7"
---
+++ packages/ykutil/ykutil-0.0.7.tar.gz/ykutil-0.0.7/ykutil/statistics.py
@@ -0,0 +1,69 @@
+import time
+from collections import defaultdict
+from math import sqrt
+
+
+class Statlogger:
+    def __init__(self):
+        self.statistics = defaultdict(float)
+        self.sum_stats = defaultdict(float)
+        self.statnumbers = defaultdict(int)
+        self.timers = dict()
+
+    def reset(self):
+        self.statistics.clear()
+        self.sum_stats.clear()
+        self.statnumbers.clear()
+
+    def update(self, key, value):
+        self.statistics[key] = (
+            (self.statistics[key] * self.statnumbers[key]) + value
+        ) / (self.statnumbers[key] + 1)
+        self.statnumbers[key] = self.statnumbers[key] + 1
+
+    def update_sum_stat(self, key, value):
+        self.sum_stats[key] += value
+
+    def start_timer(self, key):
+        self.timers[key] = time.perf_counter()
+
+    def stop_timer(self, key, average=False, summed=True):
+        assert summed or average
+        if average:
+            self.update(f"average_{key}", time.perf_counter() - self.timers[key])
+        if summed:
+            self.update_sum_stat(f"total_{key}", time.perf_counter() - self.timers[key])
+        del self.timers[key]
+
+    @property
+    def stats(self):
+        some_dic = self.statistics.copy()
+        some_dic.update(self.sum_stats)
+        return some_dic
+
+
+class Welfords:
+    def __init__(self):
+        self.count: int = 0
+        self.mean: float = 0.0
+        self.M2: float = 0.0
+
+    def update(self, new_value):
+        self.count += 1
+        delta = new_value - self.mean
+        self.mean += delta / self.count
+        delta2 = new_value - self.mean
+        self.M2 += delta * delta2
+
+    @property
+    def std(self) -> float:
+        return sqrt(self.M2 / self.count) if self.count > 1 else 0.0
+
+    def normalize(self, value):
+        return (value - self.mean) / self.std if self.std > 0 else 0.0
+
+    def unnormalize(self, value):
+        return value * self.std + self.mean
+
+    def __repr__(self):
+        return f"Welfords(Mean: {self.mean}, Std: {self.std}, Count: {self.count})"
