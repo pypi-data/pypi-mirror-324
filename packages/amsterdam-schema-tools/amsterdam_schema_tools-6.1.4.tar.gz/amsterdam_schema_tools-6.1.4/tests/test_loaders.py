dc4ad1b6194e2f9eef8cb6794cb85e20df153c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 03:05:39 +0000

    Add package amsterdam-schema-tools/6.1.4/amsterdam_schema_tools-6.1.4.tar.gz

---
+++ packages/amsterdam-schema-tools/amsterdam_schema_tools-6.1.4.tar.gz/amsterdam_schema_tools-6.1.4/tests/test_loaders.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from schematools.loaders import URLSchemaLoader
+
+
+def test_load_all_publishers(schema_loader):
+    pubs = schema_loader.get_all_publishers()
+    assert pubs == {
+        "GLEBZ": {
+            "id": "GLEBZ",
+            "name": "Datateam Glebz",
+            "shortname": "braft",
+            "tags": {"costcenter": "12345.6789"},
+        },
+        "HARRY": {
+            "id": "HARRY",
+            "name": "Datateam Harry",
+            "shortname": "harhar",
+            "tags": {"costcenter": "123456789.4321.13519", "team": "taggy"},
+        },
+        "NOTTHESAMEASFILENAME": {
+            "id": "NOTTHESAMEASFILENAME",
+            "name": "Datateam incorrect",
+            "shortname": "nono",
+            "tags": {"costcenter": "1236789.4321.13519", "team": "taggy"},
+        },
+    }
+
+
+def test_publisher_url():
+    loader = URLSchemaLoader("https://foo.bar/baz/datasets/")
+    assert loader._get_publisher_url() == "https://foo.bar/baz/publishers"
