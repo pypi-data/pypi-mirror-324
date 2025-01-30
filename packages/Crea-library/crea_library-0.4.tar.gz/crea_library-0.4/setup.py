ae532cf4b779b491e5c1bf464f39a3839b952e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:18 +0000

    Add package Crea-library/0.4/crea_library-0.4.tar.gz

---
+++ packages/Crea-library/crea_library-0.4.tar.gz/crea_library-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Crea_library",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[ "scipy", "scikit-learn"],  
+    description="A library for selecting and comparing word vectors from the CREA dataset",
+    author="Alex Skitowski",
+    author_email="askitowski@mcw.edu",
+    url="https://github.com/askitowski1/CREA-Vectors/tree/main/crea_library",
+)
+ 
