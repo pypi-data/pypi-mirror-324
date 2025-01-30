33a93d116d3625c5eb66266a9ed3090450e470f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 16:05:48 +0000

    Add package aicastle/0.7.24/aicastle-0.7.24.tar.gz

---
+++ packages/aicastle/aicastle-0.7.24.tar.gz/aicastle-0.7.24/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+from get_version import read_version
+
+setup(
+    name='aicastle',
+    version=read_version(),
+    packages=find_packages(include=['aicastle', 'aicastle.*']),
+    include_package_data=True,
+    package_data={
+        'aicastle': ['package_data/*'],
+    },
+    
+    # 의존성
+    install_requires=[ 
+        # 'tqdm', 
+        # 'pandas', 
+        # 'scikit-learn',
+        "click",      # CLI 도구
+
+        ## drfc-aws
+        "boto3",
+        "paramiko",
+        # "python-dotenv",
+
+        ### chat
+        "pyyaml",
+        "streamlit",  # Streamlit 의존성
+        'openai',
+        'tiktoken',
+        'pathspec',
+        'pymupdf',
+        'python-dotenv',
+        # 'python-magic',
+        
+        # 'azure-identity',
+        # 'boto3',
+
+        'pillow',
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "aicastle=aicastle.cli:main",  # aicastle 명령어 실행 엔트리포인트
+        ],
+    },
+
+    author='aicastle',
+    author_email='dev@aicastle.io',
+    description='AI Castle Package',
+    url='https://github.com/ai-castle/aicastle',
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    zip_safe=False,
+)
