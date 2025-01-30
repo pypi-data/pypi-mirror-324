40c569ee2ec51ce0adcfc2c6699a271ce777a68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:05:15 +0000

    Add package reaktiv/0.1.0/reaktiv-0.1.0-py3-none-any.whl

---
+++ packages/reaktiv/reaktiv-0.1.0-py3-none-any.whl/reaktiv/__init__.py
@@ -0,0 +1,8 @@
+"""
+Reactive signals for Python with first-class async support
+"""
+
+from .core import Signal, ComputeSignal, Effect
+
+__version__ = "0.1.0"
+__all__ = ["Signal", "ComputeSignal", "Effect"]
