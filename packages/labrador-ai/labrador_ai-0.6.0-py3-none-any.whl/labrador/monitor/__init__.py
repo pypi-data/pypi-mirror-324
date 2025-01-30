2aae88bffeeeac610a4b733a768fa8ccc982b2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 04:05:01 +0000

    Add package labrador-ai/0.6.0/labrador_ai-0.6.0-py3-none-any.whl

---
+++ packages/labrador-ai/labrador_ai-0.6.0-py3-none-any.whl/labrador/monitor/__init__.py
@@ -0,0 +1,9 @@
+from labrador.monitor.watsonx import (
+    WatsonxExternalPromptMonitoring,
+    WatsonxPromptMonitoring,
+)
+
+__all__ = [
+    "WatsonxExternalPromptMonitoring",
+    "WatsonxPromptMonitoring"
+]
