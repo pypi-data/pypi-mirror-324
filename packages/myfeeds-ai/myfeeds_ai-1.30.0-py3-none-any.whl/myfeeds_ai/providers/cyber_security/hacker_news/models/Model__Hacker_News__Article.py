e565ec43076ee94a531a6a228520ed1d0b3215c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:05:18 +0000

    Add package myfeeds-ai/1.30.0/myfeeds_ai-1.30.0-py3-none-any.whl

---
+++ packages/myfeeds-ai/myfeeds_ai-1.30.0-py3-none-any.whl/myfeeds_ai/providers/cyber_security/hacker_news/models/Model__Hacker_News__Article.py
@@ -0,0 +1,13 @@
+from osbot_utils.type_safe.Type_Safe                                                  import Type_Safe
+from osbot_utils.helpers.Guid                                                        import Guid
+from myfeeds_ai.providers.cyber_security.hacker_news.models.Model__Hacker_News__When import Model__Hacker_News__When
+
+
+class Model__Hacker_News__Article(Type_Safe):  # Schema for a single Hacker News article
+    article_id : Guid
+    author     : str
+    description: str
+    image_url  : str
+    link       : str
+    when       : Model__Hacker_News__When
+    title      : str
