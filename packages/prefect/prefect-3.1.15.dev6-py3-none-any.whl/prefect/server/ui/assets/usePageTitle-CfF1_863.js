c428c01e4df7f89d4e5eff66561d617f258a3686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 16:05:22 +0000

    Add package prefect/3.1.15.dev6/prefect-3.1.15.dev6-py3-none-any.whl

---
+++ packages/prefect/prefect-3.1.15.dev6-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-CfF1_863.js
@@ -0,0 +1,2 @@
+import{i as a,bw as c,m as i}from"./index-C8MFjmt7.js";function u(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>i(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{u};
+//# sourceMappingURL=usePageTitle-CfF1_863.js.map
---
+++ packages/prefect/prefect-3.1.15.dev6-py3-none-any.whl/prefect/server/ui/assets/usePrefectApi-C-k4U9Yj.js
@@ -0,0 +1,2 @@
+import{cD as r}from"./index-C8MFjmt7.js";import{p as e}from"./api-Cc6yv1-t.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-C-k4U9Yj.js.map
