3861fb40f9ed781379083441e8846441fc2c74cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 07:04:06 +0000

    Add package blue-objects/5.265.1/blue_objects-5.265.1.tar.gz

---
+++ packages/blue-objects/blue_objects-5.265.1.tar.gz/blue_objects-5.265.1/blue_objects/.abcli/alias.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+alias @download=abcli_download
+
+alias @gif=abcli_gif
+
+alias @cache=abcli_cache
+alias @ref="abcli_cache read"
+alias @tags=abcli_tags
+
+alias @clone=abcli_clone
+alias @cp=abcli_cp
+
+alias @file=blue_objects_file
+
+alias @host=abcli_host
+
+alias @ls=abcli_ls
+
+alias @metadata=abcli_metadata
+
+alias @mlflow=abcli_mlflow
+
+alias @mysql=abcli_mysql
+
+alias @object=abcli_object
+
+alias @objects=blue_objects
+
+alias @publish=abcli_publish
+
+alias @select=abcli_select
+
+alias @upload=abcli_upload
