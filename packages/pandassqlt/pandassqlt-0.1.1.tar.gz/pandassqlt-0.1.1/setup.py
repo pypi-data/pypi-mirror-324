1d708743560b7b6f0c009c11e380f2b7bc55cb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:03:55 +0000

    Add package pandassqlt/0.1.1/pandassqlt-0.1.1.tar.gz

---
+++ packages/pandassqlt/pandassqlt-0.1.1.tar.gz/pandassqlt-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pandassqlt",
+    version="0.1.1",
+    packages=find_packages(),
+    description="",
+    author="Your Name",
+    python_requires=">=3.6",
+)
