ce0767be5420f658a362bda37ffa44e1e404edce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 15:05:10 +0000

    Add package g4f/0.4.3.6/g4f-0.4.3.6.tar.gz

---
+++ packages/g4f/g4f-0.4.3.6.tar.gz/g4f-0.4.3.6/g4f/Provider/OIVSCode.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from .template import OpenaiTemplate
+
+class OIVSCode(OpenaiTemplate):
+    label = "OI VSCode Server"
+    url = "https://oi-vscode-server.onrender.com"
+    api_base = "https://oi-vscode-server.onrender.com/v1"
+    working = True
+    needs_auth = False
+    
+    default_model = "gpt-4o-mini-2024-07-18"
+    default_vision_model = default_model
+    vision_models = [default_model, "gpt-4o-mini"]    
+    model_aliases = {"gpt-4o-mini": "gpt-4o-mini-2024-07-18"}
