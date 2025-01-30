bf0a67170d4eb14b29960930072877b034ecdb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 16:04:48 +0000

    Add package odp-sdk/0.25.9/odp_sdk-0.25.9-py3-none-any.whl

---
+++ packages/odp-sdk/odp_sdk-0.25.9-py3-none-any.whl/odp/client/dto/file_dto.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Any, Dict, Optional, Union
+from uuid import UUID
+
+from pydantic import BaseModel, Field, validator
+
+
+class FileMetadataDto(BaseModel):
+    """File Metadata Model."""
+
+    name: str
+    mime_type: Optional[str] = None
+    dataset: Optional[UUID] = None
+    metadata: Dict[str, Union[bool, int, str]] = Field(default_factory=dict)
+    geo_location: Optional[Any] = None
+    size_bytes: Optional[int] = None
+    checksum: Optional[str] = None
+    created_time: Optional[datetime] = None
+    modified_time: Optional[datetime] = None
+    deleted_time: Optional[datetime] = None
+
+    @validator("name")
+    def lstrip_name(cls, v):
+        if v.startswith("/"):
+            raise ValueError("name cannot start with '/'. Absolute paths are not allowed.")
+        return v
---
+++ packages/odp-sdk/odp_sdk-0.25.9-py3-none-any.whl/odp/client/dto/table_spec.py
@@ -0,0 +1,27 @@
+from typing import List, Literal, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, root_validator
+
+from .tabular_store import TablePartitioningSpec
+
+
+class TableSpec(BaseModel):
+    table_schema: dict
+    partitioning: Optional[List[TablePartitioningSpec]] = None
+
+
+class StageDataPoints(BaseModel):
+    """Model for update data point endpoint."""
+
+    action: Literal["create", "commit"]
+    stage_id: Optional[UUID]
+
+    @root_validator(pre=True)
+    def _validate_action(cls, values):
+        if values.get("action") == "create" and values.get("stage_id"):
+            raise ValueError("stage id cannot be issued with create action")
+        elif values.get("action") == "commit" and not values.get("stage_id"):
+            raise ValueError("stage id must be issued with commit action")
+
+        return values
