043b22719dbe07ec97fc3661e2599dd601751462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 16:04:50 +0000

    Add package halerium-utilities/2.15.0/halerium_utilities-2.15.0-py3-none-any.whl

---
+++ packages/halerium-utilities/halerium_utilities-2.15.0-py3-none-any.whl/halerium_utilities/board/schemas/workflow.py
@@ -0,0 +1,9 @@
+from typing import List
+from halerium_utilities.board.schemas.id_schema import WorkflowId
+from halerium_utilities.board.schemas.path_element import PathElement
+
+
+class Workflow(WorkflowId):
+    name: str = ""
+    chatText: str = ""
+    linearTasks: List[PathElement] = []
