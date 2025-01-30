f7c179c25751ed35ab397b824c76e90bbad08e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:11 +0000

    Add package tethysext-workflows/0.0.2/tethysext_workflows-0.0.2.tar.gz

---
+++ packages/tethysext-workflows/tethysext_workflows-0.0.2.tar.gz/tethysext_workflows-0.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_namespace_packages
+import os
+
+def find_resource_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+# -- Apps Definition -- #
+ext_package = 'workflows'
+release_package = 'tethysext-workflows'
+
+# -- Python Dependencies -- #
+dependencies = ['param', 'geojson', 'panel', 'plotly', 'pyshp', 'tethys_dataset_services', 'tethysplatform']
+
+# -- Get Resource File -- #
+resource_files = find_resource_files('tethysext/workflows/templates')
+resource_files += find_resource_files('tethysext/workflows/public')
+resource_files += find_resource_files('tethysext/workflows/job_scripts')
+
+
+setup(
+    name=release_package,
+    version='0.0.2',
+    description='',
+    long_description='',
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_namespace_packages(),
+    package_data={'': resource_files},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=dependencies,
+)
---
+++ packages/tethysext-workflows/tethysext_workflows-0.0.2.tar.gz/tethysext_workflows-0.0.2/tethysext/workflows/results/__init__.py
@@ -0,0 +1,7 @@
+from .dataset_workflow_result import DatasetWorkflowResult  
+from .plot_workflow_result import PlotWorkflowResult
+from .report_workflow_result import ReportWorkflowResult
+from .spatial_workflow_result import SpatialWorkflowResult
+
+
+__all__ = ['DatasetWorkflowResult', 'PlotWorkflowResult', 'ReportWorkflowResult', 'SpatialWorkflowResult']
