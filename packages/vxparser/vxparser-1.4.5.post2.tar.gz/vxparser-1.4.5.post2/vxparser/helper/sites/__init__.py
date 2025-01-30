a56dca77065131367da9f2944ae66f35e5695108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 05:04:32 +0000

    Add package vxparser/1.4.5.post2/vxparser-1.4.5.post2.tar.gz

---
+++ packages/vxparser/vxparser-1.4.5.post2.tar.gz/vxparser-1.4.5.post2/vxparser/helper/sites/__init__.py
@@ -0,0 +1,3 @@
+from . import einschalten, hdfilme, kinokiste, kkiste, megakino, movie2k, movie4k, serienstream
+# dokus4, hdfilme_1, moflix-stream, topstreamfilm, filmpalast, streamcloud, xcine
+sites = [einschalten, hdfilme, kinokiste, kkiste, megakino, movie2k, movie4k, serienstream]
