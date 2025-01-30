3b6aaba85a22c366ff5abff17967fd452bccef29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:11 +0000

    Add package xraptor/0.3.3/xraptor-0.3.3.tar.gz

---
+++ packages/xraptor/xraptor-0.3.3.tar.gz/xraptor-0.3.3/xraptor/antenna_implementations/memory.py
@@ -0,0 +1,35 @@
+import asyncio
+from asyncio import Queue
+from typing import AsyncIterator
+
+import meeseeks
+
+from xraptor.core.interfaces import Antenna
+
+
+@meeseeks.OnlyOne()
+class MemoryAntenna(Antenna):
+    _queues = {}
+    _config = {}
+
+    async def subscribe(self, antenna_id: str) -> AsyncIterator:
+        if antenna_id not in self._queues:
+            self._queues.update({antenna_id: Queue()})
+        _q: Queue = self._queues[antenna_id]
+        while True:
+            if _q.empty() is False:
+                yield await _q.get()
+            await asyncio.sleep(0.05)
+
+    async def post(self, antenna_id: str, message: str):
+        if antenna_id not in self._queues:
+            self._queues.update({antenna_id: Queue()})
+        _q: Queue = self._queues[antenna_id]
+        await _q.put(message)
+
+    async def is_alive(self, antenna_id: str) -> bool:
+        return antenna_id in self._queues
+
+    @classmethod
+    def set_config(cls, config: dict):
+        pass
