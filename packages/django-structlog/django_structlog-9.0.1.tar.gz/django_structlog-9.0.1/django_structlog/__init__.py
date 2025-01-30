c41e9439953d191a695fa4aaeee47309994a824a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:03:45 +0000

    Add package django-structlog/9.0.1/django_structlog-9.0.1.tar.gz

---
+++ packages/django-structlog/django_structlog-9.0.1.tar.gz/django_structlog-9.0.1/django_structlog/__init__.py
@@ -0,0 +1,8 @@
+""" ``django-structlog`` is a structured logging integration for ``Django`` project using ``structlog``.
+"""
+
+name = "django_structlog"
+
+VERSION = (9, 0, 1)
+
+__version__ = ".".join(str(v) for v in VERSION)
