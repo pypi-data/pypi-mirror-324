9020ff6dab6f383b2dc686a57ae6a5981b3d593c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 15:05:08 +0000

    Add package ClassyData/1.6/ClassyData-1.6.tar.gz

---
+++ packages/ClassyData/ClassyData-1.6.tar.gz/ClassyData-1.6/bin/TopHat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# PYTHON_ARGCOMPLETE_OK
+
+import os, re, sys, argparse, json, datetime, xmltodict, uuid, logging
+
+if os.path.dirname(sys.argv[0]) == '.':
+	sys.path.append('..')
+
+from Handlers.TopHat import TopHat, args
+
+if __name__ == '__main__': 
+	result = args.execute()
+	if result:
+		prettyPrintLn(result, ignore=True)
+
+
+
