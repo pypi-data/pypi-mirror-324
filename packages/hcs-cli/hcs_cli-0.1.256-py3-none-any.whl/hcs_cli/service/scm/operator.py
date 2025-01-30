2f09820165e9541adde48de4175dc405b1019146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 14:04:46 +0000

    Add package hcs-cli/0.1.256/hcs_cli-0.1.256-py3-none-any.whl

---
+++ packages/hcs-cli/hcs_cli-0.1.256-py3-none-any.whl/hcs_cli/service/scm/operator.py
@@ -0,0 +1,18 @@
+from hcs_core.sglib.client_util import hdc_service_client
+
+from hcs_cli.service.task import TaskModel
+
+_client = hdc_service_client("scm")
+
+
+def run(org_id: str, name: str, params: str) -> TaskModel:
+    url = f"/v1/auto-infra/operators/{name}?org_id={org_id}"
+    if params:
+        params = params.replace(" ", "&")
+        url += f"&{params}"
+    return _client.post(url, type=TaskModel)
+
+
+def logs(org_id: str, name: str) -> list:
+    items = _client.get(f"/v1/auto-infra/operators/{name}/logs?org_id={org_id}")
+    return [TaskModel(**item) for item in items]
