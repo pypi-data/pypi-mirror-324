0753a7a88fbcca0e0f68b50eaa6a63fdc4db17d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 06:03:45 +0000

    Add package moose-lib/0.3.769/moose_lib-0.3.769.tar.gz

---
+++ packages/moose-lib/moose_lib-0.3.769.tar.gz/moose_lib-0.3.769/setup.py
@@ -0,0 +1,18 @@
+import sys
+from setuptools import setup, find_packages
+
+version = "0.0.0"
+
+if '--version' in sys.argv:
+    index = sys.argv.index('--version')
+    sys.argv.pop(index)
+    version = sys.argv.pop(index)
+
+setup(
+    name='moose_lib',
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        'pyjwt[crypto]==2.9.0',
+    ],
+)
