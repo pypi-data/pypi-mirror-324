9ebcd6881232feae0909ed7dc6c78c7ab586b916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-30 03:05:04 +0000

    Add package gentrace-py/0.41.0/gentrace_py-0.41.0-py3-none-any.whl

---
+++ packages/gentrace-py/gentrace_py-0.41.0-py3-none-any.whl/gentrace/apis/paths/v2_test_cases_id.py
@@ -0,0 +1,9 @@
+from gentrace.paths.v2_test_cases_id.get import ApiForget
+from gentrace.paths.v2_test_cases_id.delete import ApiFordelete
+
+
+class V2TestCasesId(
+    ApiForget,
+    ApiFordelete,
+):
+    pass
