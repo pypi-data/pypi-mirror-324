50e99d7692f4c1ade500cf3c1e8b37460c482937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:52 +0000

    Add package a5-client/0.1.5/a5-client-0.1.5.tar.gz

---
+++ packages/a5-client/a5-client-0.1.5.tar.gz/a5-client-0.1.5/src/a5client/__init__.py
@@ -0,0 +1,3 @@
+from .a5_client import Serie, Observacion, Crud, observacionesListToDataFrame, createEmptyObsDataFrame
+
+__all__ = ['Serie','Observacion','Crud', 'observacionesListToDataFrame', 'createEmptyObsDataFrame']
