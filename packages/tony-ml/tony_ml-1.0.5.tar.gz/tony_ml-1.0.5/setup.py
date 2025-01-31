41f3db7e71e6a29b7b4d050989593aab18f4fb77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:41 +0000

    Add package tony-ml/1.0.5/tony_ml-1.0.5.tar.gz

---
+++ packages/tony-ml/tony_ml-1.0.5.tar.gz/tony_ml-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tony_ml',
+    version='1.0.5',
+    packages=find_packages(exclude=['main']),
+    install_requires=[],
+    url='https://github.com/anthonygfruit/AutoML',
+    license='Apache 2.0',
+    author='AFruit',
+    author_email='anthony.fruit@gmail.com',
+    description='Testing out some automl shizzle'
+)
