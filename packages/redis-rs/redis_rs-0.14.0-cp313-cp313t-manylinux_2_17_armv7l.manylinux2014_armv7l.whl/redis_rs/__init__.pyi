0f2ce48a2453a400ba954b9397f268cefc504599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 17:05:17 +0000

    Add package redis-rs/0.14.0/redis_rs-0.14.0-cp313-cp313t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/redis-rs/redis_rs-0.14.0-cp313-cp313t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/redis_rs/__init__.pyi
@@ -0,0 +1,33 @@
+__all__ = [
+    "create_client",
+    "Client",
+    "AsyncClient",
+    "exceptions",
+]
+
+from typing import Dict, List, Optional
+
+from redis_rs.client_async import AsyncClient
+
+class exceptions:
+    class PoolError(Exception): ...
+    class RedisError(Exception): ...
+
+class Client:
+    def status(self) -> Dict: ...
+    async def __aenter__(self) -> AsyncClient: ...
+    async def __aexit__(self, *args, **kwargs): ...
+
+def create_client(
+    *args: str,
+    host: str = "localhost",
+    port: int = 6379,
+    max_size: Optional[int] = None,
+    cluster: Optional[bool] = None,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    db: Optional[int] = None,
+    client_id: Optional[str] = None,
+    features: Optional[List[str]] = None,
+    max_delay_ms: Optional[int] = None,
+) -> Client: ...
