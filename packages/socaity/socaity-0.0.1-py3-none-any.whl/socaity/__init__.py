af2bfb2f195cae65596ccebd4bc03c771adf8bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:34 +0000

    Add package socaity/0.0.1/socaity-0.0.1-py3-none-any.whl

---
+++ packages/socaity/socaity-0.0.1-py3-none-any.whl/socaity/__init__.py
@@ -0,0 +1,5 @@
+# settings first to set environment variables
+from socaity.settings import *
+from socaity.api import Face2Face, SpeechCraft
+from fastsdk import gather_generator, gather_results
+from fastsdk.registry import Registry
---
+++ packages/socaity/socaity-0.0.1-py3-none-any.whl/socaity/api/audio/__init__.py
@@ -0,0 +1,1 @@
+from .text2voice import SpeechCraft
