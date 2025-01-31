3fd21751a9bdfcbde580f3f9ac665b65cef4e79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 17:05:10 +0000

    Add package syrius-sdk/0.2.63/syrius_sdk-0.2.63-py3-none-any.whl

---
+++ packages/syrius-sdk/syrius_sdk-0.2.63-py3-none-any.whl/syrius/commands/RunwayImageToVideoCommand.py
@@ -0,0 +1,11 @@
+from syrius.commands.LoopInputCommand import loopType
+from syrius.commands.abstract import AbstractCommand, Command
+
+
+class RunwayImageToVideoCommand(Command):
+    id: int = 70
+    model: str | AbstractCommand | loopType = "gen3a_turbo"
+    prompt: str | AbstractCommand | loopType
+    image: str | AbstractCommand | loopType
+    aspect_ratio: str | AbstractCommand | loopType
+    api_key: str | AbstractCommand | loopType
