52f65fb9a572d1944f3c3bb4c163c6b57cf83306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:04:48 +0000

    Add package logicblocks.event.store/0.1.4a24/logicblocks_event_store-0.1.4a24.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.4a24.tar.gz/logicblocks_event_store-0.1.4a24/src/logicblocks/event/processing/broker/subscribers/store/base.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+
+from ...types import EventSubscriber, EventSubscriberKey
+
+
+@dataclass(frozen=True)
+class EventSubscriberState:
+    group: str
+    id: str
+    last_seen: datetime
+
+    @property
+    def key(self) -> EventSubscriberKey:
+        return EventSubscriberKey(self.group, self.id)
+
+
+class EventSubscriberStore(ABC):
+    @abstractmethod
+    async def add(self, subscriber: EventSubscriber) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def remove(self, subscriber: EventSubscriber) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def list(
+        self,
+        subscriber_group: str | None = None,
+        max_time_since_last_seen: timedelta | None = None,
+    ) -> Sequence[EventSubscriberState]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def heartbeat(self, subscriber: EventSubscriber) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def purge(
+        self, max_time_since_last_seen: timedelta = timedelta(seconds=360)
+    ) -> None:
+        raise NotImplementedError()
