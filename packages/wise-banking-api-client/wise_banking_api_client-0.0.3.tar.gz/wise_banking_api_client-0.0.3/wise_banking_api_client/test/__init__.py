6676b94e31a49944870c273bcc3ee16f9ddede1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:05:30 +0000

    Add package wise-banking-api-client/0.0.3/wise_banking_api_client-0.0.3.tar.gz

---
+++ packages/wise-banking-api-client/wise_banking_api_client-0.0.3.tar.gz/wise_banking_api_client-0.0.3/wise_banking_api_client/test/__init__.py
@@ -0,0 +1,4 @@
+from wise_banking_api_client.client import Client
+from wise_banking_api_client.test.record import TestClient, record
+
+__all__ = ["Client", "TestClient", "record"]
---
+++ packages/wise-banking-api-client/wise_banking_api_client-0.0.3.tar.gz/wise_banking_api_client-0.0.3/wise_banking_api_client/test/test_uuid.py
@@ -0,0 +1,38 @@
+"""Test the UUID functionality."""
+
+import pytest
+from pydantic import BaseModel, ValidationError
+
+from wise_banking_api_client.model.uuid import UUID, new_uuid
+
+
+class M(BaseModel):
+    uuid: UUID
+
+
+def test_uuid_differs():
+    """Check that the UUIDs differ."""
+    assert new_uuid() != new_uuid()
+
+
+def test_uuid_is_str():
+    """Check we have a string."""
+    assert isinstance(new_uuid(), UUID)
+
+
+def test_model_validation():
+    """Check that the model validation works."""
+    m = M(uuid=new_uuid())
+    assert isinstance(m.uuid, UUID)
+    m.model_validate(m)
+
+
+def test_model_validation_str():
+    """Check that the model validation works."""
+    m = M(uuid=str(new_uuid()))
+    m.model_validate(m)
+
+
+def test_invalid_uuid():
+    with pytest.raises(ValidationError):
+        M(uuid="not a uuid")
