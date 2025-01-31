12ead3047e4aa1b9c50167ba1ec41ead950ee4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:19 +0000

    Add package print-in-color/0.1/print_in_color-0.1.tar.gz

---
+++ packages/print-in-color/print_in_color-0.1.tar.gz/print_in_color-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="print_in_color",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Lyubomir Romanov",
+    description="Color print in Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
+
+# pypi-AgEIcHlwaS5vcmcCJDQxYTFhNThjLTIzOWItNDNkNi04MzRlLTdlMGNiYWU2YjlkMAACKlszLCJhNmJhYjI1Yi03N2U2LTQ3ZWItOTkzMS1lNWFkMjFiNTdkODQiXQAABiBFq18U-VV60YxV4hPUI4_uFt1uT4HiZmEl3aadoOcefQ
