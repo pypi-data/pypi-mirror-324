bc0c23b9a8672d4e663db4168cc1e545124d0d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:34 +0000

    Add package langchain-kaburi/0.0.7/langchain_kaburi-0.0.7-py3-none-any.whl

---
+++ packages/langchain-kaburi/langchain_kaburi-0.0.7-py3-none-any.whl/langchain_kaburi/math.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def sub(a: int, b: int) -> int:
+    return a - b
+
+
+def mul(a: int, b: int) -> int:
+    return a * b
