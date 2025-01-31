2eb8d7d7fe95e99eeec736b4edb711fe313c18e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 05:05:48 +0000

    Add package torii-ila/0.1.0/torii_ila-0.1.0-py3-none-any.whl

---
+++ packages/torii-ila/torii_ila-0.1.0-py3-none-any.whl/torii_ila/__init__.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+try:
+	from importlib import metadata
+	__version__ = metadata.version(__package__)
+except ImportError: # :nocov:
+	__version__ = 'unknown'
+
+from .ila import IntegratedLogicAnalyzer, StreamILA
+
+try:
+	from .uart import UARTIntegratedLogicAnalyzer, UARTIntegratedLogicAnalyzerBackhaul
+	_ILA_UART_IMPORTS = ('UARTIntegratedLogicAnalyzer', 'UARTIntegratedLogicAnalyzerBackhaul',)
+	ILA_HAS_UART_SUPPORT = True
+except ImportError:
+	_ILA_UART_IMPORTS = tuple()
+	ILA_HAS_UART_SUPPORT = False
+
+try:
+	from .usb import USBIntegratedLogicAnalyzer, USBIntegratedLogicAnalyzerBackhaul
+	_ILA_USB_IMPORTS = ('USBIntegratedLogicAnalyzer', 'USBIntegratedLogicAnalyzerBackhaul',)
+	ILA_HAS_USB_SUPPORT = True
+except ImportError:
+	_ILA_USB_IMPORTS = tuple()
+	ILA_HAS_USB_SUPPORT = False
+
+__all__ = (
+	'IntegratedLogicAnalyzer', 'StreamILA',
+	*_ILA_UART_IMPORTS,
+	*_ILA_USB_IMPORTS,
+)
