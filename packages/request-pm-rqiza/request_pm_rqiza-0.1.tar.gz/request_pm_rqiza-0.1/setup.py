582b8af5fcfb61118d1595f91ae057ab2aa8c8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:05:50 +0000

    Add package request-pm-rqiza/0.1/request_pm_rqiza-0.1.tar.gz

---
+++ packages/request-pm-rqiza/request_pm_rqiza-0.1.tar.gz/request_pm_rqiza-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="request_pm_rqiza",
+    version="0.1",
+    packages=find_packages(),
+    description="sex",
+    author="rqiza",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/exam_library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
