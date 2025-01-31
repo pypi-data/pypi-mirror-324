65dd6f9a1d5bd29cda83effab36981b5180bfa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:05:09 +0000

    Add package aiohttp-asyncmdnsresolver/0.0.2/aiohttp_asyncmdnsresolver-0.0.2-py3-none-any.whl

---
+++ packages/aiohttp-asyncmdnsresolver/aiohttp_asyncmdnsresolver-0.0.2-py3-none-any.whl/aiohttp_asyncmdnsresolver/__init__.py
@@ -0,0 +1,4 @@
+"""aiohttp_asyncmdnsresolver: An async resolver that support MDNS."""
+
+__version__ = "0.0.2"
+__all__ = ()
---
+++ packages/aiohttp-asyncmdnsresolver/aiohttp_asyncmdnsresolver-0.0.2-py3-none-any.whl/aiohttp_asyncmdnsresolver/api.py
@@ -0,0 +1,5 @@
+"""Public API of the property caching library."""
+
+from ._impl import AsyncMDNSResolver
+
+__all__ = ("AsyncMDNSResolver",)
