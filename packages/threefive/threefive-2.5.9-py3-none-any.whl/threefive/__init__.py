98b08283ccc5c6d42a6d2bd85c651bd31ead4805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 14:05:21 +0000

    Add package threefive/2.5.9/threefive-2.5.9-py3-none-any.whl

---
+++ packages/threefive/threefive-2.5.9-py3-none-any.whl/threefive/__init__.py
@@ -0,0 +1,7 @@
+import sys
+print("\n\033[1;41mHeads Up! \n\nthreefive has been deprecated, and is no longer supported.")
+print("\nplease upgrade to threefive3\n")
+print("https://github.com/superkabuki/threefive3\n")
+print("python3 -mpip install threefive3 \033[0m")
+print("\n\nI realize thiis is a dick move, \nbut y'all are killing me. \n\n \t\t~ Adrian of Doom")
+sys.exit()
