df882a6d20f444d40e6f7ac35255eeab306dd267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 05:07:32 +0000

    Add package solo-server/0.3.4/solo_server-0.3.4.tar.gz

---
+++ packages/solo-server/solo_server-0.3.4.tar.gz/solo_server-0.3.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="solo-server",
+    version="0.3.4",
+    author="Dhruv Diddi",
+    author_email="dhruv.diddi@gmail.com",
+    description="AIOps for the Physical World.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIEngineersDev/solo-server",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'solo_server': [
+            'commands/*.py',
+            'cli.py',
+            'setup.py',
+            'utils.py',
+            'config.py'
+        ],
+    },
+    install_requires=[
+        "typer",
+    ],
+    extras_require={
+        "dev": ["pytest", "black", "isort"],
+    },
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "solo-server=solo_server.cli:app",
+        ],
+    },
+)
