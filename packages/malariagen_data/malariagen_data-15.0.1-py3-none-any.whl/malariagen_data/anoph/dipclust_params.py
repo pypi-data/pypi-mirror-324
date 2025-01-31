8e4c12087986bdfd2fc8148f610310bff6ddec05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:54 +0000

    Add package malariagen_data/15.0.1/malariagen_data-15.0.1-py3-none-any.whl

---
+++ packages/malariagen_data/malariagen_data-15.0.1-py3-none-any.whl/malariagen_data/anoph/dipclust_params.py
@@ -0,0 +1,9 @@
+"""Parameters for diplotype clustering functions."""
+
+from .distance_params import distance_metric
+from .clustering_params import linkage_method
+
+
+linkage_method_default: linkage_method = "complete"
+
+distance_metric_default: distance_metric = "cityblock"
