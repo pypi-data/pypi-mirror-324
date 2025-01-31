33d25d3b1ee845ba6ae83860eae933859c51daac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:48 +0000

    Add package nonebot-adapter-qq/1.6.2/nonebot_adapter_qq-1.6.2.tar.gz

---
+++ packages/nonebot-adapter-qq/nonebot_adapter_qq-1.6.2.tar.gz/nonebot_adapter_qq-1.6.2/nonebot/adapters/qq/__init__.py
@@ -0,0 +1,17 @@
+from .bot import Bot as Bot
+from .utils import log as log
+from .event import *  # noqa: F403
+from .utils import escape as escape
+from .adapter import Adapter as Adapter
+from .message import Message as Message
+from .permission import *  # noqa: F403
+from .utils import unescape as unescape
+from .exception import ActionFailed as ActionFailed
+from .exception import NetworkError as NetworkError
+from .message import MessageSegment as MessageSegment
+from .exception import AuditException as AuditException
+from .exception import NoLogException as NoLogException
+from .exception import ApiNotAvailable as ApiNotAvailable
+from .exception import QQAdapterException as QQAdapterException
+from .exception import RateLimitException as RateLimitException
+from .exception import UnauthorizedException as UnauthorizedException
