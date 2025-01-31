a767d62c49145d19d0e4103f46adca21c54ee605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:54 +0000

    Add package morph-data/0.1.2/morph_data-0.1.2-py3-none-any.whl

---
+++ packages/morph-data/morph_data-0.1.2-py3-none-any.whl/morph/frontend/template/vite.config.ts
@@ -0,0 +1,64 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import ViteRestart from "vite-plugin-restart";
+import mdx from "@mdx-js/rollup";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeExtractToc from "@stefanprobst/rehype-extract-toc";
+import rehypeExtractTocMdxExport from "@stefanprobst/rehype-extract-toc/mdx";
+import rehypeSlug from "rehype-slug";
+
+/** @type {import('rehype-pretty-code').Options} */
+const prettyCodeOptions = { theme: "github-dark" };
+
+// https://vitejs.dev/config/
+export default defineConfig((env) => ({
+  plugins: [
+    react(),
+    {
+      enforce: "pre",
+      ...mdx({
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+          rehypeSlug,
+          [rehypePrettyCode, prettyCodeOptions],
+          rehypeExtractToc,
+          rehypeExtractTocMdxExport,
+        ],
+      }),
+    },
+    ViteRestart({
+      restart: ["../../src/pages/**/*"],
+    }),
+  ],
+  base: env.mode === "development" ? "" : "/_vite-static",
+  server: {
+    host: "0.0.0.0",
+    open: false,
+    watch: {
+      usePolling: true,
+      disableGlobbing: false,
+    },
+    cors: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: resolve("./dist"),
+    assetsDir: "assets",
+    target: "es2015",
+    manifest: "manifest.json",
+    rollupOptions: {
+      input: {
+        main: resolve("./src/main.tsx"),
+      },
+      output: {
+        entryFileNames: `assets/bundle.js`,
+      },
+    },
+  },
+}));
