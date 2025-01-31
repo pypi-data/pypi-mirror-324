fdf45e43ff8e41cacf8980eec975945fcf9b7648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:04:58 +0000

    Add package pyamber/0.5.13/pyamber-0.5.13.tar.gz

---
+++ packages/pyamber/pyamber-0.5.13.tar.gz/pyamber-0.5.13/pyamber/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+__title__ = "pyamber"
+__version__ = "0.1.1"
+__author__ = "Thomas Schmelzer"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 by Thomas Schmelzer"
