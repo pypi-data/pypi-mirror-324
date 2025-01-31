fb8fb82730ac9de63c97e21373288cd3c76e3a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:31 +0000

    Add package durukti/0.0.2/durukti-0.0.2.tar.gz

---
+++ packages/durukti/durukti-0.0.2.tar.gz/durukti-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+ver = '0.0.2'
+
+classifiers=[
+                   "Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+               ]
+
+# data_files = []
+setup (name = 'durukti',
+       version = ver,
+       description = 'Package for writing code via AI',
+       author = 'Piklu Das',
+       author_email = 'pikludas86717@gmail.com',
+       python_requires='>=3.10.11',
+       packages=find_packages(include=['durukti','durukti' ]),
+       py_modules=['durukti']
+       )
