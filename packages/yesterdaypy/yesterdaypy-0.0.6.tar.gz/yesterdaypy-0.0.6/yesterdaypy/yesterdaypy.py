23d598d0a7b4321845579f1c8179f21f46dc91fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 14:05:50 +0000

    Add package yesterdaypy/0.0.6/yesterdaypy-0.0.6.tar.gz

---
+++ packages/yesterdaypy/yesterdaypy-0.0.6.tar.gz/yesterdaypy-0.0.6/yesterdaypy/yesterdaypy.py
@@ -0,0 +1,46 @@
+# External Imports
+# Import only with "import package",
+# it will make explicity in the code where it came from.
+import argparse
+import os
+import sys
+
+# Internal Imports
+# Import only with "from x import y", to simplify the code.
+#SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+#sys.path.append(os.path.dirname(SCRIPT_DIR))
+#sys.path.append(os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from yesterdaypy.products.firewall import backup as firewall_backup
+from yesterdaypy.utils.utils import error
+
+#PRODUCTS = ["firewall", "linode"]
+PRODUCTS = ["firewall"]
+
+
+def backup() -> None:
+    """"Backup objects to local or Linode Object Storage."""
+    if args.storage is None:
+        storage = os.getcwd()
+    else:
+        storage = args.storage
+    for product in args.products:
+        eval(f"{product}_backup(storage='{storage}')")
+
+
+if "LINODE_TOKEN" in os.environ:
+    token = os.environ["LINODE_TOKEN"]
+else:
+    error(code=1, text="Environment variable LINODE_TOKEN not setup")
+parser = argparse.ArgumentParser()
+parser.add_argument("--storage", type=str,
+                    help="storage to save the data.")
+parser.add_argument("--products", choices=PRODUCTS, nargs="+", default=PRODUCTS,
+                    help="products to backup.")
+args = parser.parse_args()
+
+def main() -> None:
+    backup()
+
+if __name__=="__main__":
+    main()
