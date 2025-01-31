9b0fcae3e33bba3eb6588fe93356eb87fe02e231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 17:05:15 +0000

    Add package dlordinal/2.3.2/dlordinal-2.3.2-py3-none-any.whl

---
+++ packages/dlordinal/dlordinal-2.3.2-py3-none-any.whl/dlordinal/losses/__init__.py
@@ -0,0 +1,27 @@
+from .beta_loss import BetaCrossEntropyLoss
+from .binomial_loss import BinomialCrossEntropyLoss
+from .custom_targets_loss import CustomTargetsCrossEntropyLoss
+from .exponential_loss import ExponentialRegularisedCrossEntropyLoss
+from .general_triangular_loss import GeneralTriangularCrossEntropyLoss
+from .geometric_loss import GeometricCrossEntropyLoss
+from .mceloss import MCELoss
+from .mcewkloss import MCEAndWKLoss
+from .ordinal_ecoc_distance_loss import OrdinalECOCDistanceLoss
+from .poisson_loss import PoissonCrossEntropyLoss
+from .triangular_loss import TriangularCrossEntropyLoss
+from .wkloss import WKLoss
+
+__all__ = [
+    "CustomTargetsCrossEntropyLoss",
+    "BetaCrossEntropyLoss",
+    "PoissonCrossEntropyLoss",
+    "BinomialCrossEntropyLoss",
+    "ExponentialRegularisedCrossEntropyLoss",
+    "WKLoss",
+    "MCELoss",
+    "MCEAndWKLoss",
+    "OrdinalECOCDistanceLoss",
+    "TriangularCrossEntropyLoss",
+    "GeneralTriangularCrossEntropyLoss",
+    "GeometricCrossEntropyLoss",
+]
