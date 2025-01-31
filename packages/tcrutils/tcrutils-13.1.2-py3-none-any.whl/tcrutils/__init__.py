0405e0e8cf7b0add092c59878a0ec78c791e7497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 14:07:07 +0000

    Add package tcrutils/13.1.2/tcrutils-13.1.2-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-13.1.2-py3-none-any.whl/tcrutils/__init__.py
@@ -0,0 +1,143 @@
+"""# Useful stuff for tcr projects.
+
+```py
+>>> from tcrutils.<feature_module> import feature
+```
+
+Nyaaa :3
+
+```py
+>>> from tcrutils.print import gay
+>>> print(gay('nyaaaaa')) # uwu :3
+"""
+
+__tcr_rainbow__ = True
+
+
+if False:  # A crude performance-measuring function
+	import time
+
+	def b(s=""):
+		if not hasattr(b, "t"):
+			b.t = time.perf_counter()
+			b.s = 0
+			return
+		t1 = b.t
+		t2 = time.perf_counter()
+
+		t = t2 - t1
+		b.s += t
+
+		print(f"{s:10} {t:.10f}")
+		b.t = time.perf_counter()
+
+
+if False:  # Left `if False`d out for archival purposes (yeah.. there's git but i like it being here :3 uwu)
+	# fmt: off
+	from . import case as case
+	from . import ensure_deps as ensure_depencencies
+	from . import joke as joke
+	from . import repl
+	from . import types as types
+	from ._version import __version__
+	from .b64 import b64
+	from .class_ import CachedInstancesMeta, DefaultsGetAttr, DefaultsGetItem, DefaultsGetSetAttr, DefaultsGetSetItem, ErrDenoted, NoInit, Singleton, new_cell, partial_class
+	from .classfuncs import get_classname, get_name_classname, get_qualname_classname
+	from .compare import able, able_simple, able_simple_result, isdunder
+	from .console import breakpoint, console, start_eval_session
+	from .console import console as c
+	from .constants import *
+	from .context import random_seed_lock
+	from .decorator import autorun, convert, copy_kwargs, copy_kwargs_sunder, instance, skip_first_call, skip_first_call_async, test, timeit, with_overrides
+	from .dev import generate_function_argument_typehints, generate_type_hinter
+	from .dict import DotDict, JSDict, JSDotDict, clean_dunder_dict, dict_zip, merge_dicts
+	from .dir import dir2, dir3, dir_recursive, vars2, vars3, vars_recursive
+	from .error import error
+	from .extract_error import extract_error, extract_traceback, module_error_map, modules_error_map, print_exception_with_traceback
+	from .F import F
+	from .getch import KeyCode, KeyCodeCompound, KeyCodeSimple, getch, getchs
+	from .import_ import load_package_dynamically
+	from .inject import ErrorCatcher, WarningCatcher
+	from .input import insist
+	from .inspect import eval_fback, get_file_colon_lineno, get_lineno
+	from .int import clamp, float2int, hex, recursive_sum
+	from .iterable import Or, batched, bogo_sort, cut_at, getattr_queue, getmanyattrs, hasmanyattrs, limited_iterable, shuffled, slice_between, stalin_sort
+	from .language import apostrophe_s, make_plural, nth, plural_s
+	from .misspellings import asert, trei
+	from .null import Null, Undefined, UniqueDefault
+	from .other import intbool
+	from .overload import Overload, OverloadMeta, overload
+	from .path import path
+	from .print import FMT_BRACKETS, FMTC, alert, fmt_iterable, gay, print_block, print_iterable
+	from .regex import RegexPreset
+	from .result import Result, ResultUnwrappedErrOnValueError, ResultUnwrappedOnErrorError
+	from .run import RunSACAble, run_sac
+	from .sdb import ShelveDB
+	from .string import SlashableString, commafy, custom_zfill, get_token, join_urlstr, polaris_progressbar
+	from .temp import temp_file
+	from .terminal import terminal
+	from .test_ import ass, raises, rass
+	from .timestr import TStr, t_day, t_hour, t_minute, t_week, t_year, timestr
+	from .typehints import force_keyword_only_typehints
+	from .uptime import Uptime
+	from .void import alambda, araiser, avoid, raiser, void
+
+	# fmt: off
+	__all__ = [  # noqa: RUF022
+		"console",                            # console
+
+		"autorun", "convert",                 # decorator
+
+		"dict_zip", "merge_dicts",            # dict
+
+		"extract_error", "extract_traceback", # extract_error
+
+		"getch",                              # getch
+
+		"batched", "cut_at", "shuffled",      # iterable
+		"limited_iterable", "getattr_queue",
+		"Or", "hasmanyattrs", "getmanyattrs",
+
+		"codeblock", "uncodeblock",           # markdown
+
+		"asert", "trei",                      # misspellings
+
+		"Null", "Undefined", "UniqueDefault", # null
+
+		"commafy",                            # string
+
+		"nth", "make_plural",                 # language
+		"apostrophe_s",
+
+		"hex", "float2int",                   # int
+
+		"intbool",                            # other
+
+		"dir2", "dir3",                       # dir
+
+		"print_iterable", "print_block",      # print
+		"fmt_iterable",
+
+		"RegexPreset",                        # regex
+
+		"run_sac",                            # run
+
+		"timestr",                            # timestr
+
+		"insist",                             # input
+
+		"able",                               # compare
+
+		"ShelveDB",                           # sdb
+
+		"Overload", "overload",               # overload
+
+		"BACKSLASH",                          # constants
+		"NEWLINE",
+		"CARR_RET",
+		"BACKSPACE",
+		"BACKTICK", "BACKTICKS",
+		"APOSTROPHE", "QUOTE",
+		"FAKE_PIPE",
+	]
+	# fmt: on
