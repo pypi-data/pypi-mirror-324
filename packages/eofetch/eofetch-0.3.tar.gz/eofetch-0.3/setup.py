03b39449624884cb9c19a572d654864710a40cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 05:06:08 +0000

    Add package eofetch/0.3/eofetch-0.3.tar.gz

---
+++ packages/eofetch/eofetch-0.3.tar.gz/eofetch-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='eofetch',
+    version='0.3',
+    description='Earth Observation Data Retrieval library',
+    url="https://github.com/stcorp/eofetch",
+    author='S[&]T',
+    license='BSD',
+    packages=['eofetch'],
+    entry_points={'console_scripts': ['eofetch = eofetch.__main__:main']},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=['boto3', 'pystac-client', 'requests']
+)
