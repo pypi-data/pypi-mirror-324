19543c59aae511d6a72932b74261a6f742a111ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:56 +0000

    Add package prefect/3.1.16.dev1/prefect-3.1.16.dev1.tar.gz

---
+++ packages/prefect/prefect-3.1.16.dev1.tar.gz/prefect-3.1.16.dev1/src/prefect/server/ui/assets/usePageTitle-NgGMZFeo.js
@@ -0,0 +1,2 @@
+import{i as a,bw as c,m as i}from"./index-Cq5Q7kWk.js";function u(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>i(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{u};
+//# sourceMappingURL=usePageTitle-NgGMZFeo.js.map
---
+++ packages/prefect/prefect-3.1.16.dev1.tar.gz/prefect-3.1.16.dev1/src/prefect/server/ui/assets/usePrefectApi-T_OCG9lz.js
@@ -0,0 +1,2 @@
+import{cD as r}from"./index-Cq5Q7kWk.js";import{p as e}from"./api-BoifTRS9.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-T_OCG9lz.js.map
