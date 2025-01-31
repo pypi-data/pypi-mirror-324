29fcba1abc1ba784f6117fcbebd322237ee7c016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 17:05:15 +0000

    Add package plot-twist-start/0.1/plot_twist_start-0.1.tar.gz

---
+++ packages/plot-twist-start/plot_twist_start-0.1.tar.gz/plot_twist_start-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='plot_twist_start',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='MTI',
+    author='Jonathan Dunn',
+    author_email='mmm@mmm.com',
+    description=''
+)
