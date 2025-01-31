192dbf7e7cdc37534dbf1ca01565d04fcf6134d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:05:06 +0000

    Add package ruff/0.9.4/ruff-0.9.4.tar.gz

---
+++ packages/ruff/ruff-0.9.4.tar.gz/ruff-0.9.4/crates/ruff_linter/src/rules/flake8_comprehensions/rules/unnecessary_list_comprehension_set.rs
@@ -0,0 +1,86 @@
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
+use ruff_macros::{derive_message_formats, ViolationMetadata};
+use ruff_python_ast as ast;
+use ruff_text_size::{Ranged, TextSize};
+
+use crate::checkers::ast::Checker;
+use crate::rules::flake8_comprehensions::fixes::{pad_end, pad_start};
+
+use super::helpers;
+
+/// ## What it does
+/// Checks for unnecessary list comprehensions.
+///
+/// ## Why is this bad?
+/// It's unnecessary to use a list comprehension inside a call to `set()`,
+/// since there is an equivalent comprehension for this type.
+///
+/// ## Examples
+/// ```python
+/// set([f(x) for x in foo])
+/// ```
+///
+/// Use instead:
+/// ```python
+/// {f(x) for x in foo}
+/// ```
+///
+/// ## Fix safety
+/// This rule's fix is marked as unsafe, as it may occasionally drop comments
+/// when rewriting the call. In most cases, though, comments will be preserved.
+#[derive(ViolationMetadata)]
+pub(crate) struct UnnecessaryListComprehensionSet;
+
+impl AlwaysFixableViolation for UnnecessaryListComprehensionSet {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        "Unnecessary list comprehension (rewrite as a set comprehension)".to_string()
+    }
+
+    fn fix_title(&self) -> String {
+        "Rewrite as a set comprehension".to_string()
+    }
+}
+
+/// C403 (`set([...])`)
+pub(crate) fn unnecessary_list_comprehension_set(checker: &mut Checker, call: &ast::ExprCall) {
+    let Some(argument) = helpers::exactly_one_argument_with_matching_function(
+        "set",
+        &call.func,
+        &call.arguments.args,
+        &call.arguments.keywords,
+    ) else {
+        return;
+    };
+    if !checker.semantic().has_builtin_binding("set") {
+        return;
+    }
+    if argument.is_list_comp_expr() {
+        let diagnostic = Diagnostic::new(UnnecessaryListComprehensionSet, call.range());
+        let fix = {
+            // Replace `set(` with `{`.
+            let call_start = Edit::replacement(
+                pad_start("{", call.range(), checker.locator(), checker.semantic()),
+                call.start(),
+                call.arguments.start() + TextSize::from(1),
+            );
+
+            // Replace `)` with `}`.
+            let call_end = Edit::replacement(
+                pad_end("}", call.range(), checker.locator(), checker.semantic()),
+                call.arguments.end() - TextSize::from(1),
+                call.end(),
+            );
+
+            // Delete the open bracket (`[`).
+            let argument_start =
+                Edit::deletion(argument.start(), argument.start() + TextSize::from(1));
+
+            // Delete the close bracket (`]`).
+            let argument_end = Edit::deletion(argument.end() - TextSize::from(1), argument.end());
+
+            Fix::unsafe_edits(call_start, [argument_start, argument_end, call_end])
+        };
+        checker.diagnostics.push(diagnostic.with_fix(fix));
+    }
+}
---
+++ packages/ruff/ruff-0.9.4.tar.gz/ruff-0.9.4/crates/ruff_linter/src/rules/pep8_naming/rules/invalid_argument_name.rs
@@ -0,0 +1,70 @@
+use ruff_python_ast::Parameter;
+
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, ViolationMetadata};
+use ruff_python_stdlib::str;
+use ruff_text_size::Ranged;
+
+use crate::rules::pep8_naming::settings::IgnoreNames;
+
+/// ## What it does
+/// Checks for argument names that do not follow the `snake_case` convention.
+///
+/// ## Why is this bad?
+/// [PEP 8] recommends that function names should be lower case and separated
+/// by underscores (also known as `snake_case`).
+///
+/// > Function names should be lowercase, with words separated by underscores
+/// > as necessary to improve readability.
+/// >
+/// > Variable names follow the same convention as function names.
+/// >
+/// > mixedCase is allowed only in contexts where that’s already the
+/// > prevailing style (e.g. threading.py), to retain backwards compatibility.
+///
+/// ## Example
+/// ```python
+/// def my_function(A, myArg):
+///     pass
+/// ```
+///
+/// Use instead:
+/// ```python
+/// def my_function(a, my_arg):
+///     pass
+/// ```
+///
+/// [PEP 8]: https://peps.python.org/pep-0008/#function-and-method-arguments
+#[derive(ViolationMetadata)]
+pub(crate) struct InvalidArgumentName {
+    name: String,
+}
+
+impl Violation for InvalidArgumentName {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let InvalidArgumentName { name } = self;
+        format!("Argument name `{name}` should be lowercase")
+    }
+}
+
+/// N803
+pub(crate) fn invalid_argument_name(
+    name: &str,
+    parameter: &Parameter,
+    ignore_names: &IgnoreNames,
+) -> Option<Diagnostic> {
+    if !str::is_lowercase(name) {
+        // Ignore any explicitly-allowed names.
+        if ignore_names.matches(name) {
+            return None;
+        }
+        return Some(Diagnostic::new(
+            InvalidArgumentName {
+                name: name.to_string(),
+            },
+            parameter.range(),
+        ));
+    }
+    None
+}
