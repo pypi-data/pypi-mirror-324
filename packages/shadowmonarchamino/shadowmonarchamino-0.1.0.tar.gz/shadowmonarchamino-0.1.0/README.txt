a8fd6a6728db5b96b02083ae346df8131116cb6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:32 +0000

    Add package shadowmonarchamino/0.1.0/shadowmonarchamino-0.1.0.tar.gz

---
+++ packages/shadowmonarchamino/shadowmonarchamino-0.1.0.tar.gz/shadowmonarchamino-0.1.0/README.txt
@@ -0,0 +1,8 @@
+shadowamino/
+├── shadowamino/
+│   ├── __init__.py
+│   └── other_module.py
+├── setup.py
+├── MANIFEST.in
+├── pyproject.toml
+└── README.md
---
+++ packages/shadowmonarchamino/shadowmonarchamino-0.1.0.tar.gz/shadowmonarchamino-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shadowmonarchamino",  # Your package name
+    version="0.1.0",  # Your package version
+    packages=find_packages(),  # Finds all the packages in your project
+    install_requires=[],  # List dependencies here
+    description="Aminoapps lib not all func working but I test some",
+    long_description_content_type="text/markdown",
+    url="https://github.com/6ndw/shadowamino",  # GitHub URL
+    author="Shadow",
+    author_email="tmhat2374@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
