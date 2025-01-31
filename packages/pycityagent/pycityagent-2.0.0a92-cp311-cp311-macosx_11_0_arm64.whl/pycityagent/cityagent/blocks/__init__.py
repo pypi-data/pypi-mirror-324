d7ab67a5f24e4534990acd09a48e5ee7d2135af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:05:05 +0000

    Add package pycityagent/2.0.0a92/pycityagent-2.0.0a92-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/pycityagent/pycityagent-2.0.0a92-cp311-cp311-macosx_11_0_arm64.whl/pycityagent/cityagent/blocks/__init__.py
@@ -0,0 +1,17 @@
+from .mobility_block import MobilityBlock
+from .cognition_block import CognitionBlock
+from .plan_block import PlanBlock
+from .needs_block import NeedsBlock
+from .social_block import SocialBlock
+from .economy_block import EconomyBlock
+from .other_block import OtherBlock
+
+__all__ = [
+    "MobilityBlock",
+    "CognitionBlock",
+    "PlanBlock",
+    "NeedsBlock",
+    "SocialBlock",
+    "EconomyBlock",
+    "OtherBlock",
+]
