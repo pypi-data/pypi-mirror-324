709c5b588ebdd1cfcba677e9c957381da7a52999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:04:58 +0000

    Add package allmetal3d/0.4.0/allmetal3d-0.4.0.tar.gz

---
+++ packages/allmetal3d/allmetal3d-0.4.0.tar.gz/allmetal3d-0.4.0/src/allmetal3d/utils/constants.py
@@ -0,0 +1,5 @@
+model_weights = {
+"identity":"/.local/share/allmetal3d/weights/identity_vacancy_geometry_model_2024-03-01_train_lessclasses_skipconnect_hyperparametertuned_geometry_identity_all_g09_300epoch_p01_epoch300.pth",
+"water":"/.local/share/allmetal3d/weights/water_0.5A_allmetal_one3dchannel_epoch9.pth",
+"metal":"/.local/share/allmetal3d/weights/metal_0.5A_allmetal_one3dchannel_16Abox_filter6_epoch6.pth"
+}
