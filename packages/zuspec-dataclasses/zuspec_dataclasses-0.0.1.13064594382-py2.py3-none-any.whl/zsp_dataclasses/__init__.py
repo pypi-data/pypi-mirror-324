767525bdbd70eabc2f9eb452499959ca56bf5f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 14:05:44 +0000

    Add package zuspec-dataclasses/0.0.1.13064594382/zuspec_dataclasses-0.0.1.13064594382-py2.py3-none-any.whl

---
+++ packages/zuspec-dataclasses/zuspec_dataclasses-0.0.1.13064594382-py2.py3-none-any.whl/zsp_dataclasses/__init__.py
@@ -0,0 +1,9 @@
+
+
+from .activity_stmts import *
+from .decorators import *
+from .claims_refs import *
+from .shared_stmts import *
+from .types import *
+from .core_lib import *
+from vsc_dataclasses.expr import *
