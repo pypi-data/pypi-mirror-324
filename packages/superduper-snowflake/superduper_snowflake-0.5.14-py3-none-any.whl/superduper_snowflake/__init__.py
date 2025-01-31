9d620008106191b33db15bcd4029b6c112f4d769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:33 +0000

    Add package superduper-snowflake/0.5.14/superduper_snowflake-0.5.14-py3-none-any.whl

---
+++ packages/superduper-snowflake/superduper_snowflake-0.5.14-py3-none-any.whl/superduper_snowflake/__init__.py
@@ -0,0 +1,9 @@
+from .vector_search import SnowflakeVectorSearcher as VectorSearcher
+from .data_backend import SnowflakeDataBackend as DataBackend
+
+__version__ = "0.5.14"
+
+__all__ = [
+    "VectorSearcher",
+    "DataBackend",
+]
