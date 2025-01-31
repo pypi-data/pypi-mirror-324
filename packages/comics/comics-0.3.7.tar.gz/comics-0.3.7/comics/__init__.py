ff62a1b023e86b7e904f544751e82f4625de45c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:23 +0000

    Add package comics/0.3.7/comics-0.3.7.tar.gz

---
+++ packages/comics/comics-0.3.7.tar.gz/comics-0.3.7/comics/__init__.py
@@ -0,0 +1,11 @@
+"""
+comics
+~~~~~~
+"""
+
+from comics.constants import directory
+from comics.exceptions import InvalidDateError, InvalidEndpointError
+from comics.gocomics import search
+
+
+__version__ = "0.3.7"
