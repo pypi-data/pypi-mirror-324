16e2f0af7af74599e1699092dd19bc0236323365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:05:04 +0000

    Add package fpcli/0.1.17/fpcli-0.1.17.tar.gz

---
+++ packages/fpcli/fpcli-0.1.17.tar.gz/fpcli-0.1.17/fpcli/connection/postgresql.py
@@ -0,0 +1,13 @@
+import psycopg2  # For PostgreSQL
+from config.settings import settings
+
+def get_postgresql_connection():
+    """Get the PostgreSQL connection using psycopg2."""
+    conn = psycopg2.connect(
+        host=settings.db_host,
+        port=settings.db_port,
+        dbname=settings.db_database,
+        user=settings.db_username,
+        password=settings.db_password
+    )
+    return conn
