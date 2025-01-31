56623af8b809ec6cbde01528d4440e74087fd552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:05:57 +0000

    Add package aminoshadow/0.1/aminoshadow-0.1.tar.gz

---
+++ packages/aminoshadow/aminoshadow-0.1.tar.gz/aminoshadow-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aminoshadow',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
