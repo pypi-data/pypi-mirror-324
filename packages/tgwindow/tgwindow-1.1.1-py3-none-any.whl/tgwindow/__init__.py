ad87ea16c5822a41f143b3c0adeb6261d29c59e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:04:51 +0000

    Add package tgwindow/1.1.1/tgwindow-1.1.1-py3-none-any.whl

---
+++ packages/tgwindow/tgwindow-1.1.1-py3-none-any.whl/tgwindow/__init__.py
@@ -0,0 +1,9 @@
+from tgwindow.middleware import WindowMiddleware
+from tgwindow.window.base import BaseWindow
+from tgwindow.window.wrapper import auto_window
+
+__all__ = [
+    "WindowMiddleware",
+    "BaseWindow",
+    "auto_window"
+]
