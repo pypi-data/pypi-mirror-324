9d1ce3a6b0119d86fba15f464d70012238dde8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:42 +0000

    Add package hec-python-library/0.6.9/hec_python_library-0.6.9-py3-none-any.whl

---
+++ packages/hec-python-library/hec_python_library-0.6.9-py3-none-any.whl/hec/__init__.py
@@ -0,0 +1,16 @@
+"""
+Package to provide native Python equivalent to HEC Java classes.
+"""
+
+__all__ = [
+    "hectime",
+    "timespan",
+    "interval",
+    "duration",
+    "location",
+    "unit",
+    "parameter",
+    "quality",
+    "timeseries",
+    "const",
+]
