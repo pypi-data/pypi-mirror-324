dfa1b7121839796b0088cbf4bd1cb7621ef218a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:57 +0000

    Add package licomp-proprietary/0.5.2/licomp_proprietary-0.5.2.tar.gz

---
+++ packages/licomp-proprietary/licomp_proprietary-0.5.2.tar.gz/licomp_proprietary-0.5.2/licomp_proprietary/config.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_proprietary_version = '0.5.2'
+my_supported_api_version = '0.5'
+
+repo = 'https://github.com/hesa/licomp-proprietary/'
+disclaimer = 'This data comes with not warranty. Using this data is the user\'s responsibility. Contact a lawyer for legal advice.'
+cli_name = 'licomp-proprietary'
+module_name = 'licomp_proprietary'
+module_url = 'https://github.com/hesa/licomp-proprietary'
+original_data_url = 'https://github.com/hesa/licomp-proprietary'
+
+description = """
+
+Simple command line tool to check compatibility between two licenses, given context
+
+"""
+
+epilog = """
+
+
+
+AUTHOR
+  Henrik Sandklef
+
+
+PROJECT SITE
+
+  https://github.com/hesa/licomp-proprietary
+
+REPORTING BUGS
+
+  File a ticket at https://github.com/hesa/licomp-proprietary/issues
+
+COPYRIGHT
+
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  licomp-proprietary is using the license compatibility matrix from elmat.
+"""
