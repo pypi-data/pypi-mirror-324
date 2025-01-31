e095adbee0c3a726ecc2d0c099c34c121b11de47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:06:07 +0000

    Add package flaskavel/1.11.9/flaskavel-1.11.9.tar.gz

---
+++ packages/flaskavel/flaskavel-1.11.9.tar.gz/flaskavel-1.11.9/flaskavel/luminate/installer/upgrade.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+
+class Upgrade:
+    """
+    A class responsible for handling the upgrade process of Flaskavel.
+
+    Methods
+    -------
+    execute() : None
+        Executes the upgrade process to install the latest version of Flaskavel.
+    """
+
+    @staticmethod
+    def execute():
+        """
+        Handle the --upgrade command to update Flaskavel to the latest version.
+
+        This method attempts to upgrade Flaskavel using the pip package manager.
+        It executes a command that installs the latest version of Flaskavel, ensuring
+        the application is up-to-date.
+
+        Raises
+        ------
+        ValueError
+            If the upgrade process fails or encounters any error during execution.
+
+        Notes
+        -----
+        The upgrade process uses `pip` via the command:
+        `python -m pip install --upgrade flaskavel`.
+        """
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "flaskavel"])
+        except subprocess.CalledProcessError as e:
+            raise ValueError(f"Upgrade failed: {e}")
+        except Exception as e:
+            raise ValueError(e)
