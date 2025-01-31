36580d073444c1552cdea7198934c8cfa1091ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:05:56 +0000

    Add package shadowamino/1.0.0/shadowamino-1.0.0-py3-none-any.whl

---
+++ packages/shadowamino/shadowamino-1.0.0-py3-none-any.whl/asyncshadow/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'shadowamino'
+__author__ = 'shadow'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2025 shadow'
+
+from .acm import ACM
+from .client import Client
+from .sub_client import SubClient
+from .socket import Callbacks, SocketHandler
+
