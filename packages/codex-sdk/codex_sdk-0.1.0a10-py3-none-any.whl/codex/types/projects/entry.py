85788f7e9cfee9e73e9d0a7ea5005a2a57771388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 06:06:08 +0000

    Add package codex-sdk/0.1.0a10/codex_sdk-0.1.0a10-py3-none-any.whl

---
+++ packages/codex-sdk/codex_sdk-0.1.0a10-py3-none-any.whl/codex/types/projects/entry.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+
+from ..._models import BaseModel
+
+__all__ = ["Entry"]
+
+
+class Entry(BaseModel):
+    created_at: datetime
+
+    question: str
+
+    id: Optional[str] = None
+
+    answer: Optional[str] = None
+
+    answered_at: Optional[datetime] = None
