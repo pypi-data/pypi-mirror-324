26abe3ec4824e08efe1c861d491e563091d4f717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:05:11 +0000

    Add package fake-bpy-module-2.91/20250130/fake_bpy_module_2.91-20250130-py3-none-any.whl

---
+++ packages/fake-bpy-module-2.91/fake_bpy_module_2.91-20250130-py3-none-any.whl/bpy/ops/cycles/__init__.pyi
@@ -0,0 +1,83 @@
+import typing
+import collections.abc
+import typing_extensions
+import bpy.types
+
+def add_aov(
+    override_context: bpy.types.Context | dict[str, typing.Any] = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Add an AOV pass
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any]
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+def denoise_animation(
+    override_context: bpy.types.Context | dict[str, typing.Any] = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    /,
+    *,
+    input_filepath: str = "",
+    output_filepath: str = "",
+):
+    """Denoise rendered animation sequence using current scene and view layer settings. Requires denoising data passes and output to OpenEXR multilayer files
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any]
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param input_filepath: Input Filepath, File path for image to denoise. If not specified, uses the render file path and frame range from the scene
+    :type input_filepath: str
+    :param output_filepath: Output Filepath, If not specified, renders will be denoised in-place
+    :type output_filepath: str
+    """
+
+def merge_images(
+    override_context: bpy.types.Context | dict[str, typing.Any] = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    /,
+    *,
+    input_filepath1: str = "",
+    input_filepath2: str = "",
+    output_filepath: str = "",
+):
+    """Combine OpenEXR multilayer images rendered with different sample ranges into one image with reduced noise
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any]
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param input_filepath1: Input Filepath, File path for image to merge
+    :type input_filepath1: str
+    :param input_filepath2: Input Filepath, File path for image to merge
+    :type input_filepath2: str
+    :param output_filepath: Output Filepath, File path for merged image
+    :type output_filepath: str
+    """
+
+def remove_aov(
+    override_context: bpy.types.Context | dict[str, typing.Any] = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Remove an AOV pass
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any]
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+def use_shading_nodes(
+    override_context: bpy.types.Context | dict[str, typing.Any] = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Enable nodes on a material, world or light
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any]
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
