f5d2f4f784ff4792c8e1616a78c080cae28d7fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:19 +0000

    Add package mongodb-controller/0.2.0/mongodb_controller-0.2.0-py3-none-any.whl

---
+++ packages/mongodb-controller/mongodb_controller-0.2.0-py3-none-any.whl/mongodb_controller/mongodb_connector.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+from .mongodb_consts import MONGODB_URI
+
+client = MongoClient(MONGODB_URI)
+db_test = client['database-test']
+collection_test = db_test['collection-hello']
