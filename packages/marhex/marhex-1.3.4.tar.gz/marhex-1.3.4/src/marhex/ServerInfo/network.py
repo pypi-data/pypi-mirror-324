e00b23db29c73407c9d771c729e6ced3569217ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 17:05:11 +0000

    Add package marhex/1.3.4/marhex-1.3.4.tar.gz

---
+++ packages/marhex/marhex-1.3.4.tar.gz/marhex-1.3.4/src/marhex/ServerInfo/network.py
@@ -0,0 +1,32 @@
+import requests
+
+
+def display_short_info():
+    print('Start Script')
+    all_data = requests.get("https://api.ipapi.is").json()
+    ip = all_data['ip']
+    country = all_data['location']['country']
+    city = all_data['location']['city']
+    is_proxy = all_data['is_proxy']
+    is_vpn = all_data['is_vpn']
+    local_time = all_data['location']['local_time']
+    local_time_unix = all_data['location']['local_time_unix']
+
+    print(f"""
+        ==================================================
+        IP                   =>      {ip}
+        Country              =>      {country}
+        City                 =>      {city}
+        ==================================================
+        is_Proxy             =>      {is_proxy}
+        is_VPN               =>      {is_vpn}
+        ==================================================
+        Local_Time           =>      {local_time}
+        Local_Time_Unix      =>      {local_time_unix}
+        ==================================================
+    """)
+
+    print('Finished Script')
+
+
+display_short_info()
