6846a2c6b58bc49511bf887f48f8d05e72cb03e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:48 +0000

    Add package Xaunsurn/0.1.1/xaunsurn-0.1.1.tar.gz

---
+++ packages/Xaunsurn/xaunsurn-0.1.1.tar.gz/xaunsurn-0.1.1/README.md
@@ -0,0 +1,9 @@
+# Xaunsurn
+
+**Interactively Integrating Imaginations**  
+Xaunsurn is an AI-based startup driving innovation and advanced solutions.
+
+## Installation
+
+```bash
+pip install Xaunsurn
---
+++ packages/Xaunsurn/xaunsurn-0.1.1.tar.gz/xaunsurn-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Xaunsurn",
+    version="0.1.1",
+    author="Parth",
+    author_email="contact@xaunsurn.com",
+    description="Xaunsurn - Interactively Integrating Imaginations.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://xaunsurn.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "xaunsurn = Xaunsurn.main:main",
+        ],
+    },
+)
