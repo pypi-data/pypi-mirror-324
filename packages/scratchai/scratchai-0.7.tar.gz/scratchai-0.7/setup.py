af671de62ba3a820f7fe56427141d03745e129d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:42 +0000

    Add package scratchai/0.7/scratchai-0.7.tar.gz

---
+++ packages/scratchai/scratchai-0.7.tar.gz/scratchai-0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.7'
+DESCRIPTION = 'Python librarie for machine learning'
+LONG_DESCRIPTION = 'Python librarie the provides useful tools for machine learning and data sience'
+
+
+# Setting up
+setup(
+        name="scratchai", 
+        version=VERSION,
+        author="Mohamed Marghoub",
+        author_email="<marghoubmohamed2@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy', 'joblib'], # prerequisites
+
+        keywords=['python', 'machine_learning', 'data_science'],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
