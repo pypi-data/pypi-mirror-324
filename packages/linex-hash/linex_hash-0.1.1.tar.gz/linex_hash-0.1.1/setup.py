de25e95f6284ace216ba0efaf14414a4bcaf7c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:06:04 +0000

    Add package linex-hash/0.1.1/linex_hash-0.1.1.tar.gz

---
+++ packages/linex-hash/linex_hash-0.1.1.tar.gz/linex_hash-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='linex_hash',
+    version='0.1.1',
+    description='A custom hash function library with salt, iterations, and XOR operations',
+    author='lopeklol',
+    url='https://github.com/lopeklol/linex_hash',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
