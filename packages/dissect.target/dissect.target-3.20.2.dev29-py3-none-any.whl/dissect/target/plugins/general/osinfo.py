ffcf0eff578f3688f4dbee82af22de65d4bc830c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:05:47 +0000

    Add package dissect.target/3.20.2.dev29/dissect.target-3.20.2.dev29-py3-none-any.whl

---
+++ packages/dissect.target/dissect.target-3.20.2.dev29-py3-none-any.whl/dissect/target/plugins/general/osinfo.py
@@ -0,0 +1,38 @@
+from typing import Callable, Generator, Iterator, Union
+
+from flow.record import GroupedRecord
+
+from dissect.target import plugin
+from dissect.target.helpers.record import TargetRecordDescriptor
+
+OSInfoRecord = TargetRecordDescriptor(
+    "generic/osinfo",
+    [
+        ("string", "name"),
+        ("string", "value"),
+    ],
+)
+
+
+class OSInfoPlugin(plugin.Plugin):
+    """Convenience plugin that wraps _os.* functions in records."""
+
+    def check_compatible(self) -> None:
+        pass
+
+    @plugin.export(record=OSInfoRecord)
+    def osinfo(self) -> Iterator[Union[OSInfoRecord, GroupedRecord]]:
+        """Yield grouped records with target OS info."""
+        for os_func in self.target._os.__functions__:
+            value = getattr(self.target._os, os_func)
+            record = OSInfoRecord(name=os_func, value=None, _target=self.target)
+            if isinstance(value, Callable) and isinstance(subrecords := value(), Generator):
+                try:
+                    yield GroupedRecord("generic/osinfo/grouped", [record] + list(subrecords))
+                except Exception:
+                    # Ignore exceptions triggered by functions
+                    # that cannot be executed in this context
+                    continue
+            else:
+                record.value = value
+                yield record
