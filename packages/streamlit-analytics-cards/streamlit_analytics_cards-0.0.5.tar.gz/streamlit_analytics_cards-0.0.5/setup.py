cb08abe1930141bc9a88011bb2400992b6e7077f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 07:04:38 +0000

    Add package streamlit-analytics-cards/0.0.5/streamlit_analytics_cards-0.0.5.tar.gz

---
+++ packages/streamlit-analytics-cards/streamlit_analytics_cards-0.0.5.tar.gz/streamlit_analytics_cards-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamlit-analytics-cards",
+    version="0.0.5",
+    description="A custom Streamlit component for analytics cards",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Natalie C",
+    url="https://github.com/natxc/streamlit-analytics-cards",
+    packages=find_packages(
+        include=["streamlit_analytics_cards", "streamlit_analytics_cards.*"]
+    ),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit >= 0.63",
+        "cachetools"
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.48.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    },
+)
