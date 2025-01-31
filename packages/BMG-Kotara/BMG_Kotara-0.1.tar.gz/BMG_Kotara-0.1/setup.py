f5108b395a2b031ba61328349688fabcb7e0b969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 05:05:28 +0000

    Add package BMG-Kotara/0.1/BMG_Kotara-0.1.tar.gz

---
+++ packages/BMG-Kotara/BMG_Kotara-0.1.tar.gz/BMG_Kotara-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="BMG_Kotara",  # имя модуля которое будет использоваться при устоновке модуля в будущем
+    version="0.1", # версия модуля, при каждом обновление(отправке) версию нужно увеличевать
+    description="мои инструменты", # описание можно не использовать
+    packages=["BMG_NFS"], #название папок которые надо залить
+    author_email="miniindra7@gmail.com",
+    zip_safe=False # пароль
+    # install_requires= для коректной работы модуля
+)
