c878abeef1da7caa27327bf7ad73c2180fb59fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 16:05:36 +0000

    Add package db-connector-kr/0.1.4/db_connector_kr-0.1.4.tar.gz

---
+++ packages/db-connector-kr/db_connector_kr-0.1.4.tar.gz/db_connector_kr-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="db-connector-kr",
+    version="0.1.4",
+    author="Sanggu Lim",
+    author_email="data.ai.lim39@gmail.com",
+    description="A Python library for connecting to various databases.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lim39/dbconnector",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "snowflake-connector-python",        
+        "pymysql"
+    ],
+    extras_require={
+        "postgres": ["psycopg2-binary"],  # Optional dependency
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
