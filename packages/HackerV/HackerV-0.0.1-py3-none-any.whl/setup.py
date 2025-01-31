af34606414c3f06512c86ac96095bdf83b9038ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:12 +0000

    Add package HackerV/0.0.1/HackerV-0.0.1-py3-none-any.whl

---
+++ packages/HackerV/HackerV-0.0.1-py3-none-any.whl/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="HackerV",
+    version="0.0.1",
+    author="student_v",
+    author_email="hackerstudentv@outlook.com",
+    description="china",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://vtongxue.github.io",
+    project_urls={'Bug tracker': 'https://vtongxue.github.io'},
+    classifiers=['Development Status :: 3 - Alpha', 'Programming Language :: Python :: 3', 'License :: OSI Approved :: MIT License', 'Operating System :: OS Independent'],
+    packages=[''],
+    python_requires=">=3.6",
+    install_requires = [],
+    entry_points=""
+)
