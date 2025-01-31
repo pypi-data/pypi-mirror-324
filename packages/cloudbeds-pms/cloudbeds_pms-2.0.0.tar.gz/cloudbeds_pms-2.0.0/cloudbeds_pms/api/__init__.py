2aaf67fd2a30212d78f6438b3bd0d7cc9b403647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:05:00 +0000

    Add package cloudbeds-pms/2.0.0/cloudbeds_pms-2.0.0.tar.gz

---
+++ packages/cloudbeds-pms/cloudbeds_pms-2.0.0.tar.gz/cloudbeds_pms-2.0.0/cloudbeds_pms/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from cloudbeds_pms.api.doorlock_keys_api import DoorlockKeysApi
+from cloudbeds_pms.api.doorlock_settings_api import DoorlockSettingsApi
+from cloudbeds_pms.api.housekeeping_api import HousekeepingApi
+
