621fdad4574d7f9c3d3bb5cbe582889a22fd2c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 04:04:58 +0000

    Add package binomcikit/0.0.1/binomcikit-0.0.1.tar.gz

---
+++ packages/binomcikit/binomcikit-0.0.1.tar.gz/binomcikit-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='binomcikit',
+    version='1.0.0',
+    author='BA Pranava,Vyasa R Rajeswaran',
+    author_email='pranavabaascaran@gmail.com,vyasarshi2308@gmail.com',
+    description='simple packge for ',
+    packages=find_packages(),  
+    install_requires=['numpy','pandas','scipy','plotnine'],
+)
