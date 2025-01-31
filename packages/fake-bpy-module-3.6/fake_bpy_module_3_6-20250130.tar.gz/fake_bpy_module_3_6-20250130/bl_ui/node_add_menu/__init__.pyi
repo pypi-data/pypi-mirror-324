bbb2d4e19973f0e5905b50b99e2b888265223b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 03:05:14 +0000

    Add package fake-bpy-module-3.6/20250130/fake_bpy_module_3_6-20250130.tar.gz

---
+++ packages/fake-bpy-module-3.6/fake_bpy_module_3_6-20250130.tar.gz/fake_bpy_module_3_6-20250130/bl_ui/node_add_menu/__init__.pyi
@@ -0,0 +1,15 @@
+import typing
+import collections.abc
+import typing_extensions
+
+def add_node_type(layout, node_type, *, label=None):
+    """Add a node type to a menu."""
+
+def add_simulation_zone(layout, label):
+    """Add simulation zone to a menu."""
+
+def draw_assets_for_catalog(layout, catalog_path): ...
+def draw_node_group_add_menu(context, layout):
+    """Add items to the layout used for interacting with node groups."""
+
+def draw_root_assets(layout): ...
