2a3eacae5428edffaabd4e8aaa543dcf306ab0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-31 14:04:54 +0000

    Add package licomp-dwheeler/0.5.1/licomp_dwheeler-0.5.1-py2.py3-none-any.whl

---
+++ packages/licomp-dwheeler/licomp_dwheeler-0.5.1-py2.py3-none-any.whl/licomp_dwheeler/config.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_dwheeler_version = '0.5.1'
+my_supported_api_version = '0.5'
+
+repo = 'https://github.com/hesa/licomp-dwheeler/'
+disclaimer = 'This information is believed to be correct, but is not legal advice; for formal legal advice, please consult an attorney.'
+cli_name = 'licomp-dwheeler'
+module_name = 'licomp_dwheeler'
+module_url = 'https://github.com/hesa/licomp-dwheeler/'
+original_data_url = 'https://dwheeler.com/essays/floss-license-slide.html'
+
+description = """
+
+Simple command line tool to check compatibility between two licenses, given context
+
+"""
+
+epilog = """
+
+
+
+AUTHOR
+  Henrik Sandklef
+
+
+PROJECT SITE
+
+  https://github.com/hesa/licomp-dwheeler
+
+REPORTING BUGS
+
+  File a ticket at https://github.com/hesa/licomp-dwheeler/issues
+
+COPYRIGHT
+
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  licomp-dwheeler is using the license compatibility matrix from David Wheeler's license compatibility graph (https://dwheeler.com/essays/floss-license-slide.html).
+"""
