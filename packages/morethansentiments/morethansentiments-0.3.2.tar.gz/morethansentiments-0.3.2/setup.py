d02a96a512e812c5bd911e1bb7ea24a898358b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:32 +0000

    Add package morethansentiments/0.3.2/morethansentiments-0.3.2.tar.gz

---
+++ packages/morethansentiments/morethansentiments-0.3.2.tar.gz/morethansentiments-0.3.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+
+
+setup(
+    name = 'morethansentiments',
+    version = '0.3.2',
+    description = 'An NLP python package for computing Boilerplate score and many other text features.',
+    py_modules = ["morethansentiments"],
+    package_dir = {'':'src'},
+#     package = ['morethansentiments'],
+    author = 'Jinhang Jiang, Karthik Srinivasan',
+    author_email = 'jinhang@asu.edu',
+    long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    long_description_content_type = "text/markdown",
+    url='https://github.com/jinhangjiang/morethansentiments',
+    include_package_data=True,
+    
+    
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: BSD License",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Topic :: Text Processing',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
+    ],
+    
+    install_requires = [
+        'tqdm >= 4.59.0',
+        'spacy >= 3.3.0',
+        'pandas >= 1.2.4',
+        'nltk >= 3.6.1',
+    ],
+    
+    keywords = ['Text Mining', 'Data Science', 'Natural Language Processing', 'Accounting'],
+    
+)
