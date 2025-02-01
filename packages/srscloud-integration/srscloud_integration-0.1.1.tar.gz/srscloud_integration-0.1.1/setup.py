c69130bd3cbd02a2533ae2a59158749ca65d3383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:48 +0000

    Add package srscloud-integration/0.1.1/srscloud_integration-0.1.1.tar.gz

---
+++ packages/srscloud-integration/srscloud_integration-0.1.1.tar.gz/srscloud_integration-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="srscloud-integration",
+    version="0.1.1",  # Nova versão para sobrescrever a antiga
+    author="REMOVIDO",
+    author_email="removido@example.com",
+    description="Este pacote foi descontinuado e não deve ser utilizado.",
+    long_description="Este pacote foi removido. Entre em contato para mais informações.",
+    long_description_content_type="text/plain",
+    url="https://github.com/empresa/srscloud-integration",
+    packages=[],
+    install_requires=[],
+)
