23d061d8f46047d32e88e6c7cc6eee6e0ee3bcd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:37 +0000

    Add package rxdjango/0.0.18/rxdjango-0.0.18.tar.gz

---
+++ packages/rxdjango/rxdjango-0.0.18.tar.gz/rxdjango-0.0.18/rxdjango/exceptions.py
@@ -0,0 +1,14 @@
+class UnknownProperty(Exception):
+    pass
+
+class AnchorDoesNotExist(Exception):
+    pass
+
+class UnauthorizedError(Exception):
+    pass
+
+class ForbiddenError(Exception):
+    pass
+
+class RxDjangoBug(Exception):
+    pass
