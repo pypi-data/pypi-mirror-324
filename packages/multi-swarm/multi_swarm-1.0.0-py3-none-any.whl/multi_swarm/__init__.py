8c6ca9bf3e7f5075f7da6dc5ac996cd1106e9537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:09:17 +0000

    Add package multi-swarm/1.0.0/multi_swarm-1.0.0-py3-none-any.whl

---
+++ packages/multi-swarm/multi_swarm-1.0.0-py3-none-any.whl/multi_swarm/__init__.py
@@ -0,0 +1,12 @@
+from multi_swarm.core import Agency, BaseAgent as Agent
+from multi_swarm.ceo.ceo_agent import CEOAgent
+from multi_swarm.trends_analyst.trends_analyst import TrendsAnalyst
+
+__version__ = "0.1.2"
+
+__all__ = [
+    "Agency",
+    "Agent",
+    "CEOAgent",
+    "TrendsAnalyst",
+] 
