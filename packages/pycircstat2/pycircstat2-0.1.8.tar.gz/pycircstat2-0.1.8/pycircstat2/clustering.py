5f8c95353c0e6a86979eff5a35ee95cebe658258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:25 +0000

    Add package pycircstat2/0.1.8/pycircstat2-0.1.8.tar.gz

---
+++ packages/pycircstat2/pycircstat2-0.1.8.tar.gz/pycircstat2-0.1.8/pycircstat2/clustering.py
@@ -0,0 +1,186 @@
+from typing import Union
+
+import numpy as np
+from scipy.stats import vonmises
+
+from .descriptive import circ_kappa, circ_mean_and_r
+from .utils import data2rad
+
+
+class MoVM:
+    """
+    Mixture of von Mises Clustering
+    """
+
+    def __init__(
+        self,
+        burnin: int = 30,
+        n_clusters: int = 5,
+        n_iters: int = 100,
+        n_intervals: int = 360,
+        unit: str = "degree",
+        random_seed: int = 2046,
+        threshold: float = 1e-16,
+    ):
+        self.burnin = (
+            burnin  # wait untill burinin step of iterations for convergence
+        )
+        self.threshold = threshold  # convergence threshold
+        self.n_clusters = n_clusters  # number of clusters to estimate
+        self.n_iters = n_iters  # maximum number of iterations for EM
+        self.n_intervals = n_intervals  # for data conversion
+        self.unit = unit  # for data conversion
+        self.random_seed = random_seed
+        self.converged = False  # place holder
+
+    def _initialize(
+        self,
+        x: np.ndarray,
+        n_clusters_init: int,
+    ):
+        n = len(x)  # number of samples
+        p = (
+            np.ones(n_clusters_init) / n_clusters_init
+        )  # initial cluster probability
+        z = np.random.choice(
+            np.arange(n_clusters_init), size=n
+        )  # initial labels
+        m, r = map(
+            np.array,
+            zip(*[circ_mean_and_r(x[z == i]) for i in range(n_clusters_init)]),
+        )  # initial means and resultant vector lengths
+        kappa = np.array(
+            [circ_kappa(r=r[i]) for i in range(n_clusters_init)]
+        )  # initial kappa (without correction by hard-coding a larger enough n)
+
+        return m, kappa, p
+
+    def fit(self, x: np.ndarray, verbose: Union[bool, int] = 0):
+        # seed
+        np.random.seed(self.random_seed)
+
+        # meta
+        self.x = x
+        self.x_rad = x_rad = (
+            x if self.unit == "radian" else data2rad(x, self.n_intervals)
+        )
+        self.n = n = len(x)
+
+        # init
+        m, kappa, p = self._initialize(x_rad, self.n_clusters)
+
+        # EM
+        if verbose:
+            print(f"Iter".ljust(10) + f"nLL")
+        self.nLL = np.ones(self.n_iters) * np.nan
+        for i in range(self.n_iters):
+            # E step
+            gamma = self.compute_gamma(x_rad=self.x_rad, p=p, m=m, kappa=kappa)
+            gamma_normed = gamma / np.sum(gamma, axis=0)
+
+            # M step
+            p = (
+                np.sum(gamma_normed, axis=1)
+                / np.sum(gamma_normed, axis=1).sum()
+            )
+            m, r = map(
+                np.array,
+                zip(
+                    *[
+                        circ_mean_and_r(alpha=x_rad, w=gamma_normed[i])
+                        for i in range(self.n_clusters)
+                    ]
+                ),
+            )
+            kappa = np.array(
+                [circ_kappa(r=r[i]) for i in range(self.n_clusters)]
+            )
+
+            nLL = self.compute_nLL(gamma)
+            self.nLL[i] = nLL
+
+            if verbose:
+                if i % int(verbose) == 0:
+                    print(f"{i}".ljust(10) + f"{nLL:.03f}")
+
+            if (
+                i > self.burnin
+                and np.abs(self.nLL[i] - self.nLL[i - 1]) < self.threshold
+            ):
+                self.nLL = self.nLL[~np.isnan(self.nLL)]
+                self.converged = True
+                self.converged_iters = len(self.nLL)
+
+                if verbose:
+                    print(f"Converged at iter {i}. Final nLL = {nLL:.3f}\n")
+                break
+        else:
+            if verbose:
+                print(
+                    f"Reached max iter {self.n_iters}. Final nLL = {nLL:.3f}\n"
+                )
+
+        # save results
+        self.m = m  # cluster means
+        self.r = r  # cluster mean resultant vectors
+        self.p = p  # cluster probabilities
+        self.kappa = kappa  # cluster kappas
+        self.gamma = self.compute_gamma(
+            x_rad=self.x_rad, p=p, m=m, kappa=kappa
+        )  # update gamma one last time
+        self.labels = self.gamma.argmax(axis=0)
+
+    def compute_gamma(
+        self,
+        x_rad: np.ndarray,
+        p: np.ndarray,
+        m: np.ndarray,
+        kappa: np.ndarray,
+    ):
+        gamma = np.vstack(
+            [
+                p[i] * vonmises.pdf(x_rad, kappa=kappa[i], loc=m[i])
+                for i in range(self.n_clusters)
+            ]
+        )
+        return gamma
+
+    def compute_nLL(self, gamma: np.ndarray):
+        nLL = -np.sum(np.log(np.sum(gamma, axis=0) + 1e-16))
+        return nLL
+
+    def compute_BIC(self):
+        nLL = self.compute_nLL(self.gamma)
+        nparams = self.n_clusters * 3 - 1  # n_means + n_kappas + (n_ps - 1)
+        bic = 2 * nLL + np.log(self.n) * nparams
+
+        return bic
+
+    def predict_density(
+        self,
+        x: np.ndarray = None,
+        unit: Union[str, None] = None,
+        n_intervals: Union[float, int, None] = None,
+    ):
+        unit = self.unit if unit is None else unit
+        n_intervals = self.n_intervals if n_intervals is None else n_intervals
+
+        if x is None:
+            x = np.linspace(0, 2 * np.pi, 100)
+
+        x_rad = x if unit == "radian" else data2rad(x, n_intervals)
+
+        d = [
+            self.p[i] * vonmises.pdf(x_rad, kappa=self.kappa[i], loc=self.m[i])
+            for i in range(self.n_clusters)
+        ]
+        return np.sum(d, axis=0)
+
+    def predict(self, x: np.ndarray):
+        x_rad = x if self.unit == "radian" else data2rad(x, self.n_intervals)
+
+        gamma = self.compute_gamma(
+            x_rad=x_rad, p=self.p, m=self.m, kappa=self.kappa
+        )
+
+        return gamma.argmax(axis=0)
---
+++ packages/pycircstat2/pycircstat2-0.1.8.tar.gz/pycircstat2-0.1.8/pycircstat2/visualization.py
@@ -0,0 +1,299 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import ticker
+
+from pycircstat2.descriptive import (
+    compute_smooth_params,
+    nonparametric_density_estimation,
+)
+
+
+def circ_plot(
+    circ_data,
+    ax=None,
+    **kwargs,
+):
+
+    # process kwargs and assign defaults
+    outward = kwargs.pop("outward", True)
+    figsize = kwargs.pop("figsize", (5, 5))
+    projection = kwargs.pop("projection", "polar")
+    marker = kwargs.pop("marker", "o")
+    marker_color = kwargs.pop("marker_color", "black")
+    marker_size = kwargs.pop("marker_size", 10)
+
+    bins = kwargs.pop("bins", 12)
+
+    plot_counts = kwargs.pop("plot_counts", False)
+    # plot_rlabel = kwargs.pop("plot_rlabel", False)
+    plot_grid = kwargs.pop("plot_grid", True)
+    plot_spine = kwargs.pop("plot_spine", False)
+    plot_axis = kwargs.pop("plot_axis", True)
+
+    plot_density = kwargs.pop("plot_density", True)
+    plot_rose = kwargs.pop("plot_rose", True)
+
+    plot_mean = kwargs.pop("plot_mean", True)
+    if np.isclose(circ_data.r, 0):
+        plot_mean = False
+        # print("Mean is not plotted because `r` is close to 0")
+    mean_kwargs = kwargs.pop(
+        "mean_kwargs", {"color": "black", "linestyle": "-", "kind": "arrow"}
+    )
+
+    plot_mean_ci = kwargs.pop("plot_mean_ci", True)
+    if not hasattr(circ_data, "mean_ub") or np.isnan(circ_data.mean_ub):
+        plot_mean_ci = False
+        # print("Mean CI is not plotted because it is not computed")
+
+    plot_median = kwargs.pop("plot_median", True)
+    if plot_median and np.isnan(circ_data.median):
+        plot_median = False
+        # print("Median is not plotted because `median` is nan")
+
+    median_kwargs = kwargs.pop(
+        "median_kwargs", {"color": "black", "linestyle": "dotted"}
+    )
+
+    plot_median_ci = kwargs.pop("plot_median_ci", True)
+    if not hasattr(circ_data, "median_ub") or np.isnan(circ_data.median_ub):
+        plot_median_ci = False
+        # print("Median CI is not plotted because it is not computed.")
+
+    zero_location = kwargs.pop("zero_location", "N")
+    clockwise = kwargs.pop("clockwise", -1)
+    r_max_scatter = kwargs.pop("r_max_scatter", 1)
+    rticks = kwargs.pop("rticks", [0, 1])
+    rlim_max = kwargs.pop("rlim_max", None)
+
+    # check axes
+    if ax is None:
+        fig, ax = plt.subplots(
+            figsize=figsize,
+            subplot_kw={"projection": projection},
+            layout="constrained",
+        )
+
+    # plot
+    if not circ_data.grouped:
+
+        # plot scatter
+        alpha, counts = np.unique(circ_data.alpha.round(3), return_counts=True)
+        alpha = np.repeat(alpha, counts)
+
+        if outward:
+            radii = np.hstack(
+                [
+                    r_max_scatter
+                    + 0.05
+                    + np.arange(0, 0.05 * int(f), 0.05)[: int(f)]
+                    for f in counts
+                ]
+            )
+        else:
+            radii = np.hstack(
+                [
+                    r_max_scatter
+                    - 0.05
+                    - np.arange(0, 0.05 * int(f), 0.05)[: int(f)]
+                    for f in counts
+                ]
+            )
+        ax.scatter(
+            alpha, radii, color=marker_color, marker=marker, s=marker_size
+        )
+
+        # plot density
+        if plot_density:  # and not np.isclose(circ_data.r, 0):
+
+            kwargs_density = kwargs.pop("kwargs_density", {})
+            density_method = kwargs_density.pop("method", "nonparametric")
+            density_color = kwargs_density.pop("color", "black")
+            density_linestyle = kwargs_density.pop("linestyle", "-")
+
+            if density_method == "nonparametric":
+                h0 = kwargs_density.pop(
+                    "h0", compute_smooth_params(circ_data.r, circ_data.n)
+                )
+                x, f = nonparametric_density_estimation(circ_data.alpha, h0)
+
+            elif density_method == "MoVM":
+
+                x = np.linspace(0, 2 * np.pi, 100)
+                f = circ_data.mixture_opt.predict_density(x=x, unit="radian")
+
+            else:
+                raise ValueError(
+                    f"`{kwargs_density['method']}` in `kwargs_density` is not supported."
+                )
+
+            # save density to circ_data
+            circ_data.density_x = x
+            circ_data.density_f = f
+            f_ = f + 1.05  # add the radius of the plotted circle
+            ax.plot(
+                x,
+                f_,
+                color=density_color,
+                linestyle=density_linestyle,
+            )
+            if rlim_max is None:
+                ax.set_ylim(0, f_.max())
+            else:
+                ax.set_ylim(0, rlim_max)
+        else:
+            if rlim_max is None:
+                ax.set_ylim(0, radii.max() + 0.025)
+            else:
+                ax.set_ylim(0, rlim_max)
+
+    # plot rose diagram
+    if plot_rose:
+
+        if not circ_data.grouped:
+            alpha = circ_data.alpha
+            w, beta = np.histogram(
+                alpha, bins=bins, range=(0, 2 * np.pi)
+            )  # np.histogram return bin edges
+            beta = 0.5 * (beta[:-1] + beta[1:])
+        else:
+            w = circ_data.w
+            beta = circ_data.alpha
+
+        w_sqrt = np.sqrt(w)
+        w_norm = w_sqrt / w_sqrt.max()
+
+        width = kwargs.pop("width", 2 * np.pi / len(beta))
+
+        bars = ax.bar(
+            beta,
+            w_norm,
+            width=width,
+            color="gray",
+            ec="black",
+            alpha=0.8,
+            bottom=0,
+            zorder=2,
+        )
+        if plot_counts:
+            for i, v in enumerate(w):
+
+                angle = rotation = beta[i].round(3)
+                if angle >= np.pi / 2 and angle < 3 * np.pi / 2:
+                    # alignment = "right"
+                    rotation = rotation + np.pi
+                # else:
+                #     alignment = "left"
+
+                if v != 0:
+                    ax.text(
+                        x=angle,
+                        y=bars[i].get_height() - 0.075,
+                        s=str(v),
+                        ha="center",
+                        va="center",
+                        rotation=rotation,
+                        rotation_mode="anchor",
+                        color="black",
+                    )
+
+        if circ_data.grouped and plot_density:
+            x = np.linspace(0, 2 * np.pi, 100)
+            f = circ_data.mixture_opt.predict_density(x=x, unit="radian") + 1
+            ax.plot(x, f, color="black", linestyle="-")
+            if rlim_max is None:
+                ax.set_ylim(0, f.max())
+            else:
+                ax.set_ylim(0, rlim_max)
+    else:
+        w = circ_data.w
+        rticks = [1]  # overwrite
+
+    if plot_mean:
+
+        radius = circ_data.r
+
+        ax.plot(
+            [0, circ_data.mean],
+            [0, radius],
+            color=mean_kwargs.pop("color", "black"),
+            ls=mean_kwargs.pop("linestyle", "-"),
+            label="mean",
+            zorder=5,
+        )
+
+    if plot_mean and plot_mean_ci:
+
+        if circ_data.mean_lb < circ_data.mean_ub:
+            x1 = np.linspace(circ_data.mean_lb, circ_data.mean_ub, num=50)
+        else:
+            x1 = np.linspace(
+                circ_data.mean_lb, circ_data.mean_ub + 2 * np.pi, num=50
+            )
+
+        # plot arc
+        ax.plot(
+            x1,
+            np.ones_like(x1) * radius,
+            ls="-",
+            color="black",
+            zorder=5,
+            lw=2,
+        )
+        # plot arc cap
+        ax.errorbar(x1[0], radius, yerr=0.03, capsize=0, color="black", lw=2)
+        ax.errorbar(x1[-1], radius, yerr=0.03, capsize=0, color="black", lw=2)
+
+    if plot_median:
+        ax.plot(
+            [0, circ_data.median],
+            [0, 0.95],
+            color=median_kwargs.pop("color", "black"),
+            ls=median_kwargs.pop("linestyle", "dotted"),
+            label="median",
+            zorder=5,
+        )
+
+    if plot_median and plot_median_ci:
+        if circ_data.median_lb < circ_data.median_ub:
+            x1 = np.linspace(circ_data.median_lb, circ_data.median_ub, num=50)
+        else:
+            x1 = np.linspace(
+                circ_data.median_lb, circ_data.median_ub + 2 * np.pi, num=50
+            )
+        # plot arc
+        ax.plot(
+            x1,
+            np.ones_like(x1) - 0.05,
+            ls="dotted",
+            color="black",
+            zorder=5,
+            lw=2,
+        )
+        # plot arc cap
+        ax.errorbar(x1[0], 0.95, yerr=0.03, capsize=0, color="black", lw=2)
+        ax.errorbar(x1[-1], 0.95, yerr=0.03, capsize=0, color="black", lw=2)
+
+    ax.set_theta_zero_location(zero_location)
+    ax.set_theta_direction(clockwise)
+    ax.grid(plot_grid)
+    ax.axis(plot_axis)
+    ax.spines["polar"].set_visible(plot_spine)
+    ax.set_rgrids(rticks, ["" for _ in range(len(rticks))], fontsize=16)
+
+    if circ_data.unit == "hour":
+        position_major = np.arange(0, 2 * np.pi, 2 * np.pi / 8)
+        position_minor = np.arange(0, 2 * np.pi, 2 * np.pi / 24)
+        labels = [f"{i}:00" for i in np.arange(0, circ_data.n_intervals, 3)]
+        ax.xaxis.set_major_locator(ticker.FixedLocator(position_major))
+        ax.xaxis.set_minor_locator(ticker.FixedLocator(position_minor))
+        ax.xaxis.set_major_formatter(ticker.FixedFormatter(labels))
+
+    gridlines = ax.yaxis.get_gridlines()
+    gridlines[-1].set_color("k")
+    gridlines[-1].set_linewidth(1)
+
+    if plot_mean or plot_median:
+        ax.legend(frameon=False)
+
+    return ax
