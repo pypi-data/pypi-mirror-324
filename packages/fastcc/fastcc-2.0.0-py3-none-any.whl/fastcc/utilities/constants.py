7b5b6a1cb3e0cae3fa73792d85e001969ebc22ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:08:56 +0000

    Add package fastcc/2.0.0/fastcc-2.0.0-py3-none-any.whl

---
+++ packages/fastcc/fastcc-2.0.0-py3-none-any.whl/fastcc/utilities/constants.py
@@ -0,0 +1,7 @@
+"""Module containing constant values."""
+from __future__ import annotations
+
+import typing
+
+#: Default timeout used to wait for a response to a request.
+MQTT_RESPONSE_TIMEOUT: typing.Final[float] = 5.0
