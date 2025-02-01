f376ba5f0539a00d4154f17d122a02dff3e8e4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:39 +0000

    Add package projectaria-tools/1.5.7/projectaria_tools-1.5.7-cp312-cp312-macosx_10_15_x86_64.whl

---
+++ packages/projectaria-tools/projectaria_tools-1.5.7-cp312-cp312-macosx_10_15_x86_64.whl/projectaria_tools/projects/ase.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+import projectaria_tools.core.calibration
+__all__ = ['get_ase_rgb_calibration']
+def get_ase_rgb_calibration(image_size: int = ...) -> projectaria_tools.core.calibration.CameraCalibration:
+    ...
