09ec0268119db2cce0e9777972059a7020b1b078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:08:56 +0000

    Add package SurakshaMitra/2.2.0/surakshamitra-2.2.0.tar.gz

---
+++ packages/SurakshaMitra/surakshamitra-2.2.0.tar.gz/surakshamitra-2.2.0/SurakshaMitra/email_validator.py
@@ -0,0 +1,9 @@
+import re
+
+def validate_email(email):
+    """
+    Validates an email address using a regex pattern.
+    """
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return bool(re.match(pattern, email))
+
