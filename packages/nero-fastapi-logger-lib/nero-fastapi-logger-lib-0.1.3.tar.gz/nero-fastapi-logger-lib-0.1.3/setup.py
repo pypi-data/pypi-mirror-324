bf5a43449a955704a34fb074740e7cbd01831241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 16:05:08 +0000

    Add package nero-fastapi-logger-lib/0.1.3/nero-fastapi-logger-lib-0.1.3.tar.gz

---
+++ packages/nero-fastapi-logger-lib/nero-fastapi-logger-lib-0.1.3.tar.gz/nero-fastapi-logger-lib-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nero-fastapi-logger-lib",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "colorlog",
+        "python-json-logger",
+    ],
+    author="Tanathap Roongpipat",
+    author_email="tanathep.min@gmail.com",
+    description="A logging library for FastAPI with request_id tracking",
+    url="https://github.com/neroswords/nero-fastapi-logger-lib.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
