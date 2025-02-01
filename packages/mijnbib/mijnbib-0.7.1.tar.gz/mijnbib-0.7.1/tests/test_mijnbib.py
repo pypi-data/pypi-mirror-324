7cfa5da59716278c37d27007cf6257294114bd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:35 +0000

    Add package mijnbib/0.7.1/mijnbib-0.7.1.tar.gz

---
+++ packages/mijnbib/mijnbib-0.7.1.tar.gz/mijnbib-0.7.1/tests/test_mijnbib.py
@@ -0,0 +1,28 @@
+def test_mijnbib_available_imports():
+    import mijnbib
+
+    # make sure we don't expose too few, or too much
+    imps = [i for i in dir(mijnbib) if not i.startswith("__")]
+    assert set(imps) == set(
+        [
+            "MijnBibliotheek",
+            "Loan",
+            "Reservation",
+            "Account",
+            "ItemAccessError",
+            "AuthenticationError",
+            "CanNotConnectError",
+            "ExtendLoanError",
+            "IncompatibleSourceError",
+            "MijnbibError",
+            "TemporarySiteError",
+            # things we actually don't want to have exported
+            "const",
+            "errors",
+            "mijnbibliotheek",
+            "parsers",
+            "models",
+            "login_handlers",
+            "importlib",
+        ]
+    )
