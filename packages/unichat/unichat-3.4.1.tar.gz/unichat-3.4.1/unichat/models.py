8c28bddde099ec5c146530578dcc38a57306520d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 16:04:58 +0000

    Add package unichat/3.4.1/unichat-3.4.1.tar.gz

---
+++ packages/unichat/unichat-3.4.1.tar.gz/unichat-3.4.1/unichat/models.py
@@ -0,0 +1,68 @@
+MODELS_LIST =  {
+    "anthropic_models": [
+        "claude-3-5-haiku-latest",
+        "claude-3-5-sonnet-latest",
+        "claude-3-opus-latest"
+    ],
+    "mistral_models": [
+        "mistral-small-latest",
+        "mistral-large-latest",
+        "codestral-latest"
+    ],
+    "openai_models": [
+        "chatgpt-4o-latest",
+        "gpt-4o-mini",
+        "gpt-4o",
+        "gpt-4o-2024-11-20",
+        "o1-mini",
+        "o1-preview",
+        "o1",
+        "o3-mini"
+    ],
+    "grok_models": [
+        "grok-beta"
+    ],
+    "gemini_models": [
+        "gemini-1.5-flash",
+        "gemini-1.5-pro",
+        "gemini-2.0-flash-exp",
+        "gemini-2.0-flash-thinking-exp",
+        "gemini-exp-1206"
+    ],
+    "deepseek_models": [
+        "deepseek-chat",
+        "deepseek-reasoner"
+    ],
+    "alibaba_models": [
+        "qwen-turbo-latest",
+        "qwen-plus-latest",
+        "qwen-max-latest"
+    ]
+}
+MODELS_MAX_TOKEN =  {
+    "chatgpt-4o-latest": 16384,
+    "gpt-4o-mini": 16384,
+    "gpt-4o": 16384,
+    "gpt-4o-2024-11-20": 16384,
+    "o1-mini": 65536,
+    "o1-preview": 32768,
+    "o1": 100000,
+    "o3-mini": 100000,
+    "mistral-small-latest": 32000,
+    "mistral-large-latest": 128000,
+    "codestral-latest": 256000,
+    "claude-3-5-haiku-latest": 8192,
+    "claude-3-5-sonnet-latest": 8192,
+    "claude-3-opus-latest": 4096,
+    "grok-beta": 131072,
+    "gemini-1.5-flash": 8192,
+    "gemini-1.5-pro": 8192,
+    "gemini-2.0-flash-exp": 8192,
+    "gemini-2.0-flash-thinking-exp": 8192,
+    "gemini-exp-1206": 8192,
+    "deepseek-chat": 8192,
+    "deepseek-reasoner": 8192,
+    "qwen-turbo-latest": 8192,
+    "qwen-plus-latest": 8192,
+    "qwen-max-latest": 8192
+}
