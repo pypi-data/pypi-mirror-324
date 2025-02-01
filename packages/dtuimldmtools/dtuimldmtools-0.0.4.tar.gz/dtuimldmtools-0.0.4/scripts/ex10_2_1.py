75741cae70c34c397cfb6a33aa5833c834ebd446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:06:10 +0000

    Add package dtuimldmtools/0.0.4/dtuimldmtools-0.0.4.tar.gz

---
+++ packages/dtuimldmtools/dtuimldmtools-0.0.4.tar.gz/dtuimldmtools-0.0.4/scripts/ex10_2_1.py
@@ -0,0 +1,42 @@
+# exercise 10.2.1
+import importlib_resources
+from matplotlib.pyplot import figure, show
+from scipy.cluster.hierarchy import dendrogram, fcluster, linkage
+from scipy.io import loadmat
+
+from dtuimldmtools import clusterplot
+
+filename = importlib_resources.files("dtuimldmtools").joinpath("data/synth1.mat")
+
+# Load Matlab data file and extract variables of interest
+mat_data = loadmat(filename)
+X = mat_data["X"]
+y = mat_data["y"].squeeze()
+attributeNames = [name[0] for name in mat_data["attributeNames"].squeeze()]
+classNames = [name[0][0] for name in mat_data["classNames"]]
+N, M = X.shape
+C = len(classNames)
+
+
+# Perform hierarchical/agglomerative clustering on data matrix
+Method = "single"
+Metric = "euclidean"
+
+Z = linkage(X, method=Method, metric=Metric)
+
+# Compute and display clusters by thresholding the dendrogram
+Maxclust = 4
+cls = fcluster(Z, criterion="maxclust", t=Maxclust)
+figure(1)
+clusterplot(X, cls.reshape(cls.shape[0], 1), y=y)
+
+# Display dendrogram
+max_display_levels = 6
+figure(2, figsize=(10, 4))
+dendrogram(
+    Z, truncate_mode="level", p=max_display_levels, color_threshold=Z[-Maxclust + 1, 2]
+)
+
+show()
+
+print("Ran Exercise 10.2.1")
