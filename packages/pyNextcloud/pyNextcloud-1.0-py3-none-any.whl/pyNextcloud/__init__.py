193819c4688b738bd70eddb5defb8b1433e39a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 16:04:58 +0000

    Add package pyNextcloud/1.0/pyNextcloud-1.0-py3-none-any.whl

---
+++ packages/pyNextcloud/pyNextcloud-1.0-py3-none-any.whl/pyNextcloud/__init__.py
@@ -0,0 +1,22 @@
+
+from .nextcloud import (
+    UploadFile,
+    DownloadFile,
+    DownloadFile_URL,
+    DirectoryExists_Check,
+    CreateDirectory,
+    RenamePath,
+    DeletePath,
+    UploadFolder
+)
+
+__all__ = [
+    'UploadFile',
+    'DownloadFile',
+    'DownloadFile_URL',
+    'DirectoryExists_Check',
+    'CreateDirectory',
+    'RenamePath',
+    'DeletePath',
+    'UploadFolder'
+]
