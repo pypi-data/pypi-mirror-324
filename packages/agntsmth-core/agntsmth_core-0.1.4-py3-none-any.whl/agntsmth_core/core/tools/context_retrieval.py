d78c3abd1f153916b9c7a00617ecbbbc38667530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 16:05:14 +0000

    Add package agntsmth-core/0.1.4/agntsmth_core-0.1.4-py3-none-any.whl

---
+++ packages/agntsmth-core/agntsmth_core-0.1.4-py3-none-any.whl/agntsmth_core/core/tools/context_retrieval.py
@@ -0,0 +1,55 @@
+import yaml
+import json
+from typing import Annotated, Type, Any, List, Dict
+from pydantic import BaseModel, Field
+
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain.tools.retriever import create_retriever_tool
+from langchain_core.tools import BaseTool
+from langchain_core.vectorstores import VectorStoreRetriever
+
+from ..utls import RetrieverFactory, log
+
+
+class RetrieveAdditionalContextToolSchema(BaseModel):
+    query: str = Field(description="The query to search for.")
+
+
+class RetrieveAdditionalContextTool(BaseTool):
+    """
+    This tool queries a vectorstore for additional information.
+    """
+
+    name: str = "retrieve_additional_context"
+    description: str = "Queries a vectorstore for additional information."
+    args_schema: Type[BaseModel] = RetrieveAdditionalContextToolSchema
+    retriever: Any = None
+
+    def __init__(
+        self,
+        retriever: VectorStoreRetriever = None,
+        /,
+        **data: Any,
+    ):
+        """
+        An agent tool for fetching addition context from a vectorstore.
+        """
+
+        super().__init__(**data)
+
+        self.retriever = retriever
+        if self.retriever is None:
+            self.retriever = RetrieverFactory.create(collection_name=kwargs["collection_name"])
+
+    def _run(self, query: str) -> str:
+        """Use the tool"""
+        log(f"{self.name}. START: query: {query}")
+        resp = self.retriever.invoke(query)
+        log(f"{self.name}. END: query: {query}")
+        return resp
+
+    async def _arun(self, query: str) -> str:
+        """Use the tool"""
+        return self._run(query)
