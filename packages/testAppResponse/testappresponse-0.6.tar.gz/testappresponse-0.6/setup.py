abd411b450f0db8abe5e798304cda5f7966843c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:46 +0000

    Add package testAppResponse/0.6/testappresponse-0.6.tar.gz

---
+++ packages/testAppResponse/testappresponse-0.6.tar.gz/testappresponse-0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testAppResponse',
+    version='0.6',
+    description='Manage your responses',
+    packages=find_packages(), 
+    author_email='manojan.mano@gmail.com',
+)
