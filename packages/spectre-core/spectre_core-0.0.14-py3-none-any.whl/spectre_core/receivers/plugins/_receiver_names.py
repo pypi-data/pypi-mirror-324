17225568f30a3f5f9ea6fabf80380a98c1cd60fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:12 +0000

    Add package spectre-core/0.0.14/spectre_core-0.0.14-py3-none-any.whl

---
+++ packages/spectre-core/spectre_core-0.0.14-py3-none-any.whl/spectre_core/receivers/plugins/_receiver_names.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from enum import Enum
+
+class ReceiverName(Enum):  
+    """A `spectre` supported receiver.
+    
+    :ivar RSP1A: SDRPlay RSP1A
+    :ivar RSPDUO: SDRPlay RSPduo
+    :ivar TEST: `spectre` test receiver.
+    """
+    RSP1A  = "rsp1a"
+    RSPDUO = "rspduo"
+    TEST   = "test"
