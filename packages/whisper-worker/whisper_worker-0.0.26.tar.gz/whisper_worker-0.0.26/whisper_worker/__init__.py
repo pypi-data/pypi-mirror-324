826a08e47d397622fb37e4cd09cdf55f222d976a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:34 +0000

    Add package whisper-worker/0.0.26/whisper_worker-0.0.26.tar.gz

---
+++ packages/whisper-worker/whisper_worker-0.0.26.tar.gz/whisper_worker-0.0.26/whisper_worker/__init__.py
@@ -0,0 +1,6 @@
+from .logging_config import LoggingConfig
+from .mic import MicSettings, Mic
+from .recording_device import RecordingDevice
+from .settings import Settings
+from .transcription import TranscribeSettings
+from .whisper_worker import WhisperWorker
