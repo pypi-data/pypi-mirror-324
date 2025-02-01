485dc8d1c800c05a2f2c7dab34b9c4cd79db0ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:14 +0000

    Add package pygpt-net/2.5.0/pygpt_net-2.5.0-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.5.0-py3-none-any.whl/pygpt_net/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2025.01.31 22:00:00                  #
+# ================================================== #
+
+__author__ = "Marcin Szczygliński"
+__copyright__ = "Copyright 2025, Marcin Szczygliński"
+__credits__ = ["Marcin Szczygliński"]
+__license__ = "MIT"
+__version__ = "2.5.0"
+__build__ = "2025.01.31"
+__maintainer__ = "Marcin Szczygliński"
+__github__ = "https://github.com/szczyglis-dev/py-gpt"
+__report__ = "https://github.com/szczyglis-dev/py-gpt/issues"
+__website__ = "https://pygpt.net"
+__pypi__ = "https://pypi.org/project/pygpt-net"
+__snap__ = "https://snapcraft.io/pygpt"
+__donate__ = "https://pygpt.net/#donate"
+__documentation__ = "https://pygpt.readthedocs.io"
+__discord__ = "https://pygpt.net/discord"
+__email__ = "info@pygpt.net"
+__donate_coffee__ = "https://pygpt.net/donate/buymeacoffee"
+__donate_paypal__ = "https://pygpt.net/donate/paypal"
+__donate_github__ = "https://github.com/sponsors/szczyglis-dev"
