517f32054d4b4c424322f6f3a61ac773a383efc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:16 +0000

    Add package itch-dl/0.5.2/itch_dl-0.5.2.tar.gz

---
+++ packages/itch-dl/itch_dl-0.5.2.tar.gz/itch_dl-0.5.2/README.md
@@ -0,0 +1,61 @@
+# itch-dl
+
+[![PyPI](https://img.shields.io/pypi/v/itch-dl)](https://pypi.org/project/itch-dl/)
+
+Bulk download games from [itch.io](https://itch.io/)
+
+- Can handle links with:
+  - [Game jams](https://itch.io/jams) (ex. https://itch.io/jam/gmtk-2023 or https://itch.io/jam/gbcompo23),
+  - [Browse pages](https://itch.io/games) (popular, newest, browse by tag...),
+  - [Collections](https://itch.io/my-collections) (ex. https://itch.io/c/4187503/test-collection),
+  - [Your library](https://itch.io/my-purchases),
+  - Creator/profile pages (ex. https://kiseff.itch.io/, https://itch.io/profile/pancelor).
+  - Individual games and titles (ex. https://maddymakesgamesinc.itch.io/celeste,
+    https://dragonruby.itch.io/dragonruby-gtk, or https://supergiant-games.itch.io/pyre).
+- Currently **NOT** supported:
+  - Bundles ([#11](https://github.com/DragoonAethis/itch-dl/issues/11))
+  - Access restricted games ([#16](https://github.com/DragoonAethis/itch-dl/issues/16))
+- Requires Python 3.8+, grab it from [PyPI](https://pypi.org/project/itch-dl/): `pip install itch-dl`
+- For development, use [Poetry](https://python-poetry.org/).
+
+> [!WARNING]
+> This tool does not let you download paid games for free. To download paid games, you must have
+> them attached in [your library](https://itch.io/my-purchases).
+
+
+## How to use
+
+- Log into itch.io with the account you'd like to use for downloading.
+- Generate [a new API key](https://itch.io/user/settings/api-keys) on your user account page.
+- Optional: Save the API key in the [itch-dl configuration file](https://github.com/DragoonAethis/itch-dl/wiki/Configuration-Files).
+- Run the downloader: `itch-dl https://itch.io/jam/yourjamhere` (add `--api-key <KEY>` if you did not save the API key).
+- Wait. This is going to take a while.
+
+More arguments are available - check them out with `itch-dl --help`.
+
+The downloader is able to grab more or less everything you can download via the itch app.
+
+The input URL can be one of the supported link formats listed above, a path to a itch.io JSON
+file with game jam entries, a list of itch.io game URLs (not browse/jam pages!) to download.
+
+**It's expected that the downloader output will not be complete** - logs are stupidly verbose
+and it prints a report on failed downloads and external URLs (links to files that are not on
+itch.io itself, but rather on an external host like Google Drive, Dropbox, etc), so you must
+manually grab whatever was not handled for you automatically.
+
+The downloader also grabs the entry page HTML, which usually comes with controls and such. By
+default, it does not download images, assets and so on, just the text - use `--mirror-web` to
+try and download these as well. This does not work very well yet, but gets the basics done.
+
+
+## Game Jam Entries JSON
+
+Downloader can parse and download games from a game jam entries JSON file if you need it.
+(The script basically automates the steps below, so if it's not able to do the same, please
+create an issue!)
+
+- Go to your jam's page, ex. https://itch.io/jam/gbcompo21 and right-click -> View Source.
+- Ctrl-F for `"id":` - it should find that text once, followed by a number. Write it down.
+- (It you found it multiple times, grab the one after I.ViewJam something something.)
+- Download https://itch.io/jam/ID/entries.json (replacing ID with what you wrote down).
+- Feed that to `itch-dl`!
---
+++ packages/itch-dl/itch_dl-0.5.2.tar.gz/itch_dl-0.5.2/itch_dl/cli.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import logging
+import argparse
+
+from .handlers import get_jobs_for_url_or_path
+from .downloader import drive_downloads
+from .config import Settings, load_config
+from .keys import get_download_keys
+from .api import ItchApiClient
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+def parse_args() -> argparse.Namespace:
+    # fmt: off
+    parser = argparse.ArgumentParser(description="Bulk download stuff from Itch.io.")
+    parser.add_argument("url_or_path",
+                        help="itch.io URL or path to a game jam entries.json file")
+    parser.add_argument("--profile", metavar="profile", default=None,
+                        help="configuration profile to load")
+
+    # These args must match config.py -> Settings class. Make sure all defaults here
+    # evaluate to False, or apply_args_on_settings will override profile settings.
+    parser.add_argument("--api-key", metavar="key", default=None,
+                        help="itch.io API key - https://itch.io/user/settings/api-keys")
+    parser.add_argument("--user-agent", metavar="agent", default=None,
+                        help="user agent to use when sending HTTP requests")
+    parser.add_argument("--download-to", metavar="path", default=None,
+                        help="directory to save results into (default: current working dir)")
+    parser.add_argument("--mirror-web", action="store_true",
+                        help="try to fetch assets on game sites")
+    parser.add_argument("--urls-only", action="store_true",
+                        help="print scraped game URLs without downloading them")
+    parser.add_argument("--parallel", metavar="parallel", type=int, default=None,
+                        help="how many threads to use for downloading games (default: 1)")
+    parser.add_argument("--filter-files-glob", metavar="glob", default=None,
+                        help="filter downloaded files with a shell-style glob/fnmatch (unmatched files are skipped)")
+    parser.add_argument("--filter-files-regex", metavar="regex", default=None,
+                        help="filter downloaded files with a Python regex (unmatched files are skipped)")
+    parser.add_argument("--verbose", action="store_true",
+                        help="print verbose logs")
+
+    return parser.parse_args()
+    # fmt: on
+
+
+def run() -> int:
+    args = parse_args()
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    settings: Settings = load_config(args, profile=args.profile)
+    if settings.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if not settings.api_key:
+        sys.exit(
+            "You did not provide an API key which itch-dl requires.\n"
+            "See https://github.com/DragoonAethis/itch-dl/wiki/API-Keys for more info."
+        )
+
+    url_or_path = args.url_or_path
+    del args  # Do not use `args` beyond this point.
+
+    # Check API key validity:
+    client = ItchApiClient(settings.api_key, settings.user_agent)
+    profile_req = client.get("/profile")
+    if not profile_req.ok:
+        sys.exit(
+            f"Provided API key appears to be invalid: {profile_req.text}\n"
+            "See https://github.com/DragoonAethis/itch-dl/wiki/API-Keys for more info."
+        )
+
+    jobs = get_jobs_for_url_or_path(url_or_path, settings)
+    jobs = list(set(jobs))  # Deduplicate, just in case...
+    logging.info("Found %d URL(s).", len(jobs))
+
+    if len(jobs) == 0:
+        sys.exit("No URLs to download.")
+
+    if settings.urls_only:
+        for job in jobs:
+            print(job)
+
+        return 0
+
+    # If the download dir is not set, use the current working dir:
+    settings.download_to = os.path.normpath(settings.download_to or os.getcwd())
+    os.makedirs(settings.download_to, exist_ok=True)
+
+    # Grab all the download keys (there's no way to fetch them per title...):
+    keys = get_download_keys(client)
+
+    drive_downloads(jobs, settings, keys)
+    return 0
