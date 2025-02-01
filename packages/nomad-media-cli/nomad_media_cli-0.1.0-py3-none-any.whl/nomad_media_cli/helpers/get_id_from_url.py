4d9936c81a29dc7110fe84dc8870b1a3b3fcb55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:09:02 +0000

    Add package nomad-media-cli/0.1.0/nomad_media_cli-0.1.0-py3-none-any.whl

---
+++ packages/nomad-media-cli/nomad_media_cli-0.1.0-py3-none-any.whl/nomad_media_cli/helpers/get_id_from_url.py
@@ -0,0 +1,33 @@
+import click
+import json
+import sys
+
+def get_id_from_url(ctx, url, object_key, nomad_sdk):
+    if url and "::" not in url:
+        click.echo(json.dumps({ "error": "Please provide a valid path or set the default bucket." }))               
+        sys.exit(1)
+    elif object_key:
+        if "bucket" in ctx.obj["config"]:
+            url = f"{ctx.obj["config"]["bucket"]}::{object_key}"
+        else:
+            click.echo(json.dumps({ "error": "Please set bucket using `set-bucket` or use url." }))
+            sys.exit(1)
+
+    url_search_results = nomad_sdk.search(None, None, None, [{
+        "fieldName": "url",
+        "operator": "equals",
+        "values": url
+    }], None, None, None, None, None, None, None, None, None, None, None)
+    
+    if not url_search_results or len(url_search_results["items"]) == 0:
+        click.echo(json.dumps({ "error": f"URL {url} not found." }))
+        sys.exit(1)
+        
+    for item in url_search_results["items"]:
+        id = item["id"]
+        asset_details = nomad_sdk.get_asset_details(id)
+        if asset_details:
+            return id
+    
+    click.echo(json.dumps({ "error": f"Asset with url {url} not found." }))
+    sys.exit(1)
