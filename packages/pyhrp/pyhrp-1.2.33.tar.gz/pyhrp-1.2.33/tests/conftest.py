04bef9c776bb1c893e2474b575c5cb4b5c29536c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:26 +0000

    Add package pyhrp/1.2.33/pyhrp-1.2.33.tar.gz

---
+++ packages/pyhrp/pyhrp-1.2.33.tar.gz/pyhrp-1.2.33/tests/conftest.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="session", name="resource_dir")
+def resource_fixture():
+    """resource fixture"""
+    return Path(__file__).parent / "resources"
