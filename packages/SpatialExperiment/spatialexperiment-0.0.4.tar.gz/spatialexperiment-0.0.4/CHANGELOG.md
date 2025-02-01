2e6d1d7d6d794483037802bcc6b300bce3825fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:07:01 +0000

    Add package SpatialExperiment/0.0.4/spatialexperiment-0.0.4.tar.gz

---
+++ packages/SpatialExperiment/spatialexperiment-0.0.4.tar.gz/spatialexperiment-0.0.4/CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## Version 0.0.3 - 0.0.4
+
+- Streamlining the `SpatialImage` class implementations.
+
+## Version 0.0.1 - 0.0.2
+
+- Initial version of the SpatialExperiment class with the additional slots.
+- Allow spatial coordinates to be a numpy array
---
+++ packages/SpatialExperiment/spatialexperiment-0.0.4.tar.gz/spatialexperiment-0.0.4/tests/test_spe.py
@@ -0,0 +1,22 @@
+from biocframe import BiocFrame
+from spatialexperiment import SpatialExperiment
+
+__author__ = "keviny2"
+__copyright__ = "keviny2"
+__license__ = "MIT"
+
+
+def test_SPE_empty_constructor():
+    tspe = SpatialExperiment()
+
+    assert isinstance(tspe, SpatialExperiment)
+
+    assert isinstance(tspe.img_data, BiocFrame)
+    assert tspe.img_data.shape[0] == 0
+
+    assert len(tspe.spatial_coords_names) == 0
+    assert isinstance(tspe.spatial_coords, BiocFrame)
+    assert tspe.spatial_coords.shape == (tspe.shape[1], 0)
+
+    assert "sample_id" in tspe.column_data.columns.as_list()
+    assert tspe.column_data.shape == (tspe.shape[1], 1)
