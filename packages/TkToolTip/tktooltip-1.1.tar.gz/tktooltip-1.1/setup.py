3fda30689ab3d769c1ef1bdf82082135360c8a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:05:13 +0000

    Add package TkToolTip/1.1/tktooltip-1.1.tar.gz

---
+++ packages/TkToolTip/tktooltip-1.1.tar.gz/tktooltip-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TkToolTip",
+    version="1.1",
+    author="Seu Nome",
+    author_email="seuemail@example.com",
+    description="Uma breve descrição do seu projeto",
+    long_description="Conteúdo do README",
+    long_description_content_type="text/markdown",  
+    url="https://github.com/seuusuario/TkToolTip",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
