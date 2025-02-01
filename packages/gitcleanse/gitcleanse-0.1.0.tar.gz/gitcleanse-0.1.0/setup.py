979ab3419e6d498c014c6405bf9ca8a15267129a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:05:13 +0000

    Add package gitcleanse/0.1.0/gitcleanse-0.1.0.tar.gz

---
+++ packages/gitcleanse/gitcleanse-0.1.0.tar.gz/gitcleanse-0.1.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GitCleanse",
+    version="0.1.0",
+    author="Pouya Shahrdami",
+    author_email="pooyashahrdami@gmail.com",
+    description="A powerful command-line tool to manage your GitHub followers and following.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pouyashahrdami/GitCleanse",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'rich',
+        'requests',
+        'python-dotenv'
+    ],
+    entry_points={
+        'console_scripts': [
+            'gitcleanse=main:main',
+        ],
+    },
+    package_data={
+        "": ["README.md"]
+    }
+)
