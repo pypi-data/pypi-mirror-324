84bbab0ed44c120e1d5b27593b8ec54967ba835f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:08:55 +0000

    Add package threatexchange/1.2.4/threatexchange-1.2.4.tar.gz

---
+++ packages/threatexchange/threatexchange-1.2.4.tar.gz/threatexchange-1.2.4/threatexchange/signal_type/tests/test_hash_from_x.py
@@ -0,0 +1,50 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+import unittest
+
+from threatexchange.signal_type import (
+    md5,
+    raw_text,
+    trend_query,
+    url_md5,
+    url,
+)
+from threatexchange.signal_type.pdq import signal
+from threatexchange.signal_type.signal_base import TextHasher
+
+
+class SignalTypeHashTest(unittest.TestCase):
+    """
+    Sanity check for signal type hashing methods.
+    """
+
+    # TODO - maybe make a metaclass for this to automatically detect?
+    SIGNAL_TYPES_TO_TEST = [
+        md5.VideoMD5Signal,
+        signal.PdqSignal,
+        raw_text.RawTextSignal,
+        trend_query.TrendQuerySignal,
+        url_md5.UrlMD5Signal,
+        url.URLSignal,
+    ]
+
+    def test_signal_names_unique(self):
+        seen = {}
+        for s in self.SIGNAL_TYPES_TO_TEST:
+            name = s.get_name()
+            assert (
+                name not in seen
+            ), f"Two signal types share the same name: {s!r} and {seen[name]!r}"
+
+    def test_signal_types_have_content(self):
+        for s in self.SIGNAL_TYPES_TO_TEST:
+            assert s.get_content_types(), "{s!r} has no content types"
+
+    def test_str_hashers_have_impl(self):
+        text_hashers = [
+            s for s in self.SIGNAL_TYPES_TO_TEST if isinstance(s, TextHasher)
+        ]
+        for s in text_hashers:
+            assert s.hash_from_str(
+                "test string"
+            ), "{s!r} produced no output from hasher"
