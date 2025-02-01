04c222571fb487fa01ecd6b8064649ac8306d48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:05:09 +0000

    Add package elixirdb/0.11/elixirdb-0.11.tar.gz

---
+++ packages/elixirdb/elixirdb-0.11.tar.gz/elixirdb-0.11/src/elixirdb/dictionaries/__init__.py
@@ -0,0 +1,19 @@
+# "Import dictionaries"
+
+# from elixirdb.dictionaries.db_errors import db_errors
+# from elixirdb.dictionaries.model_errors import (
+#     db_model_errors,
+#     db_validation_errors,
+# )
+# from elixirdb.dictionaries.model_fields import db_model_fields
+# from elixirdb.dictionaries.resources import drivers, url_keys
+
+
+# __all__ = [
+#     "db_errors",
+#     "db_model_errors",
+#     "db_model_fields",
+#     "db_validation_errors",
+#     "drivers",
+#     "url_keys",
+# ]
