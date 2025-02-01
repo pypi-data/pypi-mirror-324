f713e923401db4a9e2eb4a072c2cee98511453e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:06:04 +0000

    Add package CYB600-nobins-web-time-server/1.0.2/CYB600-nobins-web-time-server-1.0.2.tar.gz

---
+++ packages/CYB600-nobins-web-time-server/CYB600-nobins-web-time-server-1.0.2.tar.gz/CYB600-nobins-web-time-server-1.0.2/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='CYB600-nobins-web-time-server',
+    version='1.0.2',
+    packages=find_packages(),
+    install_requires=['flask'],
+    entry_points={
+        'console_scripts': [
+            'web-time-server=web_time_server.server:run_server',
+        ],
+    },
+)
+
