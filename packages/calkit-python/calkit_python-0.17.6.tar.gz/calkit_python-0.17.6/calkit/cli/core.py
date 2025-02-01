1f15e2268b52d8fdc7c43f77b2a95bd3eed31a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:05:22 +0000

    Add package calkit-python/0.17.6/calkit_python-0.17.6.tar.gz

---
+++ packages/calkit-python/calkit_python-0.17.6.tar.gz/calkit_python-0.17.6/calkit/cli/core.py
@@ -0,0 +1,32 @@
+"""Core CLI functionality."""
+
+import os
+import subprocess
+
+import typer
+
+
+def print_sep(name: str):
+    width = 66
+    txt_width = len(name) + 2
+    buffer_width = (width - txt_width) // 2
+    buffer = "-" * buffer_width
+    typer.echo(f"{buffer} {name} {buffer}")
+
+
+def run_cmd(cmd: list[str]):
+    if os.name == "nt":
+        subprocess.call(cmd)
+    else:
+        import pty
+
+        pty.spawn(cmd, lambda fd: os.read(fd, 1024))
+
+
+def raise_error(txt: str):
+    typer.echo(typer.style("Error: " + str(txt), fg="red"), err=txt)
+    raise typer.Exit(1)
+
+
+def warn(txt: str):
+    typer.echo(typer.style("Warning: " + str(txt), fg="yellow"))
