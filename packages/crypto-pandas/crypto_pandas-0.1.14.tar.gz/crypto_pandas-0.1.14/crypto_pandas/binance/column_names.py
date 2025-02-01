5fd6588b76a747076fd6b3963267ce501ffad48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:41 +0000

    Add package crypto-pandas/0.1.14/crypto_pandas-0.1.14.tar.gz

---
+++ packages/crypto-pandas/crypto_pandas-0.1.14.tar.gz/crypto_pandas-0.1.14/crypto_pandas/binance/column_names.py
@@ -0,0 +1,24 @@
+klines_column_names = [
+    "openTime",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "closeTime",
+    "quoteAssetVolume",
+    "numberOfTrades",
+    "takerBuyBaseAssetVolume",
+    "takerBuyQuoteAssetVolume",
+    "ignore",
+]
+agg_trades_columns = [
+    "tradeId",
+    "price",
+    "qty",
+    "firstTradeId",
+    "lastTradeId",
+    "timestamp",
+    "isBuyerMaker",
+    "ignore",
+]
