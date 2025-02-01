df7f72333db4773b625f02a1f34bc541b21a7023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:09:03 +0000

    Add package pytest-performancetotal/0.2.8/pytest_performancetotal-0.2.8.tar.gz

---
+++ packages/pytest-performancetotal/pytest_performancetotal-0.2.8.tar.gz/pytest_performancetotal-0.2.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pytest-performancetotal',
+    packages=find_packages(where='src', exclude=['*tests*']),
+    package_dir={'': 'src'},
+    version='0.2.8',
+    author='Tzur Paldi',
+    author_email='tzur.paldi@outlook.com',
+    maintainer='Tzur Paldi',
+    maintainer_email='tzur.paldi@outlook.com',
+    license='MIT',
+    url='https://github.com/tzurp/pytest_performancetotal',
+    description='A performance plugin for pytest',
+    keywords='pytest plugin performance playwright',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest_performancetotal = pytest_performancetotal.performancetotal',
+        ],
+    },
+    install_requires=[
+        'filelock>=3.13.0',
+    ]
+)
