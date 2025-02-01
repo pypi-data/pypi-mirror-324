355cd79776d845a5dd45f27d9ac49173b36780ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:05:32 +0000

    Add package datajunction-server/0.0.1a84.dev1/datajunction_server-0.0.1a84.dev1.tar.gz

---
+++ packages/datajunction-server/datajunction_server-0.0.1a84.dev1.tar.gz/datajunction_server-0.0.1a84.dev1/datajunction_server/api/graphql/scalars/metricmetadata.py
@@ -0,0 +1,58 @@
+"""Metric metadata scalars"""
+
+from typing import Optional
+
+import strawberry
+
+from datajunction_server.models.node import MetricDirection as MetricDirection_
+from datajunction_server.sql.decompose import Aggregability as Aggregability_
+from datajunction_server.sql.decompose import AggregationRule as AggregationRule_
+from datajunction_server.sql.decompose import Measure as Measure_
+
+MetricDirection = strawberry.enum(MetricDirection_)
+Aggregability = strawberry.enum(Aggregability_)
+
+
+@strawberry.type
+class Unit:  # pylint: disable=too-few-public-methods
+    """
+    Metric unit
+    """
+
+    name: str
+    label: Optional[str]
+    category: Optional[str]
+    abbreviation: Optional[str]
+
+
+@strawberry.experimental.pydantic.type(model=AggregationRule_, all_fields=True)
+class AggregationRule:  # pylint: disable=missing-class-docstring,too-few-public-methods
+    ...
+
+
+@strawberry.experimental.pydantic.type(model=Measure_, all_fields=True)
+class Measure:  # pylint: disable=missing-class-docstring,too-few-public-methods
+    ...
+
+
+@strawberry.type
+class ExtractedMeasures:  # pylint: disable=too-few-public-methods
+    """
+    extracted measures from metric
+    """
+
+    measures: list[Measure]
+    derived_query: str
+    derived_expression: str
+
+
+@strawberry.type
+class MetricMetadata:  # pylint: disable=too-few-public-methods
+    """
+    Metric metadata output
+    """
+
+    direction: Optional[MetricDirection]  # type: ignore
+    unit: Optional[Unit]
+    expression: str
+    incompatible_druid_functions: list[str]
