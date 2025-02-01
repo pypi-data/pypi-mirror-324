e62918ac489578a1db7d177e128dd1c25c731c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 16:05:17 +0000

    Add package fpcli/0.1.22/fpcli-0.1.22.tar.gz

---
+++ packages/fpcli/fpcli-0.1.22.tar.gz/fpcli-0.1.22/fpcli/commands/makeapp.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import typer
+
+from ..function.startproject import create_file
+from ..content.startproject import   get_helper_utilities_content,  get_urls_contant, get_welcome_controller_contant
+from .make import make
+from .make import app
+
+
+def create_folder_structure(base_dir: str):
+    """Creates the folder and file structure."""
+    folders = [
+        "utils",
+        "views",
+        "schemas",
+        "middleware",
+        "models",
+        "services",
+      
+    ]
+
+    files = {
+        f"{base_dir}/config.py": "# Configuration file",
+        f"{base_dir}/urls.py": "# all routes file\n"+get_urls_contant(),
+        f"{base_dir}/utils/__init__.py": "# Utility functions \n\n"+get_helper_utilities_content(),
+        f"{base_dir}/views/welcome_views.py": "#Welcome Controller  "+get_welcome_controller_contant(),
+        f"{base_dir}/middleware/__init__.py": "",
+        f"{base_dir}/models/__init__.py": "",
+        f"{base_dir}/services/__init__.py": "",
+
+    }
+
+    # Create folders
+    for folder in folders:
+        os.makedirs(f"{base_dir}/{folder}", exist_ok=True)
+
+    # Create files
+    for file, content in files.items():
+        create_file(file, content)
+    
+@make.command("app")
+def startapp(name: str):
+    """Create a new project structure."""
+    base_dir = Path(name).resolve()
+    os.makedirs(base_dir, exist_ok=True)
+    create_folder_structure(str(base_dir))
+    typer.echo(f"Project '{name}' created successfully at {base_dir}!")
+
