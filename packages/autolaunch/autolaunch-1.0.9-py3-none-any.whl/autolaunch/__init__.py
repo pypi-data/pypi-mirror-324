fe8cbdb5ad4651ebf507ca0166282f8bac7a9ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:40 +0000

    Add package autolaunch/1.0.9/autolaunch-1.0.9-py3-none-any.whl

---
+++ packages/autolaunch/autolaunch-1.0.9-py3-none-any.whl/autolaunch/__init__.py
@@ -0,0 +1,5 @@
+from .autolaunch import ensure_registered, unregister_app, register_app, show_help, main
+
+__all__ = ["ensure_registered", "unregister_app", "register_app", "show_help", "main"]
+
+
