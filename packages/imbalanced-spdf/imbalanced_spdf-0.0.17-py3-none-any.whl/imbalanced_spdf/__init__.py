06b807d6bcf970ac053637d283a15bf59dafa492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:08:41 +0000

    Add package imbalanced-spdf/0.0.17/imbalanced_spdf-0.0.17-py3-none-any.whl

---
+++ packages/imbalanced-spdf/imbalanced_spdf-0.0.17-py3-none-any.whl/imbalanced_spdf/__init__.py
@@ -0,0 +1,8 @@
+"""imbalanced_spdf is a python package for imbalanced_spdf data handling in machine learning."""
+
+__author__ = """Rahul Goswami"""
+__email__ = 'yuvrajiro@gmail.com'
+__version__ = '0.0.1'
+
+from .ensemble import SPBaDF, SPBoDF
+from .tree import tree
