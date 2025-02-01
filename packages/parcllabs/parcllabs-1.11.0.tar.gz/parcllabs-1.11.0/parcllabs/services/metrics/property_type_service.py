efc341ce776c10f97734504818e0827520a53531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:06:04 +0000

    Add package parcllabs/1.11.0/parcllabs-1.11.0.tar.gz

---
+++ packages/parcllabs/parcllabs-1.11.0.tar.gz/parcllabs-1.11.0/parcllabs/services/metrics/property_type_service.py
@@ -0,0 +1,30 @@
+from typing import Any, Mapping, Optional
+
+from parcllabs.services.parcllabs_service import ParclLabsService
+from parcllabs.services.validators import Validators
+
+
+class PropertyTypeService(ParclLabsService):
+    def retrieve(
+        self,
+        parcl_ids: int,
+        start_date: str = None,
+        end_date: str = None,
+        property_type: str = None,
+        limit: Optional[int] = None,
+        params: Optional[Mapping[str, Any]] = {},
+        auto_paginate: bool = False,
+    ):
+        property_type = Validators.validate_property_type(property_type)
+
+        if property_type:
+            params["property_type"] = property_type
+
+        return super().retrieve(
+            parcl_ids=parcl_ids,
+            start_date=start_date,
+            end_date=end_date,
+            limit=limit,
+            params=params,
+            auto_paginate=auto_paginate,
+        )
