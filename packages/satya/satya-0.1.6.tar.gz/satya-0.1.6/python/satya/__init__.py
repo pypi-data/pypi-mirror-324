d66153feec55035c039fca328256139a3629c8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:05:11 +0000

    Add package satya/0.1.6/satya-0.1.6.tar.gz

---
+++ packages/satya/satya-0.1.6.tar.gz/satya-0.1.6/python/satya/__init__.py
@@ -0,0 +1,4 @@
+from ._satya import StreamValidator
+from .models import Model, Field, List, Dict
+
+__all__ = ['StreamValidator', 'Model', 'Field', 'List', 'Dict'] 
---
+++ packages/satya/satya-0.1.6.tar.gz/satya-0.1.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="satya",
+    packages=["satya"],
+    package_dir={"": "python"},
+    version="0.1.5",
+) 
