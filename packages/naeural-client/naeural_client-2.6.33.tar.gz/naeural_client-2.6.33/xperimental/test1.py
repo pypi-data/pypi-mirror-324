af1c7cd9bd240db62a780633c9c49c01d8ed56a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:48 +0000

    Add package naeural-client/2.6.33/naeural_client-2.6.33.tar.gz

---
+++ packages/naeural-client/naeural_client-2.6.33.tar.gz/naeural_client-2.6.33/xperimental/test1.py
@@ -0,0 +1,35 @@
+from naeural_client import Session, CustomPluginTemplate, PLUGIN_TYPES
+
+
+
+def run_predict(plugin: CustomPluginTemplate, inputs: list, nr_steps: int) -> list:
+  preds = plugin.basic_ts_fit_predict(inputs, nr_steps)
+  return preds
+
+
+if __name__ == '__main__':
+  
+  session = Session()
+  my_node = "0xai_ApM1AbzLq1VtsLIidmvzt1Nv4Cyl5Wed0fHNMoZv9u4X"
+  
+  app, _ = session.create_web_app(
+    node=my_node,
+    name="ratio1_simple_predict_webapp",
+    endpoints=[
+      {
+        "function": run_predict,
+        "method": "post",
+      },        
+    ]
+  )
+  try:
+    url = app.deploy()
+    print("Webapp deployed at: ", url)
+  except Exception as e:
+    print("Error deploying webapp: ", e)
+  
+  session.wait(
+    close_pipeline_on_timeout=True,
+    close_session_on_timeout=True,
+    seconds=60
+  )
