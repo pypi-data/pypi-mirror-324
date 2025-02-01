89d2476919f0543fae33d574865483b588306f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:12 +0000

    Add package utf-queue-client/1.18.0/utf_queue_client-1.18.0-py3-none-any.whl

---
+++ packages/utf-queue-client/utf_queue_client-1.18.0-py3-none-any.whl/utf_queue_client/models/schemas/utf_queue_models/models/python/generated_models.py
@@ -0,0 +1,54 @@
+from pydantic.version import VERSION as PYDANTIC_VERSION
+
+IS_PYDANTIC_V1 = PYDANTIC_VERSION.startswith("1.")
+
+if IS_PYDANTIC_V1:
+    from .generated_models_pydantic_v1 import QueueMessage
+    from .generated_models_pydantic_v1 import QueueMessageV1
+    from .generated_models_pydantic_v1 import QueueRecord
+    from .generated_models_pydantic_v1 import DataLakeData
+    from .generated_models_pydantic_v1 import TelemetryData
+    from .generated_models_pydantic_v1 import ArtifactUploadRequest
+    from .generated_models_pydantic_v1 import ArtifactMetadata
+    from .generated_models_pydantic_v1 import ArtifactBuildMetadata
+    from .generated_models_pydantic_v1 import SqaAppBuildResult
+    from .generated_models_pydantic_v1 import SqaTestResult
+    from .generated_models_pydantic_v1 import SqaTestSession
+    from .generated_models_pydantic_v1 import LogEvent
+    from .generated_models_pydantic_v1 import ExceptionEvent
+    from .generated_models_pydantic_v1 import TestRailResult
+    from .generated_models_pydantic_v1 import TestResultCustomProps
+else:
+    from .generated_models_pydantic_v2 import QueueMessage
+    from .generated_models_pydantic_v2 import QueueMessageV1
+    from .generated_models_pydantic_v2 import QueueRecord
+    from .generated_models_pydantic_v2 import DataLakeData
+    from .generated_models_pydantic_v2 import TelemetryData
+    from .generated_models_pydantic_v2 import ArtifactUploadRequest
+    from .generated_models_pydantic_v2 import ArtifactMetadata
+    from .generated_models_pydantic_v2 import ArtifactBuildMetadata
+    from .generated_models_pydantic_v2 import SqaAppBuildResult
+    from .generated_models_pydantic_v2 import SqaTestResult
+    from .generated_models_pydantic_v2 import SqaTestSession
+    from .generated_models_pydantic_v2 import LogEvent
+    from .generated_models_pydantic_v2 import ExceptionEvent
+    from .generated_models_pydantic_v2 import TestRailResult
+    from .generated_models_pydantic_v2 import TestResultCustomProps
+
+__all__ = [
+    "QueueMessage",
+    "QueueMessageV1",
+    "QueueRecord",
+    "DataLakeData",
+    "TelemetryData",
+    "ArtifactUploadRequest",
+    "ArtifactMetadata",
+    "ArtifactBuildMetadata",
+    "SqaAppBuildResult",
+    "SqaTestResult",
+    "SqaTestSession",
+    "LogEvent",
+    "ExceptionEvent",
+    "TestRailResult",
+    "TestResultCustomProps",
+]
