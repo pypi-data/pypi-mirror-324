a55d3bf7cf52de3966d3186512ba5bac9208cf21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:05:37 +0000

    Add package malac-hd/1.0.0/malac-hd-1.0.0.tar.gz

---
+++ packages/malac-hd/malac-hd-1.0.0.tar.gz/malac-hd-1.0.0/malac/models/cda/__init__.py
@@ -0,0 +1,6 @@
+from malac.models.cda import at_ext, ch_ext_pharm
+
+at_ext.AD.choice_group_names = ["item"]
+at_ext.EN.choice_group_names = ["item"]
+ch_ext_pharm.AD.choice_group_names = ["item"]
+ch_ext_pharm.EN.choice_group_names = ["item"]
