36f6829e9722db5142e4d873c7ebc098de937ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:29 +0000

    Add package dtuimldmtools/0.0.6/dtuimldmtools-0.0.6.tar.gz

---
+++ packages/dtuimldmtools/dtuimldmtools-0.0.6.tar.gz/dtuimldmtools-0.0.6/scripts/ex4_1_6.py
@@ -0,0 +1,50 @@
+# exercise 4.1.6
+
+import importlib_resources
+import numpy as np
+import scipy.linalg as linalg
+from matplotlib.pyplot import cm, figure, imshow, show, subplot, title, xticks, yticks
+from scipy.io import loadmat
+
+filename = importlib_resources.files("dtuimldmtools").joinpath("data/zipdata.mat")
+# Digits to include in analysis (to include all: n = range(10))
+n = [0]
+
+# Load Matlab data file to python dict structure
+# and extract variables of interest
+traindata = loadmat(filename)["traindata"]
+X = traindata[:, 1:]
+y = traindata[:, 0]
+N, M = X.shape
+C = len(n)
+
+# Remove digits that are not to be inspected
+class_mask = np.zeros(N).astype(bool)
+for v in n:
+    cmsk = y == v
+    class_mask = class_mask | cmsk
+X = X[class_mask, :]
+y = y[class_mask]
+N = np.shape(X)[0]
+
+mu = X.mean(axis=0)
+s = X.std(ddof=1, axis=0)
+S = np.cov(X, rowvar=0, ddof=1)
+
+figure()
+subplot(1, 2, 1)
+I = np.reshape(mu, (16, 16))
+imshow(I, cmap=cm.gray_r)
+title("Mean")
+xticks([])
+yticks([])
+subplot(1, 2, 2)
+I = np.reshape(s, (16, 16))
+imshow(I, cmap=cm.gray_r)
+title("Standard deviation")
+xticks([])
+yticks([])
+
+show()
+
+print("Ran Exercise 4.1.6")
