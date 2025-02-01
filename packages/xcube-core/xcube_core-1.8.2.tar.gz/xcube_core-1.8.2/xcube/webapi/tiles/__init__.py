791ef6f00b483707c931730776aae0372b4cf2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:46 +0000

    Add package xcube-core/1.8.2/xcube_core-1.8.2.tar.gz

---
+++ packages/xcube-core/xcube_core-1.8.2.tar.gz/xcube_core-1.8.2/xcube/webapi/tiles/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2018-2024 by xcube team and contributors
+# Permissions are hereby granted under the terms of the MIT License:
+# https://opensource.org/licenses/MIT.
+
+from .routes import PATH_PARAM_X
+from .routes import PATH_PARAM_Y
+from .routes import PATH_PARAM_Z
+from .routes import QUERY_PARAM_TIME
+from .routes import QUERY_PARAM_FORMAT
+from .routes import QUERY_PARAM_RETINA
+from .routes import api
---
+++ packages/xcube-core/xcube_core-1.8.2.tar.gz/xcube_core-1.8.2/xcube/webapi/viewer/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2018-2024 by xcube team and contributors
+# Permissions are hereby granted under the terms of the MIT License:
+# https://opensource.org/licenses/MIT.
+
+# noinspection PyUnresolvedReferences
+
+from .routes import api
+from .viewer import Viewer
