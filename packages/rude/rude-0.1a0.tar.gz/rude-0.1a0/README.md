6c0ca5ec9e086e979d3d4e19edb1cb5e7afb95ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:05:43 +0000

    Add package rude/0.1a0/rude-0.1a0.tar.gz

---
+++ packages/rude/rude-0.1a0.tar.gz/rude-0.1a0/README.md
@@ -0,0 +1,3 @@
+# RUDE
+
+An opinionated linter using LibCST.
---
+++ packages/rude/rude-0.1a0.tar.gz/rude-0.1a0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+# https://setuptools.pypa.io/en/latest/history.html#v42-0-0
+setup(setup_requires="setuptools>=42.0.0")
