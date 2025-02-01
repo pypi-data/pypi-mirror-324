24d7d9690618aed82e374ab62c0cc72fa98d65dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:44 +0000

    Add package nafcodec/0.3.1/nafcodec-0.3.1-cp312-cp312-macosx_12_0_x86_64.whl

---
+++ packages/nafcodec/nafcodec-0.3.1-cp312-cp312-macosx_12_0_x86_64.whl/nafcodec/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.2.0"
+
+from . import lib
+from .lib import (
+    Decoder,
+    Record,
+    Encoder,
+    open,
+)
+
+__author__ = lib.__author__
+__license__ = "MIT"
+__doc__ = lib.__doc__
