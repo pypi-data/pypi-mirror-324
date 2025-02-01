53c10cf3fe2c9481d7e20da1fdcb65fba5db36c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:05:12 +0000

    Add package cgroup-monitor/0.1.3/cgroup_monitor-0.1.3.tar.gz

---
+++ packages/cgroup-monitor/cgroup_monitor-0.1.3.tar.gz/cgroup_monitor-0.1.3/cgroup_monitor/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+def is_cgroup_v2():
+    return os.path.exists("/sys/fs/cgroup/cgroup.controllers")
+
+
+if is_cgroup_v2():
+    from .v2_monitor import CGroupMonitor
+else:
+    from .main_monitor import CGroupMonitor
+
+__version__ = "0.1.3"
+__all__ = ["CGroupMonitor"]
