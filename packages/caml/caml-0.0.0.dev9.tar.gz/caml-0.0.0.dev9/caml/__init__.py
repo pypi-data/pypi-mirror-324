cde8dc56263714577f2c9fe41f9adf6a12593e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:06:43 +0000

    Add package caml/0.0.0.dev9/caml-0.0.0.dev9.tar.gz

---
+++ packages/caml/caml-0.0.0.dev9.tar.gz/caml-0.0.0.dev9/caml/__init__.py
@@ -0,0 +1,4 @@
+"""Copyright (c) 2024 Jacob Pieniazek. All rights reserved."""
+
+from ._version import __version__
+from .core.cate import CamlCATE
---
+++ packages/caml/caml-0.0.0.dev9.tar.gz/caml-0.0.0.dev9/caml/core/dml.py
@@ -0,0 +1,8 @@
+# from ..utils import descriptors
+from ._base import CamlBase
+
+
+class CamlDML(CamlBase):
+    """The CamlDML class is..."""
+
+    pass
