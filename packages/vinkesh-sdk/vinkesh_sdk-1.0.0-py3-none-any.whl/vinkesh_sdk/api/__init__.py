ed93fc0f64746059b6af75c5f2de73e35525a57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:07:03 +0000

    Add package vinkesh-sdk/1.0.0/vinkesh_sdk-1.0.0-py3-none-any.whl

---
+++ packages/vinkesh-sdk/vinkesh_sdk-1.0.0-py3-none-any.whl/vinkesh_sdk/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from vinkesh_sdk.api.pet_api import PetApi
+from vinkesh_sdk.api.store_api import StoreApi
+from vinkesh_sdk.api.user_api import UserApi
+
