a64eea84591d8250986951710db7ea58d188e625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:09:19 +0000

    Add package cchloader/0.4.7/cchloader-0.4.7.tar.gz

---
+++ packages/cchloader/cchloader-0.4.7.tar.gz/cchloader-0.4.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+readme = open("README.md").read()
+
+setup(
+    name='cchloader',
+    version='0.4.7',
+    packages=find_packages(),
+    url='https://github.com/Som-Energia/cchloader',
+    license='GPLv3',
+    author='',
+    author_email='',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    description='''Implementation based on sippers from GISCE
+    https://github.com/gisce/sippers''',
+    entry_points='''
+        [console_scripts]
+        usmartdata=usmartdata.cli:usmartdata
+    ''',
+    package_data={
+        'cchloader': ['data/*']
+    },
+    install_requires=[
+        "raven",
+        "pymongo<3.0",
+        "osconf",
+        "marshmallow>=2.13.5",
+        "click",
+        "pytz",
+        "psycopg2-binary"
+    ],
+    test_suite='tests',
+)
