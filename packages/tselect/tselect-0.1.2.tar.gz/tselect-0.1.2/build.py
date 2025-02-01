0659c77e27065bd472d78127eddb5f67f60dc485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 03:09:02 +0000

    Add package tselect/0.1.2/tselect-0.1.2.tar.gz

---
+++ packages/tselect/tselect-0.1.2.tar.gz/tselect-0.1.2/build.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+
+from pathlib import Path
+
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Distribution
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+
+
+def build() -> None:
+    ext_modules = cythonize([
+    Extension(
+        'tselect.rank_correlation.spearman', ['tselect/rank_correlation/spearman.pyx'],
+        include_dirs=[np.get_include()]
+    ), ]
+    )
+
+    distribution = Distribution({
+        "name": "package",
+        "ext_modules": ext_modules
+    })
+
+    cmd = build_ext(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        output = Path(output)
+        relative_extension = Path(".") / output.relative_to(cmd.build_lib)
+
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
