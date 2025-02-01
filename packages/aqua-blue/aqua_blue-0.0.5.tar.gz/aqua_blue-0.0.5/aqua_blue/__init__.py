c84a0875624febb5601d2053064ae187b4a51c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:06:55 +0000

    Add package aqua-blue/0.0.5/aqua_blue-0.0.5.tar.gz

---
+++ packages/aqua-blue/aqua_blue-0.0.5.tar.gz/aqua_blue-0.0.5/aqua_blue/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.5"
+__author__ = "Jacob Jeffries"
+__author_email__ = "jacob.jeffries@chicagoclubteam.org"
+__url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue"
+
+from .reservoir import EchoStateNetwork as EchoStateNetwork
+from .time_series import TimeSeries as TimeSeries
