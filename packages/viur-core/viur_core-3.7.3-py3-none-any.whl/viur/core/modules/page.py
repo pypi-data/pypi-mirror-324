b6f4fb5e9556bcf3641b04b0c051f81a1b49d2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 14:05:13 +0000

    Add package viur-core/3.7.3/viur_core-3.7.3-py3-none-any.whl

---
+++ packages/viur-core/viur_core-3.7.3-py3-none-any.whl/viur/core/modules/page.py
@@ -0,0 +1,47 @@
+from viur.core.prototypes.tree import Tree, TreeSkel
+from viur.core.bones import *
+
+
+class PageSkel(TreeSkel):
+    kindName = "page"
+    searchindex = "page"
+
+    name = StringBone(
+        descr="Name",
+        searchable=True,
+        required=True
+    )
+
+    descr = TextBone(
+        descr="Content",
+        required=True,
+        searchable=True
+    )
+
+
+class Page(Tree):
+    """
+    A simple page construction module, allowing to create a structure of pages.
+    """
+
+    adminInfo = {
+        "name": "Pages",
+        "handler": "tree.nodeonly.page",
+        "icon": "cloud-fill",
+        "columns": ["name", "language", "isactive"],
+        "preview": {
+            "Web": "/{{module}}/view/{{key}}"
+        }
+    }
+
+    viewTemplate = "page_view"
+
+    def getAvailableRootNodes(self, *args, **kwargs):
+        repo = self.ensureOwnModuleRootNode()
+        return [{
+            "name": "pages",
+            "key": repo.key
+        }]
+
+
+Page.html = True
