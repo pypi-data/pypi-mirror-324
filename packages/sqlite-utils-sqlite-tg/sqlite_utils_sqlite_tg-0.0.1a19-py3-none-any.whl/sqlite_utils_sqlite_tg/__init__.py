93f6d417625c32b89df8088dfc2aa5ede60d25a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:44 +0000

    Add package sqlite-utils-sqlite-tg/0.0.1a19/sqlite_utils_sqlite_tg-0.0.1a19-py3-none-any.whl

---
+++ packages/sqlite-utils-sqlite-tg/sqlite_utils_sqlite_tg-0.0.1a19-py3-none-any.whl/sqlite_utils_sqlite_tg/__init__.py
@@ -0,0 +1,12 @@
+
+from sqlite_utils import hookimpl
+import sqlite_tg
+
+__version__ = "0.0.1a19"
+__version_info__ = tuple(__version__.split("."))
+
+@hookimpl
+def prepare_connection(conn):
+  conn.enable_load_extension(True)
+  sqlite_tg.load(conn)
+  conn.enable_load_extension(False)
