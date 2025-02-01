f3a3c86b6a08f5f9f26491b224e980496b29a7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:37 +0000

    Add package grafs_e/0.3.68/grafs_e-0.3.68.tar.gz

---
+++ packages/grafs_e/grafs_e-0.3.68.tar.gz/grafs_e-0.3.68/src/grafs_e/launch.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import streamlit.cli
+
+
+def run():
+    """Lance Streamlit en utilisant le package installé"""
+    package_path = os.path.dirname(__file__)  # Trouve le chemin de grafs_e
+    app_path = os.path.join(package_path, "app.py")  # Chemin de app.py
+    print(f"Launching Streamlit from: {app_path}")  # Debugging
+    sys.argv = ["streamlit", "run", app_path]  # Simule une commande CLI
+    streamlit.cli.main()  # Démarre Streamlit directement
