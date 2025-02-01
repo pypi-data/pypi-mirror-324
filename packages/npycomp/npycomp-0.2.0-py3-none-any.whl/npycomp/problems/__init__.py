a8b8011ed931d44ac845f5bc8e40023dc9d3a945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 15:05:27 +0000

    Add package npycomp/0.2.0/npycomp-0.2.0-py3-none-any.whl

---
+++ packages/npycomp/npycomp-0.2.0-py3-none-any.whl/npycomp/problems/__init__.py
@@ -0,0 +1,20 @@
+"""Implementations of various NP-complete problems.
+
+.. currentmodule:: npycomp.problems
+
+NPyComp ``problems`` provides implementations of various NP-complete problems.
+Each problem in this module can be reduced to any other problem in this module.
+
+Problems
+--------
+.. autosummary::
+   :toctree: problems
+
+   SAT - A SAT problem defined by clauses in CNF form.
+   ThreeSAT - A 3-SAT problem defined by clauses in CNF form.
+   Clique - A Clique problem defined by an adjacency matrix and a clique size.
+"""
+
+from npycomp.problems._3sat import ThreeSAT
+from npycomp.problems._clique import Clique
+from npycomp.problems._sat import SAT
