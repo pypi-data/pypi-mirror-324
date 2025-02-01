479e75e570799e5197a559dfc8dc9380a031e435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 05:06:17 +0000

    Add package mi-tabletqt/1.2.3/mi_tabletqt-1.2.3.tar.gz

---
+++ packages/mi-tabletqt/mi_tabletqt-1.2.3.tar.gz/mi_tabletqt-1.2.3/src/tabletqt/graphics/crayon_box.py
@@ -0,0 +1,74 @@
+""" crayon_box.py - Sets the Qt pen and brush properties using the StyleDB """
+
+# System
+import logging
+from typing import Optional
+
+# Qt
+from PyQt6.QtGui import QBrush, QPen, QColor
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QAbstractGraphicsShapeItem, QGraphicsLineItem
+
+# Tablet
+from tabletqt.styledb import StyleDB, FloatRGB
+
+_logger = logging.getLogger(__name__)
+class CrayonBox:
+    """
+    Get your crayons here. Specifically, your Qt pen and brush settings.
+    """
+    @classmethod
+    def choose_crayons(cls, item: QAbstractGraphicsShapeItem | QGraphicsLineItem, border_style: str,
+                       fill: Optional['str'] = None):
+        """
+        Create and assign a pen and brush for a given QT graphics item with settings found
+        in the StyleDB
+
+        :param item: Qt graphic item to be displayed
+        :param border_style: Name of the user border style key in the StyleDB
+        :param fill: Name of user fill key in the StyleDB, if any
+        """
+        # Create a pen and set its properties using the StyleDB
+        # Color
+        cname = StyleDB.line_style[border_style].color
+        c_color = StyleDB.color[cname]
+        # Width
+        w = StyleDB.line_style[border_style].width
+        pen = QPen(QColor(*c_color), w)
+        # Pattern
+        pname = StyleDB.line_style[border_style].pattern  # name of line style's pattern
+        if pname != 'no dash':
+            pvalue = StyleDB.dash_pattern[pname]  # find pattern value in dash pattern dict
+            pen.setDashPattern([*pvalue])
+
+        if isinstance(item, QAbstractGraphicsShapeItem):
+            # item is fillable, not a line item, for example
+            if fill:
+                # If a fill is specified, create a corresponding brush
+                fill_rgb_color_value = StyleDB.color[fill]
+                brush = QBrush(QColor(*fill_rgb_color_value))
+                _logger.info(f"Brush color: [{fill}]")
+            else:
+                brush = QBrush(Qt.GlobalColor.transparent)
+            item.setBrush(brush)
+
+        # Set the item pen and brush of the graphic item
+        item.setPen(pen)
+        _logger.info(f"Pen color: [{cname}], width: [{w}], pattern: [{pname}]")
+
+    @classmethod
+    def choose_fill_only(cls, item: QAbstractGraphicsShapeItem, fill: FloatRGB):
+        """
+        Create and assign a transparent pen (no border) and brush for a given QT graphics item
+        with settings found in the StyleDB.
+
+        :param item: Qt graphic item to be displayed (must be a closed shape)
+        :param fill: RGB color
+        :return:
+        """
+        _logger.info(f"Brush color: {fill}")
+
+        pen = QPen(Qt.GlobalColor.transparent)
+        brush = QBrush(QColor(*fill))
+        item.setPen(pen)
+        item.setBrush(brush)
---
+++ packages/mi-tabletqt/mi_tabletqt-1.2.3.tar.gz/mi_tabletqt-1.2.3/src/tabletqt/graphics/polygon_se.py
@@ -0,0 +1,94 @@
+""" polygon_se.py -- Polygon Shape Element """
+
+# System
+import logging
+from typing import TYPE_CHECKING, List
+
+# Qt
+from PyQt6.QtWidgets import QGraphicsPolygonItem
+from PyQt6.QtCore import QPointF
+from PyQt6.QtGui import QPolygonF
+
+# Tablet
+from tabletqt.geometry_types import Position
+from tabletqt.graphics.crayon_box import CrayonBox
+from tabletqt.graphics.line_segment import LineSegment
+
+if TYPE_CHECKING:
+    from tabletqt.layer import Layer
+
+_logger = logging.getLogger(__name__)
+class PolygonSE:
+    """
+    Manage the rendering of Polygon Shape Elements which are Closed Shapes
+
+    Additionally, a contiguous sequence of Line Segements are managed here as an
+    open polygon shape. There is no concept of an open polygon in the class models so
+    the add_open method could just as easily be part of the Line Segment Element class.
+    In fact, there is no enforced constraint that the segments be contiguous.
+
+    (The idea was to support drawing a pattern like an open arrow head).
+
+    For legacy reasons, let's just leave it here for now.
+
+    Attributes and relationships defined on the class model
+
+    Subclass of Closed Shape on class model (R22)
+    Related via {R17} to an ordered set of Vertex instances
+    These are implemented as a supplied list of tablet positions
+
+    - ID {I} -- Element ID, unique within a Layer, implemented as object reference
+    - Layer {I, R22} -- Element drawn on this Layer via R22/R12/R15/Element/R19/Layer
+    """
+
+    @classmethod
+    def add(cls, layer: 'Layer', asset: str, vertices: List[Position]):
+        """
+        Add a closed polygon to the layer
+
+        :param layer: Draw on this layer
+        :param asset: Used to determine draw style
+        :param vertices: Polygon vertices in Tablet coordinates
+        """
+        # Flip each position to device coordinates
+        device_vertices = [layer.Tablet.to_dc(v) for v in vertices]
+        pverts = [QPointF(*v) for v in device_vertices]
+        polygon = QPolygonF(pverts)
+        poly_item = QGraphicsPolygonItem(polygon)
+
+        # Set pen and brush
+        border_style = layer.Presentation.Shape_presentation[asset]
+        fill = layer.Presentation.Closed_shape_fill[asset]
+        CrayonBox.choose_crayons(
+            item=poly_item,
+            border_style=border_style,
+            fill=fill)
+
+        # Add polygon item to layer
+        layer.Polygons.append(poly_item)
+
+    @classmethod
+    def add_open(cls, layer: 'Layer', asset: str, vertices: List[Position]):
+        """
+        Rather than manage a seperate render open polygon list in the Layer, we
+        can just add these to the line segment list.
+
+        :param layer: Draw on this layer
+        :param asset: Used to look up the line style
+        :param vertices: A sequences of 2 or more vertices
+        """
+        for v1, v2 in zip(vertices, vertices[1:]):
+            assert len(vertices) > 1, "Open polygon has less than two vertices"
+            LineSegment.add(layer=layer, asset=asset, from_here=v1, to_there=v2)
+
+    @classmethod
+    def render(cls, layer: 'Layer'):
+        """
+        Draw the closed non-rectangular shapes
+
+        :param layer: Draw on this layer
+        """
+        for p in layer.Polygons:
+            # _logger.info(f"> Polygon at: [{p.vertices}]")
+
+            layer.Scene.addItem(p)
