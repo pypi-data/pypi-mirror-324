644665123f6010f259c7517a205c338a8c8a3299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 04:05:13 +0000

    Add package proteobench/0.6.0/proteobench-0.6.0.tar.gz

---
+++ packages/proteobench/proteobench-0.6.0.tar.gz/proteobench-0.6.0/webinterface/Home.py
@@ -0,0 +1,12 @@
+"""Proteobench Streamlit-based web server."""
+
+from _base import StreamlitPage
+
+
+class StreamlitPageHome(StreamlitPage):
+    def _main_page(self):
+        pass
+
+
+if __name__ == "__main__":
+    StreamlitPageHome()
