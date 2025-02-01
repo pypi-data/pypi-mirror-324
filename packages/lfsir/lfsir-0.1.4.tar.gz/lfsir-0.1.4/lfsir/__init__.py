89c669c591324650d6ebe6687f05e4fdbde6ab7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-01 06:05:58 +0000

    Add package lfsir/0.1.4/lfsir-0.1.4.tar.gz

---
+++ packages/lfsir/lfsir-0.1.4.tar.gz/lfsir-0.1.4/lfsir/__init__.py
@@ -0,0 +1,17 @@
+from .api import (
+    load_table,
+    load_knowledge,
+    add_attribute,
+    add_classification,
+    setup,
+    setup_config,
+)
+
+__all__ = [
+    "load_table",
+    "load_knowledge",
+    "add_attribute",
+    "add_classification",
+    "setup",
+    "setup_config",
+]
