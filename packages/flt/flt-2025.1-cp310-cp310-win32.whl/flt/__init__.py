0e8c7c2c1db3be76412d34a3cb6b6310637a8cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:04 +0000

    Add package flt/2025.1/flt-2025.1-cp310-cp310-win32.whl

---
+++ packages/flt/flt-2025.1-cp310-cp310-win32.whl/flt/__init__.py
@@ -0,0 +1,19 @@
+"""
+Fast Legendre Transform
+"""
+
+__all__ = [
+    "dlt",
+    "idlt",
+    "theta",
+    "dct2dlt",
+    "dlt2dct",
+]
+
+from flt.generic import (
+    dlt,
+    idlt,
+    theta,
+    dct2dlt,
+    dlt2dct,
+)
