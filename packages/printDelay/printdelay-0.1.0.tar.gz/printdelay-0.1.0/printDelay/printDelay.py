64a93a4f47fdc857901b93787de1b4a158ecda62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:18 +0000

    Add package printDelay/0.1.0/printdelay-0.1.0.tar.gz

---
+++ packages/printDelay/printdelay-0.1.0.tar.gz/printdelay-0.1.0/printDelay/printDelay.py
@@ -0,0 +1,15 @@
+import time
+import sys
+
+#prints without a newline
+def printf (string,sec):
+    for x in string:
+        print(x,end='')
+        time.sleep(sec)
+    
+#prints with a newline
+def printl (string,sec):
+    for x in string:
+        print(x,end='')
+        time.sleep(sec)
+    print()
