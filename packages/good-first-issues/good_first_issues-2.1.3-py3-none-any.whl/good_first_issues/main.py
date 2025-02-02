2943bcafe0a1e067fa48bc2b2d5117e1efbc5848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:07:05 +0000

    Add package good-first-issues/2.1.3/good_first_issues-2.1.3-py3-none-any.whl

---
+++ packages/good-first-issues/good_first_issues-2.1.3-py3-none-any.whl/good_first_issues/main.py
@@ -0,0 +1,26 @@
+"""Entrypoint of the CLI"""
+
+import click
+from rich.console import Console
+
+from good_first_issues.commands import config, rate_limit, search
+
+console = Console(color_system="auto")
+
+
+@click.group()
+def cli():
+    """
+    Get good first issues to start hacking.
+
+    (Requires GitHub Authentication Token)
+
+    $ gfi search
+
+    """
+    pass
+
+
+cli.add_command(config)
+cli.add_command(search)
+cli.add_command(rate_limit)
