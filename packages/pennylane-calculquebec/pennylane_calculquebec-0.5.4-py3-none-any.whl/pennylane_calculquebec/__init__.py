c0b1b337c87489aad66cf61803ba330243a40813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:10 +0000

    Add package pennylane-calculquebec/0.5.4/pennylane_calculquebec-0.5.4-py3-none-any.whl

---
+++ packages/pennylane-calculquebec/pennylane_calculquebec-0.5.4-py3-none-any.whl/pennylane_calculquebec/__init__.py
@@ -0,0 +1,5 @@
+"""this is the top level module for the Pennylane Snowflurry plugin. It is used for communicating with MonarQ.
+"""
+
+from .snowflurry_device import SnowflurryQubitDevice
+from .monarq_device import MonarqDevice
