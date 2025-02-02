1f482e4a4eb811501e28c3afe10dfd24eabba024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:07:05 +0000

    Add package llama-stack-client/0.1.1/llama_stack_client-0.1.1.tar.gz

---
+++ packages/llama-stack-client/llama_stack_client-0.1.1.tar.gz/llama_stack_client-0.1.1/src/llama_stack_client/types/shared_params/agent_config.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, List, Union, Iterable
+from typing_extensions import Literal, Required, TypeAlias, TypedDict
+
+from ..tool_def_param import ToolDefParam
+from .response_format import ResponseFormat
+from .sampling_params import SamplingParams
+
+__all__ = ["AgentConfig", "Toolgroup", "ToolgroupUnionMember1"]
+
+
+class ToolgroupUnionMember1(TypedDict, total=False):
+    args: Required[Dict[str, Union[bool, float, str, Iterable[object], object, None]]]
+
+    name: Required[str]
+
+
+Toolgroup: TypeAlias = Union[str, ToolgroupUnionMember1]
+
+
+class AgentConfig(TypedDict, total=False):
+    enable_session_persistence: Required[bool]
+
+    instructions: Required[str]
+
+    max_infer_iters: Required[int]
+
+    model: Required[str]
+
+    client_tools: Iterable[ToolDefParam]
+
+    input_shields: List[str]
+
+    output_shields: List[str]
+
+    response_format: ResponseFormat
+
+    sampling_params: SamplingParams
+
+    tool_choice: Literal["auto", "required"]
+
+    tool_prompt_format: Literal["json", "function_tag", "python_list"]
+
+    toolgroups: List[Toolgroup]
