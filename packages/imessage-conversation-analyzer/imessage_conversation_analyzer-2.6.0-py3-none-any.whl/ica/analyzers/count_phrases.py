c61d36c1ec05f2371c0bdc3c9c03c55703318896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:06:58 +0000

    Add package imessage-conversation-analyzer/2.6.0/imessage_conversation_analyzer-2.6.0-py3-none-any.whl

---
+++ packages/imessage-conversation-analyzer/imessage_conversation_analyzer-2.6.0-py3-none-any.whl/ica/analyzers/count_phrases.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import re
+
+import pandas as pd
+
+import ica
+
+
+def main() -> None:
+
+    cli_parser = ica.get_cli_parser()
+    cli_parser.add_argument("phrases", nargs="+", help="one or more phrases to count")
+    cli_args = cli_parser.parse_args()
+    phrases = cli_args.phrases
+    dfs = ica.get_dataframes(
+        contact_name=cli_args.contact_name,
+        timezone=cli_args.timezone,
+        from_date=cli_args.from_date,
+        to_date=cli_args.to_date,
+        from_person=cli_args.from_person,
+    )
+    filtered_messages = dfs.messages[~dfs.messages["is_reaction"]]
+
+    ica.output_results(
+        (
+            pd.DataFrame(
+                {
+                    "phrase": phrases,
+                    "count": (
+                        filtered_messages["text"]
+                        .str.count(re.escape(phrase), flags=re.IGNORECASE)
+                        .sum()
+                        for phrase in phrases
+                    ),
+                }
+            ).set_index("phrase")
+        ),
+        format=cli_args.format,
+        output=cli_args.output,
+        prettify_index=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
