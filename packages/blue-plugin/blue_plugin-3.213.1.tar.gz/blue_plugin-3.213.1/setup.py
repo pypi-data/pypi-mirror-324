049ef66420afaf72c3469018fe2c0550b635ed6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:07:00 +0000

    Add package blue-plugin/3.213.1/blue_plugin-3.213.1.tar.gz

---
+++ packages/blue-plugin/blue_plugin-3.213.1.tar.gz/blue_plugin-3.213.1/setup.py
@@ -0,0 +1,24 @@
+from blue_plugin import NAME, VERSION, DESCRIPTION, REPO_NAME
+from blueness.pypi import setup
+
+setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[
+        NAME,
+        f"{NAME}.help",
+        f"{NAME}.help.node",
+        f"{NAME}.node",
+    ],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            "config.env",
+            "sample.env",
+            ".abcli/**/*.sh",
+        ],
+    },
+)
