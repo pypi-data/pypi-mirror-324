fd1a9f1769934f03100f28812cbe3f6d00f436e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 05:06:20 +0000

    Add package tcrutils/13.1.3/tcrutils-13.1.3-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-13.1.3-py3-none-any.whl/tcrutils/void.py
@@ -0,0 +1,37 @@
+from collections.abc import Awaitable, Callable
+from typing import Any, NoReturn
+
+
+def void(*args: Any, **kwargs: Any) -> None:
+	"""Synchronous voider: take any arguments and do nothing, useful in functions that require a callback when nothing is needed to be done."""
+
+
+async def avoid(*args: Any, **kwargs: Any) -> None:
+	"""Asynchronous voider: take any arguments and do nothing, useful in functions that require a callback when nothing is needed to be done."""
+
+
+def raiser(e: Exception):
+	"""With decorator-like structure return a synchronous callable which raises specified exception on call, no matter what (with *args, **kwargs which are ignored)."""
+
+	def inner_raiser(*args, **kwargs) -> NoReturn:
+		raise e
+
+	return inner_raiser
+
+
+async def araiser(e: Exception):
+	"""With decorator-like structure return an asynchronous callable which raises specified exception on call, no matter what (with *args, **kwargs which are ignored)."""
+
+	async def inner_raiser(*args, **kwargs) -> NoReturn:
+		raise e
+
+	return inner_raiser
+
+
+def alambda[T, **P](f: Callable[P, T]) -> Callable[P, Awaitable[T]]:
+	"""Asyncify any synchronous function, mostly lambdas when needed."""
+
+	async def wrapper(*args, **kwargs):
+		return f(*args, **kwargs)
+
+	return wrapper
