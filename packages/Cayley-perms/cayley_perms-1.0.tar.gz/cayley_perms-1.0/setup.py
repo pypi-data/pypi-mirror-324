9d73479085be14dab5400022e1ba6f25331a984f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:06:45 +0000

    Add package Cayley-perms/1.0/cayley_perms-1.0.tar.gz

---
+++ packages/Cayley-perms/cayley_perms-1.0.tar.gz/cayley_perms-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Cayley_perms",
+    version="1.0",
+    description="A module for using Cayley permutations.",
+    #    author='',
+    #    author_email='foomail@foo.example',
+    packages=["cperms"],  # same as name
+    # install_requires=["typing"],  # external packages as dependencies
+)
