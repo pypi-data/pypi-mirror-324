c7c58f4f33d16a5382dd682d9fb6f2ea7fa47628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 05:06:21 +0000

    Add package gemini-webapi/1.8.4/gemini_webapi-1.8.4-py3-none-any.whl

---
+++ packages/gemini-webapi/gemini_webapi-1.8.4-py3-none-any.whl/gemini_webapi/constants.py
@@ -0,0 +1,66 @@
+from enum import Enum
+
+
+class Endpoint(Enum):
+    INIT = "https://gemini.google.com/app"
+    GENERATE = "https://gemini.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate"
+    ROTATE_COOKIES = "https://accounts.google.com/RotateCookies"
+    UPLOAD = "https://content-push.googleapis.com/upload"
+
+
+class Headers(Enum):
+    GEMINI = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        "Host": "gemini.google.com",
+        "Origin": "https://gemini.google.com",
+        "Referer": "https://gemini.google.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "X-Same-Domain": "1",
+    }
+    ROTATE_COOKIES = {
+        "Content-Type": "application/json",
+    }
+    UPLOAD = {"Push-ID": "feeds/mcudyrk2a4khkz"}
+
+
+class Model(Enum):
+    UNSPECIFIED = ("unspecified", {}, False)
+    G_1_5_FLASH = (
+        "gemini-1.5-flash",
+        {"x-goog-ext-525001261-jspb": '[null,null,null,null,"418ab5ea040b5c43"]'},
+        False,
+    )
+    G_1_5_PRO = (
+        "gemini-1.5-pro",
+        {"x-goog-ext-525001261-jspb": '[null,null,null,null,"9d60dfae93c9ff1f"]'},
+        True,
+    )
+    G_1_5_PRO_RESEARCH = (
+        "gemini-1.5-pro-research",
+        {"x-goog-ext-525001261-jspb": '[null,null,null,null,"e5a44cb1dae2b489"]'},
+        True,
+    )
+    G_2_0_FLASH_EXP = (
+        "gemini-2.0-flash-exp",
+        {"x-goog-ext-525001261-jspb": '[null,null,null,null,"f299729663a2343f"]'},
+        False,
+    )
+    G_2_0_EXP_ADVANCED = (
+        "gemini-2.0-exp-advanced",
+        {"x-goog-ext-525001261-jspb": '[null,null,null,null,"b1e46a6037e6aa9f"]'},
+        True,
+    )
+
+    def __init__(self, name, header, advanced_only):
+        self.model_name = name
+        self.model_header = header
+        self.advanced_only = advanced_only
+
+    @classmethod
+    def from_name(cls, name: str):
+        for model in cls:
+            if model.model_name == name:
+                return model
+        raise ValueError(
+            f"Unknown model name: {name}. Available models: {', '.join([model.model_name for model in cls])}"
+        )
