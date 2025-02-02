912f35d5d05568110d0da9351d87033713490efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:29 +0000

    Add package econ-llm/0.1.16/econ_llm-0.1.16.tar.gz

---
+++ packages/econ-llm/econ_llm-0.1.16.tar.gz/econ_llm-0.1.16/src/econ_llm/main.py
@@ -0,0 +1,31 @@
+import sys
+from . import experiment as exp # multi round ultimatum game
+from . import ultimatum_exp as ult # multi stage ultimatum game
+import chromedriver_autoinstaller
+
+def main():
+    # Automatically install the latest version of chromedriver
+    chromedriver_autoinstaller.install()
+
+    # Check if the user provided the correct number of arguments
+    if len(sys.argv) < 2:
+        print("Usage: econ-llm run [experiment_name] [user_id]")
+        sys.exit(1)
+    
+    command = sys.argv[1]
+    if command == "run":
+        args = sys.argv[2:]
+        print(f"Running with arguments: {args}")
+        exp.run_experiment(*args)
+    elif command == "ult": # ult stands for ultimatum
+        print("Running ultimatum game experiment")
+        ult.run_ultimatum_experiment()
+    elif command == "upload": # upload the results to the server
+        print("Uploading results to the server")
+        exp.upload_results()
+    else:
+        print(f"Unknown command: {command}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
