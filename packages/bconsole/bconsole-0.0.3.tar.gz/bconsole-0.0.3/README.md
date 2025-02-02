bc77702636c65d4cf7fe4b8cc5cbe912f7d9e147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:10 +0000

    Add package bconsole/0.0.3/bconsole-0.0.3.tar.gz

---
+++ packages/bconsole/bconsole-0.0.3.tar.gz/bconsole-0.0.3/README.md
@@ -0,0 +1,9 @@
+# Console Helper
+
+A simple console helper for python.
+
+Requires [colorama](https://pypi.org/project/colorama/) for windows compatibility
+
+## Usage
+
+See bconsole/examples for examples.
---
+++ packages/bconsole/bconsole-0.0.3.tar.gz/bconsole-0.0.3/bconsole/__init__.py
@@ -0,0 +1,8 @@
+"""A simple module to make it a little less painful to make console applications."""
+
+from .console import Background, Console, Cursor, Erase, Foreground, Modifier
+
+__author__ = "BetaKors"
+__version__ = "0.0.2"
+__license__ = "MIT"
+__all__ = ["Background", "Console", "Cursor", "Erase", "Foreground", "Modifier"]
