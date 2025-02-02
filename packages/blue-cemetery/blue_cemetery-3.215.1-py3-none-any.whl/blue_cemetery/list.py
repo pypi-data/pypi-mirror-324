fec178796e41dab681a12078ceb5d6f5de27b55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:06:41 +0000

    Add package blue-cemetery/3.215.1/blue_cemetery-3.215.1-py3-none-any.whl

---
+++ packages/blue-cemetery/blue_cemetery-3.215.1-py3-none-any.whl/blue_cemetery/list.py
@@ -0,0 +1,30 @@
+list_of_experiments = {
+    "sagesemseg": {
+        "title": "A SemSeg (Semantic Segmenter) trained and deployed on AWS Sagemaker.",
+        "url": "./blue_sandbox/sagesemseg/README.md",
+        "marquee": "https://github.com/kamangir/assets/blob/main/blue-sandbox/sagesemseg-predict.png?raw=true",
+    },
+    "`@damages`": {
+        "title": "Satellite imagery damage assessment workflow",
+        "url": "./blue_sandbox/microsoft_building_damage_assessment/README.md",
+        "marquee": "https://github.com/kamangir/assets/raw/main/blue-sandbox/Maui-Hawaii-fires-Aug-23-ingest-2025-01-10-qqJqhm.png?raw=true",
+    },
+    "VisuaLyze": {
+        "title": 'How about calling it "VisuaLyze"? ... - OpenAI',
+        "url": "./blue_sandbox/VisuaLyze",
+        "marquee": "https://github.com/kamangir/openai-commands/assets/1007567/7c0ed5f7-6941-451c-a17e-504c6adab23f",
+    },
+    "gpt": {
+        "title": "co-authored with ChapGPT.",
+        "url": "./blue_sandbox/cemetery/gpt",
+    },
+    "code generation": {
+        "title": "",
+        "marquee": "https://github.com/kamangir/openai-commands/blob/main/assets/completion_i2i_function.png?raw=true",
+        "url": "./blue_sandbox/cemetery/code_generation",
+    },
+    "template": {
+        "title": "",
+        "url": "",
+    },
+}
