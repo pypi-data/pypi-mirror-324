6fe4b8951d6fec3a3ee7f345e3b6def1753493eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:23 +0000

    Add package connecpy/1.3.1/connecpy-1.3.1.tar.gz

---
+++ packages/connecpy/connecpy-1.3.1.tar.gz/connecpy-1.3.1/example/async_client.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import httpx
+
+from connecpy.context import ClientContext
+from connecpy.exceptions import ConnecpyServerException
+
+import haberdasher_connecpy, haberdasher_pb2
+
+
+server_url = "http://localhost:3000"
+timeout_s = 5
+
+
+async def main():
+    session = httpx.AsyncClient(
+        base_url=server_url,
+        timeout=timeout_s,
+    )
+    client = haberdasher_connecpy.AsyncHaberdasherClient(server_url, session=session)
+
+    try:
+        response = await client.MakeHat(
+            ctx=ClientContext(),
+            request=haberdasher_pb2.Size(inches=12),
+            # Optionally provide a session per request
+            # session=session,
+            headers={
+                "Accept-Encoding": "gzip",
+            },
+        )
+        if not response.HasField("name"):
+            print("We didn't get a name!")
+        print(response)
+    except ConnecpyServerException as e:
+        print(e.code, e.message, e.to_dict())
+    finally:
+        # Close the session (could also use a context manager)
+        await session.aclose()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
