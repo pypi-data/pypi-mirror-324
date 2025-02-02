a050d4b9c0ac778b41112637ed600f2838241691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:06:30 +0000

    Add package styled-print/0.1.3/styled_print-0.1.3.tar.gz

---
+++ packages/styled-print/styled_print-0.1.3.tar.gz/styled_print-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='styled_print',
+    version='0.1.3',  
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'termcolor',  
+    ],
+    author='Usmonov Shoxruxmirzo',
+    author_email='usmonovshohruxmirzo@gmail.com',
+    description='A flexible terminal text styling utility for color, background, and custom formatting options.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/webbro-software/styled_print',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
