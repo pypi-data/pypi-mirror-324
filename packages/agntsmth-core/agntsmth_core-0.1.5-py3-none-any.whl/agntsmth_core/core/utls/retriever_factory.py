157796777629fdeb72911d6d5434f927508f7c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 05:06:20 +0000

    Add package agntsmth-core/0.1.5/agntsmth_core-0.1.5-py3-none-any.whl

---
+++ packages/agntsmth-core/agntsmth_core-0.1.5-py3-none-any.whl/agntsmth_core/core/utls/retriever_factory.py
@@ -0,0 +1,38 @@
+import yaml
+import json
+from typing import Annotated, Type, Any, List, Dict
+from pydantic import BaseModel, Field
+
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain.tools.retriever import create_retriever_tool
+from langchain_core.tools import BaseTool
+from langchain_core.vectorstores import VectorStoreRetriever
+
+from ..utls import create_retriever, ChromaHttpClientFactory, log, EmbeddingFactory
+
+
+class RetrieverFactory:
+
+    @staticmethod
+    def create(collection_name: str, /, **kwargs: Dict[str, Any]) -> VectorStoreRetriever:
+        """
+        A vector store retriever factory.
+        """
+
+        chroma_client = kwargs.get("chroma_client", None)
+        if not chroma_client:
+            chroma_client = ChromaHttpClientFactory.create()
+
+        embedding_function = kwargs.get("embedding_function", None)
+        if not embedding_function:
+            embedding_function = EmbeddingFactory.create()
+
+        retriever = create_retriever(
+            collection_name=collection_name,
+            chroma_client=chroma_client,
+            embedding_function=embedding_function,
+        )
+
+        return retriever
