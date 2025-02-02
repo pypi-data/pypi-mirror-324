508551e76aa7133a1cf9abef521b8ac9863345e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:05:56 +0000

    Add package perfect-cmaps/1.2.2/perfect_cmaps-1.2.2-py3-none-any.whl

---
+++ packages/perfect-cmaps/perfect_cmaps-1.2.2-py3-none-any.whl/perfect_cmaps/__init__.py
@@ -0,0 +1,7 @@
+from .colormaps import get_colormap
+from .customize_cmap import create_custom_colormap
+from .storage import get_available_colormaps
+
+
+__all__ = ["get_colormap", "create_custom_colormap", "get_available_colormaps"]
+__version__ = "1.2.2"
