5095807885865a12f0e455d0ad42a9a3baf50124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:14 +0000

    Add package camera-lib/0.1.0.post202502011631/camera_lib-0.1.0.post202502011631.tar.gz

---
+++ packages/camera-lib/camera_lib-0.1.0.post202502011631.tar.gz/camera_lib-0.1.0.post202502011631/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import datetime
+
+# Generate a timestamp-based version (e.g., 202501311230)
+VERSION = "0.1.0-" + datetime.datetime.now().strftime("%Y%m%d%H%M")
+DESC = "A camera package for Web and Wi-Fi cameras management"
+LONG_DESC = "A camera package for Web and Wi-Fi cameras management for start/stop streaming. Web Cameras are based on PyQt5, Wi-Fi cameras are based on VLC."
+
+setup(
+    name="camera-lib",
+    version=VERSION,  # Auto-generated timestamp version
+    packages=find_packages(where='src'),  # This ensures proper package discovery
+    package_dir={"": "camera"},  # Maps packages correctly under "src/"
+    install_requires=['PyQt5', 'vlc'],  # Dependencies
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Adjust as needed
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify supported Python versions
+)
