713d61d58f0bfa21995bb12afdcff31573eea3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:08 +0000

    Add package compas-notebook/0.9.0/compas_notebook-0.9.0.tar.gz

---
+++ packages/compas-notebook/compas_notebook-0.9.0.tar.gz/compas_notebook-0.9.0/src/compas_notebook/scene/graphobject.py
@@ -0,0 +1,42 @@
+import pythreejs as three
+from compas.scene import GraphObject
+
+from compas_notebook.conversions import nodes_and_edges_to_threejs
+from compas_notebook.conversions import nodes_to_threejs
+from compas_notebook.scene import ThreeSceneObject
+
+
+class ThreeGraphObject(ThreeSceneObject, GraphObject):
+    """Scene object for drawing graph."""
+
+    def draw(self):
+        """Draw the graph associated with the scene object.
+
+        Returns
+        -------
+        list[three.LineSegments, three.Points]
+            List of pythreejs objects created.
+
+        """
+        guids = []
+
+        nodes, edges = self.graph.to_nodes_and_edges()
+
+        if self.show_nodes:
+            if self.show_nodes is not True:
+                nodes = self.show_nodes
+
+            geometry = nodes_to_threejs(nodes)
+            points = three.Points(geometry, three.PointsMaterial(size=self.nodesize, color=self.contrastcolor.hex))
+            guids.append(points)
+
+        if self.show_edges:
+            if self.show_edges is not True:
+                edges = self.show_edges
+
+            geometry = nodes_and_edges_to_threejs(nodes, edges)
+            line = three.LineSegments(geometry, three.LineBasicMaterial(color=self.contrastcolor.hex))
+            guids.append(line)
+
+        self._guids = guids
+        return self.guids
