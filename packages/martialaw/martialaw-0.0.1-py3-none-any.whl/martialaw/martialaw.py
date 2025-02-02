55b47708ea160279319af2dcad23d99328963bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:22 +0000

    Add package martialaw/0.0.1/martialaw-0.0.1-py3-none-any.whl

---
+++ packages/martialaw/martialaw-0.0.1-py3-none-any.whl/martialaw/martialaw.py
@@ -0,0 +1,13 @@
+#martialaw.py
+
+from functools import partial
+
+'''
+# martialaw.py
+function : martialaw(f) to use closer.
+PPP Powerfuler Programming by closer Partion, do not impeachment, use closer.
+
+(and I said don't fight with it. It just a joke. plz. I don't care about martial law)
+'''
+
+martialaw = partial(partial, partial)
