d6e16552da0b8234afa642fe27822c58d8f154f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:57 +0000

    Add package edgy/0.26.1/edgy-0.26.1-py3-none-any.whl

---
+++ packages/edgy/edgy-0.26.1-py3-none-any.whl/edgy/core/db/constants.py
@@ -0,0 +1,20 @@
+CASCADE = "CASCADE"
+RESTRICT = "RESTRICT"
+DO_NOTHING = "DO NOTHING"
+SET_NULL = "SET NULL"
+SET_DEFAULT = "SET DEFAULT"
+PROTECT = "PROTECT"
+
+
+class ConditionalRedirect(dict): ...
+
+
+__all__ = [
+    "CASCADE",
+    "RESTRICT",
+    "DO_NOTHING",
+    "SET_NULL",
+    "SET_DEFAULT",
+    "PROTECT",
+    "ConditionalRedirect",
+]
