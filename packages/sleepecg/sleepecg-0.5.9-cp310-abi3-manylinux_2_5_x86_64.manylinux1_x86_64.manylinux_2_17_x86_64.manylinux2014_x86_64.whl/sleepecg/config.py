60b724edf1db9be33b8be3f8345640f68b55704a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:07:22 +0000

    Add package sleepecg/0.5.9/sleepecg-0.5.9-cp310-abi3-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/sleepecg/sleepecg-0.5.9-cp310-abi3-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/sleepecg/config.py
@@ -0,0 +1,119 @@
+# © SleepECG developers
+#
+# License: BSD (3-clause)
+
+"""Functions for getting and setting configuration values."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+import yaml
+
+_DEFAULT_CONFIG_PATH = Path(__file__).parent / "config.yml"
+_USER_CONFIG_PATH = Path("~/.sleepecg/config.yml").expanduser()
+
+
+def _read_yaml(path: Path) -> dict[str, Any]:
+    try:
+        with open(path) as file:
+            cfg = yaml.safe_load(file)
+    except FileNotFoundError:
+        return {}
+
+    # empty .yml-files are loaded as `None`
+    if cfg is None:
+        return {}
+    if not isinstance(cfg, dict):
+        raise ValueError(f"Invalid YAML config file at {path}")
+    return cfg
+
+
+def get_config() -> dict[str, str]:
+    """
+    Read SleepECG configuration.
+
+    For parameters not set in the user configuration file (`~/.sleepecg/config.yml`), this
+    falls back to the default values defined in `site-packages/sleepecg/config.yml`. See
+    [configuration](../configuration.md) for a list of possible settings.
+
+    Returns
+    -------
+    dict[str, str]
+        All configuration keys and values.
+    """
+    config = _read_yaml(_DEFAULT_CONFIG_PATH)
+    user_config = _read_yaml(_USER_CONFIG_PATH)
+    if invalid_keys := set(user_config) - set(config):
+        raise ValueError(
+            f"Invalid key(s) found in user config at {_USER_CONFIG_PATH}: {invalid_keys}"
+        )
+    config.update(user_config)
+    return config
+
+
+def get_config_value(key: str) -> str:
+    """
+    Read specific SleepECG configuration value.
+
+    For parameters not set in the user configuration file (`~/.sleepecg/config.yml`), this
+    falls back to the default values defined in `site-packages/sleepecg/config.yml`. See
+    [configuration](../configuration.md) for a list of possible settings.
+
+    Parameters
+    ----------
+    key : str
+        The configuration key to look for.
+
+    Returns
+    -------
+    str
+        The configuration value.
+    """
+    config = get_config()
+
+    if key not in config:
+        options = ", ".join(config)
+        raise ValueError(
+            f"Trying to get invalid config key: {key!r}, possible options: {options}"
+        )
+    return config[key]
+
+
+def set_config(**kwargs: Any) -> None:
+    """
+    Set SleepECG preferences and store them to the user configuration file.
+
+    If a value is `None`, the corresponding key is deleted from the user configuration. See
+    [configuration](../configuration.md) for a list of possible settings.
+
+    Parameters
+    ----------
+    **kwargs: dict, optional
+        The configuration keys and values to set.
+
+    Examples
+    --------
+    >>> set_config(data_dir='~/.sleepecg/datasets')
+    """
+    default_config = _read_yaml(_DEFAULT_CONFIG_PATH)
+    user_config = _read_yaml(_USER_CONFIG_PATH)
+
+    # validate all parameters before setting anything
+    for key, value in kwargs.items():
+        if key not in default_config:
+            options = ", ".join(default_config)
+            raise ValueError(
+                f"Trying to set invalid config key: {key!r}, possible options: {options}"
+            )
+
+    for key, value in kwargs.items():
+        if value is None:
+            user_config.pop(key, None)
+        else:
+            user_config[key] = value
+
+    _USER_CONFIG_PATH.parent.mkdir(parents=True, exist_ok=True)
+    with open(_USER_CONFIG_PATH, "w") as user_config_file:
+        yaml.dump(user_config, user_config_file)
---
+++ packages/sleepecg/sleepecg-0.5.9-cp310-abi3-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/sleepecg/io/utils.py
@@ -0,0 +1,95 @@
+# © SleepECG developers
+#
+# License: BSD (3-clause)
+
+"""I/O- and download-related utilities."""
+
+from __future__ import annotations
+
+import hashlib
+from pathlib import Path
+
+import requests
+
+_HASH_FUNCTIONS = {
+    "md5": hashlib.md5,
+    "sha256": hashlib.sha256,
+}
+
+
+def _calculate_checksum(filepath: Path, checksum_type: str) -> str:
+    """
+    Calculate the checksum for a file.
+
+    Parameters
+    ----------
+    filepath : pathlib.Path
+        Location of the file.
+    checksum_type : {'md5', 'sha256'}
+        Type of the checksum to calculate.
+
+    Returns
+    -------
+    str
+        The hexdigest of the checksum.
+    """
+    computed_hash = _HASH_FUNCTIONS[checksum_type]()
+    with open(filepath, "rb") as file:
+        while chunk := file.read(8192):
+            computed_hash.update(chunk)
+    return computed_hash.hexdigest()
+
+
+def _download_file(
+    url: str,
+    target_filepath: Path,
+    checksum: str | None = None,
+    checksum_type: str | None = None,
+    verbose: bool = False,
+) -> None:
+    """
+    Download a single file from `url` to `target_filepath`.
+
+    In case `checksum` and `checksum_type` are provided, the downloaded file is verified.
+    Raises a `RuntimeError` if verification fails.
+
+    Parameters
+    ----------
+    url : str
+        URL to download from.
+    target_filepath : pathlib.Path
+        Location where the downloaded file will be stored.
+    checksum : str, optional
+        Checksum to verify the file against, by default `None`.
+    checksum_type : {'md5', 'sha256'}, optional
+        Type of the checksum, by default `None`.
+    verbose : bool, optional
+        If `True`, output information during download. By default `False`.
+    """
+    if target_filepath.is_file():
+        if checksum is not None and checksum_type is not None:
+            calculated_checksum = _calculate_checksum(target_filepath, checksum_type)
+            if calculated_checksum == checksum:
+                if verbose:
+                    print(f"Skipping {url}, already downloaded.")
+                return
+
+    target_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+    if verbose:
+        print(f"Downloading {url}...")
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(target_filepath, "wb") as file:
+        file.write(response.content)
+
+    if checksum is not None and checksum_type is not None:
+        calculated_checksum = _calculate_checksum(target_filepath, checksum_type)
+        if calculated_checksum != checksum:
+            raise RuntimeError(
+                f"Checksum mismatch for {target_filepath}:\n"
+                f"    {checksum!r} (expected)\n"
+                f"    {calculated_checksum!r} (calculated)"
+            )
