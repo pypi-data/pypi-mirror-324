6250b07e40e656c53aa2c4ba1cdd39772163e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 05:06:24 +0000

    Add package homoglyphs-fork/2.1.1/homoglyphs_fork-2.1.1-py2.py3-none-any.whl

---
+++ packages/homoglyphs-fork/homoglyphs_fork-2.1.1-py2.py3-none-any.whl/homoglyphs_fork/__init__.py
@@ -0,0 +1,19 @@
+"""
+Homoglyphs
+
+* Get similar letters
+* Convert string to ASCII letters
+* Detect possible letter languages
+* Detect letter UTF-8 group.
+"""
+
+# main package info
+__title__ = 'Homoglyphs'
+__author__ = 'Gram Orsinium'
+__license__ = 'MIT'
+
+
+from .core import ( # noQA
+    Categories, Languages, Homoglyphs,
+    STRATEGY_LOAD, STRATEGY_IGNORE, STRATEGY_REMOVE
+)
