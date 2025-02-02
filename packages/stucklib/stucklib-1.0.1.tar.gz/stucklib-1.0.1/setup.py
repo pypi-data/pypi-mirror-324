b69ea522af5603474ae7912996ec62ebf4fd6cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:23 +0000

    Add package stucklib/1.0.1/stucklib-1.0.1.tar.gz

---
+++ packages/stucklib/stucklib-1.0.1.tar.gz/stucklib-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='stucklib',
+    version='1.0.1',
+    description='shhhhhhhhh',
+
+
+    author='Skib Skibidi',
+    author_email='your@email.com',
+
+    license='MIT',
+
+    packages=['stucklib'],
+)
