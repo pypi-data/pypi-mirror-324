fb761a35a214f522adcfa801ecf54b7ae08229d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:06:33 +0000

    Add package pokpok-sdk/0.1.4/pokpok_sdk-0.1.4-py3-none-any.whl

---
+++ packages/pokpok-sdk/pokpok_sdk-0.1.4-py3-none-any.whl/pokpok_sdk/constants.py
@@ -0,0 +1,5 @@
+BACKEND_BASE_URL = "http://localhost:6500/api/v1"
+QUOTE_ENDPOINT = "/sdk/merchant/quote/"
+
+ALCHEMY_URL = "https://base-sepolia.g.alchemy.com/v2"
+PROTOCOL_JSON = 'protocolAbi.json'
