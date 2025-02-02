3e37c3439c1e593b840f35c93d0247d20e66a201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:09 +0000

    Add package agentuity/0.0.5.dev0/agentuity-0.0.5.dev0.tar.gz

---
+++ packages/agentuity/agentuity-0.0.5.dev0.tar.gz/agentuity-0.0.5.dev0/src/agentuity/__init__.py
@@ -0,0 +1,24 @@
+from agentuity.bridge import Agentuity
+from typing import Any, Dict
+
+print("Agentuity __init__ starting")
+
+instance = Agentuity()
+
+print("Agentuity __init__ after instance")
+
+def init(lib_path: str = None):
+    return instance.init(lib_path)
+
+def echo(data: Dict[str, Any] = None) -> Dict[str, Any]:
+    return instance.echo(data)
+
+def event(data: Dict[str, Any]) -> None:
+    return instance.event(data)
+
+def version() -> str:
+    return instance.version()
+
+__version__ = "0.0.5-dev"
+
+print("Agentuity __init__ loaded")
