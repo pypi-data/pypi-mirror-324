ab1d89d79bd9329283e0c4e56eab222fd1caa6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:35 +0000

    Add package blue-assistant/4.24.1/blue_assistant-4.24.1.tar.gz

---
+++ packages/blue-assistant/blue_assistant-4.24.1.tar.gz/blue_assistant-4.24.1/blue_assistant/.abcli/tests/help.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+function test_blue_assistant_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@assistant" \
+        \
+        "@assistant pypi" \
+        "@assistant pypi browse" \
+        "@assistant pypi build" \
+        "@assistant pypi install" \
+        \
+        "@assistant pytest" \
+        \
+        "@assistant test" \
+        "@assistant test list" \
+        \
+        "@assistant browse" \
+        \
+        "@assistant chat" \
+        \
+        "blue_assistant"; do
+        abcli_eval ,$options \
+            abcli_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
