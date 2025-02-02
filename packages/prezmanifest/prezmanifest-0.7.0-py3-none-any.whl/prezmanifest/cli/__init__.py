922ffb52ac185db7c2852e047880aba986252553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:16 +0000

    Add package prezmanifest/0.7.0/prezmanifest-0.7.0-py3-none-any.whl

---
+++ packages/prezmanifest/prezmanifest-0.7.0-py3-none-any.whl/prezmanifest/cli/__init__.py
@@ -0,0 +1,11 @@
+from prezmanifest.cli.app import app
+from prezmanifest.cli.commands.document import app as document_app
+from prezmanifest.cli.commands.label import app as label_app
+from prezmanifest.cli.commands.load import app as load_app
+from prezmanifest.cli.commands.validate import app as validate_app
+from prezmanifest.cli.commands.validate import validate_command
+
+app.add_typer(document_app, name="document")
+app.add_typer(label_app, name="label")
+app.add_typer(load_app, name="load")
+# app.add_typer(validate_command)
