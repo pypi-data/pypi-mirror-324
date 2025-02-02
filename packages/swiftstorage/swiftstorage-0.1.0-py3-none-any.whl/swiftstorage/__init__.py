27d9ab0eb9f186f2f5612a8b4c9d5caf856b3bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:02 +0000

    Add package swiftstorage/0.1.0/swiftstorage-0.1.0-py3-none-any.whl

---
+++ packages/swiftstorage/swiftstorage-0.1.0-py3-none-any.whl/swiftstorage/__init__.py
@@ -0,0 +1,16 @@
+"""
+swiftstorage
+============
+
+Copyright (c) 2025 Sean Yeatts. All rights reserved.
+
+A simple paradigm for local and remote file manipulation.
+"""
+
+from __future__ import annotations
+
+__title__       = "easystorage"
+__author__      = "Sean Yeatts"
+__copyright__   = "Copyright (c) 2025 Sean Yeatts. All rights reserved."
+
+from .swiftstorage import *
