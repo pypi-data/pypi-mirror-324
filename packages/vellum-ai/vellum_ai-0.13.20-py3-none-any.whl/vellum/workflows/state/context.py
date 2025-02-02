12386f224d13f81c5af08689d05459082c352bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:07:03 +0000

    Add package vellum-ai/0.13.20/vellum_ai-0.13.20-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-0.13.20-py3-none-any.whl/vellum/workflows/state/context.py
@@ -0,0 +1,42 @@
+from functools import cached_property
+from queue import Queue
+from typing import TYPE_CHECKING, Optional
+
+from vellum import Vellum
+from vellum.workflows.events.types import ParentContext
+from vellum.workflows.vellum_client import create_vellum_client
+
+if TYPE_CHECKING:
+    from vellum.workflows.events.workflow import WorkflowEvent
+
+
+class WorkflowContext:
+    def __init__(
+        self,
+        *,
+        vellum_client: Optional[Vellum] = None,
+        parent_context: Optional[ParentContext] = None,
+    ):
+        self._vellum_client = vellum_client
+        self._parent_context = parent_context
+        self._event_queue: Optional[Queue["WorkflowEvent"]] = None
+
+    @cached_property
+    def vellum_client(self) -> Vellum:
+        if self._vellum_client:
+            return self._vellum_client
+
+        return create_vellum_client()
+
+    @cached_property
+    def parent_context(self) -> Optional[ParentContext]:
+        if self._parent_context:
+            return self._parent_context
+        return None
+
+    def _emit_subworkflow_event(self, event: "WorkflowEvent") -> None:
+        if self._event_queue:
+            self._event_queue.put(event)
+
+    def _register_event_queue(self, event_queue: Queue["WorkflowEvent"]) -> None:
+        self._event_queue = event_queue
