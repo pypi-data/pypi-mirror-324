7a03b819db9b5046e2df0a118ba25cb8d1c11d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:06:57 +0000

    Add package pyliz-ai/0.2.0/pyliz_ai-0.2.0.tar.gz

---
+++ packages/pyliz-ai/pyliz_ai-0.2.0.tar.gz/pyliz_ai-0.2.0/pylizai/model/ai_model_list.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class AiModelList(Enum):
+    LLAVA = "llava"
+    WHISPER = "whisper"
+    PIXSTRAL = "pixstral"
+    OPEN_MISTRAL = "open_mistral"
+    GEMINI = "gemini"
+    LLAMA_3 = "llama3"
+    LLAMA_32 = "llama32"
+    OLlAMA_CUSTOM = "ollama_custom"
