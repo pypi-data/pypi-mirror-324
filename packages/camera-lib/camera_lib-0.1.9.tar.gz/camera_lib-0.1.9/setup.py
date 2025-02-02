3a43710138c298727224d143cb0598e52d4c10e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:09 +0000

    Add package camera-lib/0.1.9/camera_lib-0.1.9.tar.gz

---
+++ packages/camera-lib/camera_lib-0.1.9.tar.gz/camera_lib-0.1.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.9"
+DESC = "A camera package for Web and Wi-Fi cameras management"
+LONG_DESC = "A camera package for Web and Wi-Fi cameras management for start/stop streaming. Web Cameras are based on PyQt5, Wi-Fi cameras are based on VLC."
+
+setup(
+    name="camera-lib",
+    version=VERSION,  # Auto-generated timestamp version
+    packages=find_packages(where='src'),  # This ensures proper package discovery
+    package_dir={"": "camera"},  # Maps packages correctly under "src/"
+    install_requires=[],  # Dependencies
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Adjust as needed
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify supported Python versions
+)
