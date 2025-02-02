6e316118011bf10d60be2a7e600731a40322ffe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:07:24 +0000

    Add package python-in-production/0.0.12/python_in_production-0.0.12-py3-none-any.whl

---
+++ packages/python-in-production/python_in_production-0.0.12-py3-none-any.whl/py_packaging/hello.py
@@ -0,0 +1,14 @@
+from __future__ import (
+    absolute_import,
+    annotations,
+)
+
+
+def main():
+    """Main function!"""
+
+    print("Hello from python-in-production!")
+
+
+if __name__ == "__main__":
+    main()
