369c215f4e494389ec562180746880ca33d06a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:08 +0000

    Add package xsget/0.1.25/xsget-0.1.25.tar.gz

---
+++ packages/xsget/xsget-0.1.25.tar.gz/xsget-0.1.25/tests/conftest.py
@@ -0,0 +1,21 @@
+# pylint: disable=C0114,C0116
+
+import pytest
+from scripttest import TestFileEnvironment
+
+
+# See https://stackoverflow.com/a/62055409
+@pytest.fixture(autouse=True)
+def _change_test_dir(monkeypatch, tmp_path):
+    monkeypatch.chdir(tmp_path)
+
+
+@pytest.fixture(autouse=True, name="script_runner")
+def fixture_script_runner(tmpdir):
+    def script_runner(cmd, *args, **kwargs):
+        cwd = tmpdir / "scripttest"
+        env = TestFileEnvironment(cwd)
+        kwargs.update({"cwd": cwd, "expect_error": True})
+        return env.run(cmd, *args, **kwargs)
+
+    return script_runner
