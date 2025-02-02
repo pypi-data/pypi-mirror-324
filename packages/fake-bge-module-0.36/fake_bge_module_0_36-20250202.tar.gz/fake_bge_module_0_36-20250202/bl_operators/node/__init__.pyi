616e6b8fd786ddd013336097529cd2f1397867f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:42 +0000

    Add package fake-bge-module-0.36/20250202/fake_bge_module_0_36-20250202.tar.gz

---
+++ packages/fake-bge-module-0.36/fake_bge_module_0_36-20250202.tar.gz/fake_bge_module_0_36-20250202/bl_operators/node/__init__.pyi
@@ -0,0 +1,199 @@
+import typing
+import collections.abc
+import typing_extensions
+import bpy.types
+
+class NODE_OT_add_node(NodeAddOperator, bpy.types.Operator):
+    """Add a node to the active tree"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    @classmethod
+    def description(cls, _context, properties):
+        """
+
+        :param _context:
+        :param properties:
+        """
+
+    def execute(self, context):
+        """
+
+        :param context:
+        """
+
+class NODE_OT_add_simulation_zone(NodeAddOperator, bpy.types.Operator):
+    """Add simulation zone input and output nodes to the active tree"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+    input_node_type: typing.Any
+    output_node_type: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def execute(self, context):
+        """
+
+        :param context:
+        """
+
+class NODE_OT_collapse_hide_unused_toggle(bpy.types.Operator):
+    """Toggle collapsed nodes and hide unused sockets"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def execute(self, context):
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context):
+        """
+
+        :param context:
+        """
+
+class NODE_OT_tree_path_parent(bpy.types.Operator):
+    """Go to parent node tree"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def execute(self, context):
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context):
+        """
+
+        :param context:
+        """
+
+class NodeAddOperator:
+    def create_node(self, context, node_type):
+        """
+
+        :param context:
+        :param node_type:
+        """
+
+    @staticmethod
+    def deselect_nodes(context):
+        """
+
+        :param context:
+        """
+
+    def invoke(self, context, event):
+        """
+
+        :param context:
+        :param event:
+        """
+
+    @classmethod
+    def poll(cls, context):
+        """
+
+        :param context:
+        """
+
+    @staticmethod
+    def store_mouse_cursor(context, event):
+        """
+
+        :param context:
+        :param event:
+        """
+
+class NodeSetting(bpy.types.PropertyGroup):
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
---
+++ packages/fake-bge-module-0.36/fake_bge_module_0_36-20250202.tar.gz/fake_bge_module_0_36-20250202/bpy_extras/node_utils/__init__.pyi
@@ -0,0 +1,12 @@
+import typing
+import collections.abc
+import typing_extensions
+
+def connect_sockets(input, output):
+    """Connect sockets in a node tree.This is useful because the links created through the normal Python API are
+    invalid when one of the sockets is a virtual socket (grayed out sockets in
+    Group Input and Group Output nodes).It replaces node_tree.links.new(input, output)
+
+    """
+
+def find_node_input(node, name): ...
