a17d3e638592272a5397bde91ee4801eae63bbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:05:56 +0000

    Add package wangfumath/0.1.1/wangfumath-0.1.1.tar.gz

---
+++ packages/wangfumath/wangfumath-0.1.1.tar.gz/wangfumath-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wangfumath",
+    version="0.1.1",
+    description="A simple Python package for math operations and geometry calculations",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/wangfumath/wangfumath-0.1.1.tar.gz/wangfumath-0.1.1/test/test_mypackage.py
@@ -0,0 +1,5 @@
+import wangfumath
+
+print(wangfumath.operations.add(2, 3))
+print(wangfumath.geometry.circle_area(5))
+
