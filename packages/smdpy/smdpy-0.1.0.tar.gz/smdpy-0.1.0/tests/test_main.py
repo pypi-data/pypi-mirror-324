d645312837f2ecd178b5916aa431e3b3749e5f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:49 +0000

    Add package smdpy/0.1.0/smdpy-0.1.0.tar.gz

---
+++ packages/smdpy/smdpy-0.1.0.tar.gz/smdpy-0.1.0/tests/test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from src.main import github
+
+
+class TestMain(unittest.TestCase):
+    def test_github(self):
+        self.assertEqual(github(), 200)
+        self.assertNotEqual(github(), 404)
+        self.assertNotEqual(github(), 500)
+
+
+if __name__ == "__main__":
+    unittest.main()
