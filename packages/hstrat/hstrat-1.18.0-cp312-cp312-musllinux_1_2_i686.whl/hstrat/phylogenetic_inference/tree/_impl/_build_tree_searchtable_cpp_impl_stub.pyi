fa72279096414546f286f05a51dd271e87a94e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 15:05:14 +0000

    Add package hstrat/1.18.0/hstrat-1.18.0-cp312-cp312-musllinux_1_2_i686.whl

---
+++ packages/hstrat/hstrat-1.18.0-cp312-cp312-musllinux_1_2_i686.whl/hstrat/phylogenetic_inference/tree/_impl/_build_tree_searchtable_cpp_impl_stub.pyi
@@ -0,0 +1,34 @@
+import typing
+from unittest import mock
+
+import numpy as np
+import tqdm
+
+class Records:
+    def __init__(self, init_size: int) -> None: ...
+
+def copy_records_to_dict(records: Records) -> dict[str, np.ndarray]: ...
+def extract_records_to_dict(records: Records) -> dict[str, np.ndarray]: ...
+def collapse_dropped_unifurcations(records: Records) -> Records: ...
+def extend_tree_searchtable_cpp_from_exploded(
+    records: Records,
+    data_ids: np.ndarray,
+    num_strata_depositeds: np.ndarray,
+    ranks: np.ndarray,
+    differentiae: np.ndarray,
+    tqdm_progress_bar: typing.Union[typing.Type[tqdm.tqdm], mock.Mock],
+) -> None: ...
+def build_tree_searchtable_cpp_from_exploded(
+    data_ids: np.ndarray,
+    num_strata_depositeds: np.ndarray,
+    ranks: np.ndarray,
+    differentiae: np.ndarray,
+    tqdm_progress_bar: typing.Union[typing.Type[tqdm.tqdm], mock.Mock],
+) -> dict[str, np.ndarray]: ...
+def build_tree_searchtable_cpp_from_nested(
+    data_ids: typing.List[int],
+    num_strata_depositeds: typing.List[int],
+    ranks: typing.List[typing.List[int]],
+    differentiae: typing.List[typing.List[int]],
+    tqdm_progress_bar: typing.Union[typing.Type[tqdm.tqdm], mock.Mock],
+) -> dict[str, np.ndarray]: ...
