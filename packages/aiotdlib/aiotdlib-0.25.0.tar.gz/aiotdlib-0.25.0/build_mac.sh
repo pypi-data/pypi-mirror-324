72ab30f588e5d6677e012ba002fb5d21ef6f0173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:33 +0000

    Add package aiotdlib/0.25.0/aiotdlib-0.25.0.tar.gz

---
+++ packages/aiotdlib/aiotdlib-0.25.0.tar.gz/aiotdlib-0.25.0/build_mac.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -e
+brew install gperf cmake openssl
+rm -rf td/
+git clone https://github.com/pylakey/td.git
+cd td
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ \
+      -DCMAKE_INSTALL_PREFIX:PATH=../tdlib \
+      -DTD_ENABLE_LTO=ON \
+      ..
+cmake --build . --target install
+cd ..
+cd ..
+ls -l td/tdlib
