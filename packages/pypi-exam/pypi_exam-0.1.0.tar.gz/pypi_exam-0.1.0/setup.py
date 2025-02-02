791fa3cb29bf64c0f9a54685177477fc946ff154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:06:44 +0000

    Add package pypi-exam/0.1.0/pypi_exam-0.1.0.tar.gz

---
+++ packages/pypi-exam/pypi_exam-0.1.0.tar.gz/pypi_exam-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='pypi_exam',
+version='0.1.0',
+author='wangfu',
+author_email='wangfu0811@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
