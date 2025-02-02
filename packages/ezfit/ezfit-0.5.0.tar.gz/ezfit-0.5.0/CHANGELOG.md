43eab3e0b52ebdfe7d98f9d5b7bb701efa367bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:05:22 +0000

    Add package ezfit/0.5.0/ezfit-0.5.0.tar.gz

---
+++ packages/ezfit/ezfit-0.5.0.tar.gz/ezfit-0.5.0/CHANGELOG.md
@@ -0,0 +1,13 @@
+## v0.5.0 (2025-02-01)
+
+### Fix
+
+- removed pesky notebooks
+
+## v0.4.0 (2025-02-01)
+
+### Feat
+
+- **fit**: improved modularity for passing fit parameters
+
+## v0.1.0 (2025-01-20)
---
+++ packages/ezfit/ezfit-0.5.0.tar.gz/ezfit-0.5.0/ezfit/__init__.py
@@ -0,0 +1,23 @@
+from ezfit.fit import FitAccessor, Model, Parameter
+from ezfit.functions import (
+    exponential,
+    gaussian,
+    linear,
+    lorentzian,
+    power_law,
+    pseudo_voigt,
+)
+
+__all__ = [
+    "Parameter",
+    "Model",
+    "FitAccessor",
+    "MultiPeakModel",
+    "PeakAccessor",
+    "power_law",
+    "exponential",
+    "gaussian",
+    "lorentzian",
+    "pseudo_voigt",
+    "linear",
+]
