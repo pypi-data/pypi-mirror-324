f37e7972e2519d1425404e022d567f4b4879f9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:09 +0000

    Add package slim_gym/0.1.1/slim_gym-0.1.1-py2.py3-none-any.whl

---
+++ packages/slim_gym/slim_gym-0.1.1-py2.py3-none-any.whl/slim_gym/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: nzupp
+
+SLiM-Gym: A gymnasium environment for SLiM evolutionary simulations
+"""
+
+__version__ = "0.1.1"
+
+from .slim_gym import *
+from .sfs import *
+from .slim_injector import *
+from .random_walk import *
