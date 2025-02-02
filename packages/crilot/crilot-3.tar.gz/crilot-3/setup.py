15d4c5cf26f325f308017ef502895419efa3fc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:20 +0000

    Add package crilot/3/crilot-3.tar.gz

---
+++ packages/crilot/crilot-3.tar.gz/crilot-3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crilot",
+    version="3",
+    author="Axmadjon Qaxxorov",
+    description="A Python library to convert Krill text to Latin and vice versa.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://youtube.com/@axmadjonqaxxorovc",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
