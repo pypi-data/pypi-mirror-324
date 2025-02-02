92081afb1cf92be3dc59979ea01a5e6a34a8e39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:06:42 +0000

    Add package hyperscale/0.2.4/hyperscale-0.2.4-py3-none-any.whl

---
+++ packages/hyperscale/hyperscale-0.2.4-py3-none-any.whl/hyperscale/core/engines/client/playwright/models/commands/page/expect_console_message_command.py
@@ -0,0 +1,16 @@
+from typing import Callable, Optional
+
+from playwright.async_api import ConsoleMessage
+from pydantic import (
+    BaseModel,
+    StrictFloat,
+    StrictInt,
+)
+
+
+class ExpectConsoleMessageCommand(BaseModel):
+    predicate: Optional[Callable[[ConsoleMessage], bool]] = None
+    timeout: StrictInt | StrictFloat
+
+    class Config:
+        arbitrary_types_allowed = True
