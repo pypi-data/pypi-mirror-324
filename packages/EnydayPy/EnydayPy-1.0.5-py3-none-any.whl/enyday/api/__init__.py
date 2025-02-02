163ee8854ce1c58f90bafd35639c60ae086ddb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:07:02 +0000

    Add package EnydayPy/1.0.5/EnydayPy-1.0.5-py3-none-any.whl

---
+++ packages/EnydayPy/EnydayPy-1.0.5-py3-none-any.whl/enyday/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from enyday.api.address_api import AddressApi
+from enyday.api.authorization_api import AuthorizationApi
+from enyday.api.consumption_data_api import ConsumptionDataApi
+from enyday.api.eloverblik_api import EloverblikApi
+from enyday.api.users_api import UsersApi
+
