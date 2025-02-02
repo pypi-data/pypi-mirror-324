dce9e8180e1712041b78544f49d0609f20e74d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:10 +0000

    Add package pydanclick/0.4.0/pydanclick-0.4.0.tar.gz

---
+++ packages/pydanclick/pydanclick-0.4.0.tar.gz/pydanclick-0.4.0/pydanclick/model/docstrings.py
@@ -0,0 +1,45 @@
+"""Extract attribute documentation from docstrings."""
+
+from typing import Literal
+
+from pydantic import BaseModel
+from typing_extensions import TypeAlias
+
+from pydanclick.types import FieldName
+
+DocstringStyle: TypeAlias = Literal["google", "numpy", "sphinx"]
+
+
+def parse_attribute_documentation(
+    model_cls: type[BaseModel], docstring_style: DocstringStyle = "google"
+) -> dict[FieldName, str]:
+    """Parse the docstring of a `BaseModel` and returns a mapping from field name to their documentation.
+
+    Requires the optional dependency `griffe`. If it is not installed, returns an empty dictionary.
+
+    Args:
+        model_cls: base model to parse
+        docstring_style: docstring style (see `griffe` documentation for details)
+
+    Returns:
+        a mapping from field name to their documentation. Only documented fields will be present
+    """
+    try:
+        import logging
+
+        from griffe import Docstring, DocstringAttribute, DocstringSectionAttributes
+
+        logging.getLogger(f"griffe.docstrings.{docstring_style}").disabled = True
+        logging.getLogger("griffe.agents.nodes").disabled = True
+    except ImportError:
+        return {}
+    docstring = Docstring(model_cls.__doc__ or "").parse(docstring_style)
+    fields = {}
+    for section in docstring:
+        if not isinstance(section, DocstringSectionAttributes):
+            continue
+        for attribute in section.value:
+            if not isinstance(attribute, DocstringAttribute):
+                continue
+            fields[FieldName(attribute.name)] = attribute.description
+    return fields
