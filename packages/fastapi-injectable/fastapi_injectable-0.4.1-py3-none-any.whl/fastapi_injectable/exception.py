9051c4540825b60d9760531a1a22e4e98ef6db3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 04:06:22 +0000

    Add package fastapi-injectable/0.4.1/fastapi_injectable-0.4.1-py3-none-any.whl

---
+++ packages/fastapi-injectable/fastapi_injectable-0.4.1-py3-none-any.whl/fastapi_injectable/exception.py
@@ -0,0 +1,10 @@
+class DependencyResolveError(Exception):
+    """Custom error for dependency resolve issues."""
+
+
+class DependencyCleanupError(Exception):
+    """Custom error for dependency cleanup issues."""
+
+
+class RunCoroutineSyncMaxRetriesError(Exception):
+    """Custom error for run coroutine sync max retries issues."""
