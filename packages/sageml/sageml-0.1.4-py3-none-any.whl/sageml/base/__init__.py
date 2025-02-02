53b8cc9628a853648910add6af7fdf02341f6eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 03:07:25 +0000

    Add package sageml/0.1.4/sageml-0.1.4-py3-none-any.whl

---
+++ packages/sageml/sageml-0.1.4-py3-none-any.whl/sageml/base/__init__.py
@@ -0,0 +1,5 @@
+from .model import Model
+from .workflow import Workflow
+from .process import Process
+from .preprocess import Preprocessor
+from .model import ModelProcess, __ALL_MODELS__
---
+++ packages/sageml/sageml-0.1.4-py3-none-any.whl/sageml/utils/options.py
@@ -0,0 +1,18 @@
+import logging
+from typing import Literal
+
+
+class options:
+    print_logs: bool = True
+    # TODO: change default to auto-adjustem based on terminal size before release
+    text_size: int = 64
+    validation: bool = True  # TODO: change default to False before release
+    # TODO: change default to automatic detection
+    device: Literal['cpu', 'cuda', 'mps'] = 'cpu'
+    threads: int = -1
+    dev_mode = True
+    dev_mode_logging = logging.INFO
+    user_mode_logging = logging.ERROR
+    blacklist = ['CalibratedClassifierCV']
+    hyperparameters_declaration_priority: Literal['sklearn',
+                                                  'custom'] = 'custom'
