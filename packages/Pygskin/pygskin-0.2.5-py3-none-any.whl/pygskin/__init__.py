8d75f2e6c4e7a8ba5000e8a73b26364850b281b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-02 14:06:57 +0000

    Add package Pygskin/0.2.5/pygskin-0.2.5-py3-none-any.whl

---
+++ packages/Pygskin/pygskin-0.2.5-py3-none-any.whl/pygskin/__init__.py
@@ -0,0 +1,79 @@
+from pygskin import easing
+from pygskin import imgui
+from pygskin.animation import EasingFn
+from pygskin.animation import LerpFn
+from pygskin.animation import animate
+from pygskin.assets import Assets
+from pygskin.camera import Camera
+from pygskin.clock import Clock
+from pygskin.clock import Timer
+from pygskin.dialogue import iter_dialogue
+from pygskin.direction import Direction
+from pygskin.ecs import FilterFn
+from pygskin.ecs import SystemFn
+from pygskin.ecs import filter_entities
+from pygskin.ecs import get_ecs_update_fn
+from pygskin.func import bind
+from pygskin.game import run_game
+from pygskin.gradient import make_color_gradient
+from pygskin.input import map_inputs_to_actions
+from pygskin.lazy import lazy
+from pygskin.parallax import scroll_parallax_layers
+from pygskin.pubsub import channel
+from pygskin.rect import add_padding
+from pygskin.rect import get_rect_attrs
+from pygskin.rect import grid
+from pygskin.screen import ScreenFn
+from pygskin.screen import screen_manager
+from pygskin.shake import shake
+from pygskin.spritesheet import spritesheet
+from pygskin.spritestack import draw_sprite_stack
+from pygskin.statemachine import statemachine
+from pygskin.stylesheet import get_styles
+from pygskin.surface import make_sprite
+from pygskin.surface import rotate_surface
+from pygskin.text import speech_duration
+from pygskin.text import to_snakecase
+from pygskin.tile import tile
+from pygskin.vector import angle_between
+
+__all__ = [
+    "Assets",
+    "Camera",
+    "Clock",
+    "Direction",
+    "EasingFn",
+    "FilterFn",
+    "LerpFn",
+    "ScreenFn",
+    "SystemFn",
+    "Timer",
+    "add_padding",
+    "angle_between",
+    "animate",
+    "bind",
+    "channel",
+    "draw_sprite_stack",
+    "easing",
+    "filter_entities",
+    "get_ecs_update_fn",
+    "get_rect_attrs",
+    "get_styles",
+    "grid",
+    "imgui",
+    "iter_dialogue",
+    "lazy",
+    "make_color_gradient",
+    "make_sprite",
+    "map_inputs_to_actions",
+    "rotate_surface",
+    "run_game",
+    "screen_manager",
+    "scroll_parallax_layers",
+    "shake",
+    "speech_duration",
+    "spritesheet",
+    "statemachine",
+    "tile",
+    "to_snakecase",
+]
---
+++ packages/Pygskin/pygskin-0.2.5-py3-none-any.whl/pygskin/ecs.py
@@ -0,0 +1,59 @@
+"""Entity-Component-System (ECS) implementation."""
+
+from collections.abc import Callable
+from collections.abc import Iterable
+from functools import wraps
+from typing import Any
+from typing import Concatenate
+from typing import get_type_hints
+
+FilterFn = Callable[[Any], bool]
+SystemFn = Callable[Concatenate[Any, ...], None]
+
+
+def filter_entities(filter_fn: FilterFn) -> Callable[[SystemFn], SystemFn]:
+    """Decorator to set filter function for system.
+
+    >>> def has_velocity(entity):
+    ...     return hasattr(entity, "velocity")
+    >>> @filter_entities(has_velocity)
+    ... def apply_velocity(entity):
+    ...     entity.position += entity.velocity
+    """
+
+    def _filtered_system(system_fn: SystemFn) -> SystemFn:
+
+        @wraps(system_fn)
+        def _system_fn(entity: Any, **kwargs) -> None:
+            if filter_fn(entity):
+                system_fn(entity, **kwargs)
+
+        _system_fn.filtered = True  # type: ignore[attr-defined]
+        return _system_fn
+
+    return _filtered_system
+
+
+def get_ecs_update_fn(systems: list[SystemFn]) -> Callable:
+    """Return an update function for ECS."""
+    filter_cache: dict[SystemFn, SystemFn] = {}
+
+    def get_filtered_system_fn(system: SystemFn) -> SystemFn:
+        if getattr(system, "filtered", False):
+            return system
+
+        if callable(filter_ := getattr(system, "filter_entities", None)):
+            return filter_entities(filter_)(system)
+
+        entity_type = next(iter(get_type_hints(system).values()))
+        return filter_entities(lambda _: isinstance(_, entity_type))(system)
+
+    def ecs_update(entities: Iterable[Any], **kwargs) -> None:
+        """Update entities with systems."""
+        for system in systems:
+            if not (system_fn := filter_cache.get(system)):
+                system_fn = filter_cache[system] = get_filtered_system_fn(system)
+            for entity in entities:
+                system_fn(entity, entities=entities, **kwargs)
+
+    return ecs_update
