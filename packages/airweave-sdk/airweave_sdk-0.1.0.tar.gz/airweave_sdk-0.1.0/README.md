820d652b9929efe9f4c305bca35d82f69be6016f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:09:09 +0000

    Add package airweave-sdk/0.1.0/airweave_sdk-0.1.0.tar.gz

---
+++ packages/airweave-sdk/airweave_sdk-0.1.0.tar.gz/airweave_sdk-0.1.0/README.md
@@ -0,0 +1,9 @@
+# Airweave SDK
+
+This is the Airweave SDK for Python. It can be used to interact with the Airweave API.
+
+## Installation
+
+```bash
+pip install airweave-sdk
+```
---
+++ packages/airweave-sdk/airweave_sdk-0.1.0.tar.gz/airweave_sdk-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airweave-sdk",
+    version="0.1.0",
+    description="Airweave SDK for Python. Can be used to interact with the Airweave API.",
+    author="Airweave",
+    author_email="rauf@airweave.io",
+    packages=[],
+    python_requires=">=3.7",
+)
