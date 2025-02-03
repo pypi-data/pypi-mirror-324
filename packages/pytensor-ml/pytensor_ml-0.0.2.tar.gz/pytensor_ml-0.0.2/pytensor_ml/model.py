4c8349514425fa8b786433c5af2d010e99add116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:07:22 +0000

    Add package pytensor-ml/0.0.2/pytensor_ml-0.0.2.tar.gz

---
+++ packages/pytensor-ml/pytensor_ml-0.0.2.tar.gz/pytensor_ml-0.0.2/pytensor_ml/model.py
@@ -0,0 +1,108 @@
+from collections.abc import Generator
+from functools import partial
+from typing import Literal, cast
+
+import numpy as np
+
+from pytensor import config
+from pytensor.compile.sharedvalue import SharedVariable
+from pytensor.graph import graph_inputs
+from pytensor.graph.basic import Constant
+from pytensor.printing import debugprint
+from pytensor.tensor import TensorVariable
+
+from pytensor_ml.pytensorf import function, rewrite_for_prediction
+
+InitializationSchemes = Literal["zeros", "xavier_uniform", "xavier_normal"]
+
+
+def required_graph_inputs(tensor: TensorVariable) -> Generator[TensorVariable, None, None]:
+    return (
+        cast(TensorVariable, var)
+        for var in graph_inputs([tensor])
+        if not isinstance(var, Constant | SharedVariable)
+    )
+
+
+class Model:
+    def __init__(self, X: TensorVariable, y: TensorVariable, compile_kwargs: dict | None = None):
+        self.X = X
+        self.y = y
+
+        self._compile_kwargs = compile_kwargs if compile_kwargs else {}
+        self._weight_values: list[np.ndarray[float]] | None = None
+        self._predict_fn = None
+
+    def initialize_weights(
+        self,
+        scheme: InitializationSchemes = "xavier_normal",
+        random_seed: int | str | np.random.Generator | None = None,
+    ):
+        self._weight_values = initialize_weights(self, scheme, random_seed)
+
+    @property
+    def weights(self) -> list[TensorVariable]:
+        return [var for var in required_graph_inputs(self.y) if var is not self.X]
+
+    @property
+    def weight_values(self) -> list[np.ndarray[float]]:
+        if self._weight_values is None:
+            raise ValueError("Weights have not been initialized")
+        return self._weight_values
+
+    @weight_values.setter
+    def weight_values(self, values: list[np.ndarray[float]]):
+        for i, new_value in enumerate(values):
+            self._weight_values[i][:] = new_value
+
+    def predict(self, X_values: np.ndarray) -> np.ndarray:
+        if self._predict_fn is None:
+            f = function(
+                [*self.weights, self.X],
+                rewrite_for_prediction(self.y),
+                **self._compile_kwargs,
+            )
+            self._predict_fn = partial(f, *self.weight_values)
+
+        return cast(np.ndarray, self._predict_fn(X_values))
+
+    def __str__(self):
+        return debugprint(self.y, file="str")
+
+
+def _zero_init(shape: tuple[int], *args) -> np.ndarray:
+    return np.zeros(shape, dtype=config.floatX)
+
+
+def _xavier_uniform_init(shape: tuple[int], rng: np.random.Generator) -> np.ndarray:
+    scale = np.sqrt(6.0 / np.sum([x for x in shape if x is not None]))
+    return rng.uniform(-scale, scale, size=shape).astype(config.floatX)
+
+
+def _xavier_normal_init(shape: tuple[int], rng: np.random.Generator) -> np.ndarray:
+    scale = np.sqrt(2.0 / np.sum([x for x in shape if x is not None]))
+    return rng.normal(0, scale, size=shape).astype(config.floatX)
+
+
+initialization_factory = {
+    "zeros": _zero_init,
+    "xavier_uniform": _xavier_uniform_init,
+    "xavier_normal": _xavier_normal_init,
+}
+
+
+def initialize_weights(
+    model, scheme: InitializationSchemes, random_seed: int | str | np.random.Generator | None
+):
+    if isinstance(random_seed, str):
+        random_seed = sum(map(ord, random_seed))
+    if isinstance(random_seed, int) or random_seed is None:
+        random_seed = np.random.default_rng(random_seed)
+
+    initial_values = []
+    for var in model.weights:
+        shape = var.type.shape
+        f_initialize = initialization_factory[scheme]
+        initial_values.append(f_initialize(shape, random_seed))
+
+    return initial_values
---
+++ packages/pytensor-ml/pytensor_ml-0.0.2.tar.gz/pytensor_ml-0.0.2/pytensor_ml/optimizers.py
@@ -0,0 +1,234 @@
+from abc import ABC, abstractmethod
+from collections.abc import Callable, Sequence
+
+import numpy as np
+
+from pytensor import config
+from pytensor.compile.function.types import Function
+from pytensor.gradient import grad
+from pytensor.tensor import TensorLike, TensorVariable, sqrt, tensor
+
+from pytensor_ml.model import Model
+from pytensor_ml.pytensorf import function, rewrite_pregrad
+
+
+class Optimizer(ABC):
+    def __init__(
+        self,
+        model: Model,
+        loss_fn: Callable,
+        ndim_out: int = 1,
+        optimizer_weights: list[TensorVariable] | None = None,
+        compile_kwargs: dict | None = None,
+    ):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.ndim_out = ndim_out
+        self.optimizer_weights = optimizer_weights if optimizer_weights is not None else []
+        self._optimizer_weights_values = self._initialize_weights()
+
+        self.update_fn = self.build_update_fn(compile_kwargs)
+
+    @abstractmethod
+    def update_parameters(self, params: Sequence[TensorVariable], loss: TensorVariable): ...
+
+    @property
+    def optimizer_weights_values(self) -> list[np.ndarray]:
+        return self._optimizer_weights_values
+
+    @optimizer_weights_values.setter
+    def optimizer_weights_values(self, values: list[np.ndarray]):
+        for i, new_value in enumerate(values):
+            self._optimizer_weights_values[i][:] = new_value
+
+    def _initialize_weights(self) -> list[np.ndarray]:
+        if self.optimizer_weights:
+            return [np.zeros(param.type.shape) for param in self.optimizer_weights]
+        return []
+
+    def _split_weights(
+        self, all_weights: list[TensorLike]
+    ) -> tuple[list[TensorLike], list[TensorLike]]:
+        n_params = len(self.model.weights)
+        return all_weights[:n_params], all_weights[n_params:]
+
+    def build_update_fn(self, compile_kwargs) -> Function:
+        """
+        Compile a function to update model weights
+
+        Compile a pytensor function that maps data (x), targets (y), and current parameter values to new parameter
+        values. By default, the functions also returns the loss value. To return additional diagnostics, this method
+        should be overloaded.
+
+        Parameters
+        ----------
+        compile_kwargs: dict
+            Keyword arguments passed to pytensor.function
+
+        Returns
+        -------
+        update_fn: Function
+            A function that updates the model weights given a batch of data, targets, and current weights.
+        """
+        compile_kwargs = compile_kwargs if compile_kwargs else {}
+        x, y_hat = self.model.X, self.model.y
+
+        label_slice = (slice(None),) * self.ndim_out + (0,) * (y_hat.ndim - self.ndim_out)
+        y = y_hat[np.s_[label_slice]].type()
+
+        weights = self.model.weights
+        optimizer_weights = self.optimizer_weights
+
+        loss = self.loss_fn(y, y_hat)
+        loss = rewrite_pregrad(loss)
+
+        all_weights = self.update_parameters(weights + optimizer_weights, loss)
+        fn = function(
+            [x, y, *weights, *optimizer_weights],
+            [*all_weights, loss],
+            **compile_kwargs,
+        )
+        fn.trust_input = True
+
+        return fn
+
+    def step(self, x_values, y_values) -> np.ndarray:
+        """
+        Update model weights in-place given a new batch of x_values and y_values.
+
+        Returns
+        -------
+        loss
+        """
+        *new_weights, loss_values = self.update_fn(
+            x_values, y_values, *self.model.weight_values, *self.optimizer_weights_values
+        )
+
+        self.model.weight_values, self.optimizer_weights_values = self._split_weights(new_weights)
+
+        return loss_values
+
+
+class SGD(Optimizer):
+    def __init__(
+        self,
+        model,
+        loss_fn,
+        *,
+        ndim_out: int = 1,
+        learning_rate: TensorLike = 0.01,
+        compile_kwargs: dict | None = None,
+    ):
+        self.learning_rate = learning_rate
+        super().__init__(model, loss_fn, ndim_out=ndim_out, compile_kwargs=compile_kwargs)
+
+    def update_parameters(
+        self, params: list[TensorVariable], loss: TensorVariable
+    ) -> list[TensorVariable]:
+        grads = grad(loss, params)
+        new_params = []
+        for param, d_loss_d_param in zip(params, grads):
+            new_params.append(param - self.learning_rate * d_loss_d_param)
+
+        return new_params
+
+
+class ADAGrad(Optimizer):
+    def __init__(
+        self,
+        model,
+        loss_fn,
+        *,
+        ndim_out: int = 1,
+        learning_rate: TensorLike = 0.01,
+        epsilon: TensorLike = 1e-8,
+        compile_kwargs: dict | None = None,
+    ):
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        g2_weights = [param.type() for param in model.weights]
+
+        super().__init__(
+            model,
+            loss_fn,
+            ndim_out=ndim_out,
+            optimizer_weights=g2_weights,
+            compile_kwargs=compile_kwargs,
+        )
+
+    def update_parameters(
+        self, weights: list[TensorVariable], loss: TensorVariable
+    ) -> list[TensorVariable]:
+        weights, optimizer_weights = self._split_weights(weights)
+
+        grads = grad(loss, weights)
+
+        new_weights = []
+        new_optimizer_weights = []
+
+        for param, d_loss_d_param, g2 in zip(weights, grads, optimizer_weights):
+            new_g2 = g2 + d_loss_d_param**2
+            weight_update = d_loss_d_param / np.sqrt(new_g2 + self.epsilon)
+            new_weights.append(param - self.learning_rate * weight_update)
+            new_optimizer_weights.append(new_g2)
+
+        return new_weights + new_optimizer_weights
+
+
+class Adam(Optimizer):
+    def __init__(
+        self,
+        model,
+        loss_fn,
+        *,
+        ndim_out: int = 1,
+        learning_rate: TensorLike = 0.01,
+        beta1: TensorLike = 0.9,
+        beta2: TensorLike = 0.999,
+        epsilon: TensorLike = 1e-8,
+        compile_kwargs: dict | None = None,
+    ):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+
+        m_weights = [param.type() for param in model.weights]
+        v_weights = [param.type() for param in model.weights]
+        t = tensor("t", shape=(1,), dtype=config.floatX)
+
+        optimizer_weights = m_weights + v_weights + [t]
+        super().__init__(
+            model,
+            loss_fn,
+            ndim_out=ndim_out,
+            optimizer_weights=optimizer_weights,
+            compile_kwargs=compile_kwargs,
+        )
+
+    def update_parameters(
+        self, weights: list[TensorVariable], loss: TensorVariable
+    ) -> list[TensorVariable]:
+        weights, optimizer_weights = self._split_weights(weights)
+        t = optimizer_weights.pop(-1)
+        m_weights, v_weights = self._split_weights(optimizer_weights)
+
+        grads = grad(loss, weights)
+
+        new_weights = []
+        new_m_weights = []
+        new_v_weights = []
+
+        new_t = t + 1
+        a_t = sqrt(1 - self.beta2**new_t) / (1 - self.beta1**new_t)
+
+        for param, d_loss_d_param, m, v in zip(weights, grads, m_weights, v_weights):
+            weight_update = a_t * m / (sqrt(v) + self.epsilon)
+            new_weights.append(param - self.learning_rate * weight_update)
+
+            new_m = self.beta1 * m + (1 - self.beta1) * d_loss_d_param
+            new_v = self.beta2 * v + (1 - self.beta2) * d_loss_d_param**2
+            new_m_weights.append(new_m)
+            new_v_weights.append(new_v)
+
+        return new_weights + new_m_weights + new_v_weights + [new_t]
