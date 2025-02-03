204a25bf077922196520f42e143a1515c6d489c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:12 +0000

    Add package litepipe/2.0.1/litepipe-2.0.1-py3-none-any.whl

---
+++ packages/litepipe/litepipe-2.0.1-py3-none-any.whl/litepipe/pipeline.py
@@ -0,0 +1,19 @@
+from litepipe import Transform
+
+class Pipeline:
+    def __init__(self):
+        self.start = None
+
+    def __or__(self, start: Transform):
+        self.start = start
+        return start
+
+    def run(self):
+        results = []
+        for element in self.start():
+            results.append(element)
+        return results
+
+    @property
+    def graph(self):
+        return str(self.start)
