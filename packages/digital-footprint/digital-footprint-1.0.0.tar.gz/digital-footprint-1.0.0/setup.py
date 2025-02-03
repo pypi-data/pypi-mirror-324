84a8b5c1030faff2669d0235c4f4bcf695cf666d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:04 +0000

    Add package digital-footprint/1.0.0/digital-footprint-1.0.0.tar.gz

---
+++ packages/digital-footprint/digital-footprint-1.0.0.tar.gz/digital-footprint-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="digital-footprint",
+    version="1.0.0",
+    description="Digital Footprint Scanner: Free tool to extract useful information from an email.",
+    author="ivanMartin",
+    author_email="imartin.desarrollo@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "python-whois",
+        "dnspython"
+    ],
+    entry_points={
+    "console_scripts": [
+        "digital-footprint = digital_footprint.scanner:main"
+    ]
+},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
