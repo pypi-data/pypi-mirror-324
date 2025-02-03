7fbcdd2408c606d09a6a7d53d3764bba31b1516b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:27 +0000

    Add package pydantic-set-operations/1.0.19/pydantic_set_operations-1.0.19.tar.gz

---
+++ packages/pydantic-set-operations/pydantic_set_operations-1.0.19.tar.gz/pydantic_set_operations-1.0.19/setup.py
@@ -0,0 +1,31 @@
+﻿from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setup(
+	name="pydantic_set_operations",
+	version="1.0.19",
+	description="An enhanced version of Pydantic's BaseModel, allowing for advanced model "
+	            "manipulations. (Inspired in TypeScript)",
+	package_dir={"": "src"},
+	packages=find_packages(where="src"),
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/P1etrodev/pydantic-set-operations",
+	author="P1etrodev",
+	author_email="undefinedpietro@gmail.com",
+	license="MIT",
+	classifiers=[
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3.10",
+		"Operating System :: OS Independent"
+	],
+	install_requires=[
+		"pydantic"
+	],
+	extras_require={
+		"dev": ["twine"]
+	},
+	python_requires=">=3.10",
+)
