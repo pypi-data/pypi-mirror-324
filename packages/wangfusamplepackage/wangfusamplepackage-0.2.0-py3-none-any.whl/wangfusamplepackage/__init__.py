4c9305036a11325dcbafa353cdbff83aacb7870e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:08:29 +0000

    Add package wangfusamplepackage/0.2.0/wangfusamplepackage-0.2.0-py3-none-any.whl

---
+++ packages/wangfusamplepackage/wangfusamplepackage-0.2.0-py3-none-any.whl/wangfusamplepackage/__init__.py
@@ -0,0 +1,22 @@
+# __init__.py
+
+from .autoupdater import main as auto_update
+from .logging_config import setup_logging
+
+# Set up logging once when the package is initialized
+logger = setup_logging()
+
+# Package version
+__version__ = "0.2.0"
+
+# Flag to ensure that update check happens only once
+update_checked = False
+
+# Trigger auto-update when the package is imported
+if not update_checked:
+    try:
+        logger.info("Checking for updates...")
+        auto_update("wangfusamplepackage")  # Call the auto-updater when the package is imported
+        update_checked = True  # Set the flag to True after the first check
+    except Exception as e:
+        logger.error(f"Error during package update check: {e}")
