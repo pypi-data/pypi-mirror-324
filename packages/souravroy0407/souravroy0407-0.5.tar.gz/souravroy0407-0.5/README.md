513be5e5b42d39e00383dc417bad79e17606586c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:38 +0000

    Add package souravroy0407/0.5/souravroy0407-0.5.tar.gz

---
+++ packages/souravroy0407/souravroy0407-0.5.tar.gz/souravroy0407-0.5/README.md
@@ -0,0 +1,3 @@
+# sourav0407
+
+A simple Python package that provides a `hello()` function.
---
+++ packages/souravroy0407/souravroy0407-0.5.tar.gz/souravroy0407-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="souravroy0407",
+    version="0.5",
+    author="souravroy0407",
+    description="A simple package with a hello function",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts":[
+            "sourav0407 = sourav0407:hello"
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent", 
+    ],
+      
+)
