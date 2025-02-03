1b921691ed7fce843d2f70f979a838f3fc501d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:32 +0000

    Add package pyspark_huggingface/0.1.0/pyspark_huggingface-0.1.0.tar.gz

---
+++ packages/pyspark_huggingface/pyspark_huggingface-0.1.0.tar.gz/pyspark_huggingface-0.1.0/pyspark_huggingface/huggingface.py
@@ -0,0 +1,51 @@
+from typing import Optional
+
+from pyspark.sql.datasource import DataSource, DataSourceArrowWriter, DataSourceReader
+from pyspark.sql.types import StructType
+
+from pyspark_huggingface.huggingface_sink import HuggingFaceSink
+from pyspark_huggingface.huggingface_source import HuggingFaceSource
+
+
+class HuggingFaceDatasets(DataSource):
+    """
+    DataSource for reading and writing HuggingFace Datasets in Spark.
+
+    Read
+    ------
+    See :py:class:`HuggingFaceSource` for more details.
+
+    Write
+    ------
+    See :py:class:`HuggingFaceSink` for more details.
+    """
+
+    # Delegate the source and sink methods to the respective classes.
+    def __init__(self, options: dict):
+        super().__init__(options)
+        self.options = options
+        self.source: Optional[HuggingFaceSource] = None
+        self.sink: Optional[HuggingFaceSink] = None
+
+    def get_source(self) -> HuggingFaceSource:
+        if self.source is None:
+            self.source = HuggingFaceSource(self.options.copy())
+        return self.source
+
+    def get_sink(self):
+        if self.sink is None:
+            self.sink = HuggingFaceSink(self.options.copy())
+        return self.sink
+
+    @classmethod
+    def name(cls):
+        return "huggingface"
+
+    def schema(self):
+        return self.get_source().schema()
+
+    def reader(self, schema: StructType) -> "DataSourceReader":
+        return self.get_source().reader(schema)
+
+    def writer(self, schema: StructType, overwrite: bool) -> "DataSourceArrowWriter":
+        return self.get_sink().writer(schema, overwrite)
