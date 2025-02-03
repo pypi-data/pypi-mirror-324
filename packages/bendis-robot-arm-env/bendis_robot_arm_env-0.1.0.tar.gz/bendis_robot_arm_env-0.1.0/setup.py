fe17747a8baadd7e8e7b9159e62b03460330249f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:01 +0000

    Add package bendis-robot-arm-env/0.1.0/bendis_robot_arm_env-0.1.0.tar.gz

---
+++ packages/bendis-robot-arm-env/bendis_robot_arm_env-0.1.0.tar.gz/bendis_robot_arm_env-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bendis-robot-arm-env',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'gym',
+        'pybullet',
+        'pybullet_data',
+        'numpy',
+        'stable-baselines3',
+        'opencv-python',
+    ],
+    description='Robotic Arm Environment for Training RL Agents',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your_email@example.com',
+    url='https://github.com/yourusername/bendis-robot-arm-env',  # Optional, if you plan to host it on GitHub
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
