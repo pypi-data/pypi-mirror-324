432897e88bfe612b83ba8c6c52d37e67f801d5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:45 +0000

    Add package cdk-cloudformation-dynatrace-configuration-dashboard/1.6.0a7/cdk_cloudformation_dynatrace_configuration_dashboard-1.6.0a7.tar.gz

---
+++ packages/cdk-cloudformation-dynatrace-configuration-dashboard/cdk_cloudformation_dynatrace_configuration_dashboard-1.6.0a7.tar.gz/cdk_cloudformation_dynatrace_configuration_dashboard-1.6.0a7/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-cloudformation-dynatrace-configuration-dashboard",
+    "version": "1.6.0.a7",
+    "description": "Manage a dashboard in Dynatrace.",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws-ia/cloudformation-dynatrace-resource-providers",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cdklabs/cdk-cloudformation.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_cloudformation_dynatrace_configuration_dashboard",
+        "cdk_cloudformation_dynatrace_configuration_dashboard._jsii"
+    ],
+    "package_data": {
+        "cdk_cloudformation_dynatrace_configuration_dashboard._jsii": [
+            "dynatrace-configuration-dashboard@1.6.0-alpha.7.jsii.tgz"
+        ],
+        "cdk_cloudformation_dynatrace_configuration_dashboard": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.8",
+    "install_requires": [
+        "aws-cdk-lib>=2.177.0, <3.0.0",
+        "constructs>=10.4.2, <11.0.0",
+        "jsii>=1.106.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard>=2.13.3,<4.3.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
