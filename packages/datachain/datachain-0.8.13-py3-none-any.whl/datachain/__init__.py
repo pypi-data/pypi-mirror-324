b5b9c347e5908060e86e7ef4084826f16483cb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:38 +0000

    Add package datachain/0.8.13/datachain-0.8.13-py3-none-any.whl

---
+++ packages/datachain/datachain-0.8.13-py3-none-any.whl/datachain/__init__.py
@@ -0,0 +1,40 @@
+from datachain.lib.data_model import DataModel, DataType, is_chain_type
+from datachain.lib.dc import C, Column, DataChain, Sys
+from datachain.lib.file import (
+    ArrowRow,
+    File,
+    FileError,
+    ImageFile,
+    TarVFile,
+    TextFile,
+)
+from datachain.lib.model_store import ModelStore
+from datachain.lib.udf import Aggregator, Generator, Mapper
+from datachain.lib.utils import AbstractUDF, DataChainError
+from datachain.query import metrics, param
+from datachain.query.session import Session
+
+__all__ = [
+    "AbstractUDF",
+    "Aggregator",
+    "ArrowRow",
+    "C",
+    "Column",
+    "DataChain",
+    "DataChainError",
+    "DataModel",
+    "DataType",
+    "File",
+    "FileError",
+    "Generator",
+    "ImageFile",
+    "Mapper",
+    "ModelStore",
+    "Session",
+    "Sys",
+    "TarVFile",
+    "TextFile",
+    "is_chain_type",
+    "metrics",
+    "param",
+]
