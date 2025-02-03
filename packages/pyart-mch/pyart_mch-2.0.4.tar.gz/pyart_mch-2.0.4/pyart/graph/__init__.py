c6b71ca821ae66bfbf810bd558dc7aabc655e2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:32 +0000

    Add package pyart-mch/2.0.4/pyart_mch-2.0.4.tar.gz

---
+++ packages/pyart-mch/pyart_mch-2.0.4.tar.gz/pyart_mch-2.0.4/pyart/graph/__init__.py
@@ -0,0 +1,42 @@
+"""
+=============================
+Graphing (:mod:`pyart.graph`)
+=============================
+
+.. currentmodule:: pyart.graph
+
+Creating plots of Radar and Grid fields.
+
+Plotting radar data
+===================
+
+.. autosummary::
+    :toctree: generated/
+
+    RadarDisplay
+    RadarMapDisplay
+    AirborneRadarDisplay
+    RadarMapDisplayBasemap
+
+Plotting grid data
+==================
+
+.. autosummary::
+    :toctree: generated/
+
+    GridMapDisplay
+    GridMapDisplayBasemap
+
+"""
+
+from .radardisplay import RadarDisplay  # noqa
+from . import cm  # noqa
+from . import cm_colorblind  # noqa
+from .convstrat_scheme_plot import plot_convstrat_scheme  # noqa
+from .radardisplay_airborne import AirborneRadarDisplay  # noqa
+from .gridmapdisplay import GridMapDisplay  # noqa
+from .gridmapdisplay_basemap import GridMapDisplayBasemap  # noqa
+from .radarmapdisplay import RadarMapDisplay  # noqa
+from .radarmapdisplay_basemap import RadarMapDisplayBasemap  # noqa
+
+__all__ = [s for s in dir() if not s.startswith("_")]
