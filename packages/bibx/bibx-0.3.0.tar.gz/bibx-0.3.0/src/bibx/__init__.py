214a20bfdf2c399afa53b7b6cbb4363817ee45e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:16 +0000

    Add package bibx/0.3.0/bibx-0.3.0.tar.gz

---
+++ packages/bibx/bibx-0.3.0.tar.gz/bibx-0.3.0/src/bibx/__init__.py
@@ -0,0 +1,75 @@
+"""BibX is a library to work with bibliographic data."""
+
+import logging
+from typing import TextIO
+
+from bibx._entities.article import Article
+from bibx._entities.collection import Collection
+from bibx._entities.collection_builders.openalex import OpenAlexCollectionBuilder
+from bibx._entities.collection_builders.scopus_bib import ScopusBibCollectionBuilder
+from bibx._entities.collection_builders.scopus_ris import ScopusRisCollectionBuilder
+from bibx._entities.collection_builders.wos import WosCollectionBuilder
+from bibx.algorithms.sap import Sap
+from bibx.exceptions import BibXError
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "Article",
+    "Collection",
+    "Sap",
+    "query_openalex",
+    "read_any",
+    "read_scopus_bib",
+    "read_scopus_ris",
+    "read_wos",
+]
+
+__version__ = "0.3.0"
+
+
+def query_openalex(query: str, limit: int = 600) -> Collection:
+    """Query OpenAlex and return a collection."""
+    return OpenAlexCollectionBuilder(query, limit).build()
+
+
+def read_scopus_bib(*files: TextIO) -> Collection:
+    """Take any number of bibtex files from scopus and generates a collection.
+
+    :param files: Scopus bib files open.
+    :return: the collection
+    """
+    return ScopusBibCollectionBuilder(*files).build()
+
+
+def read_scopus_ris(*files: TextIO) -> Collection:
+    """Take any number of ris files from scopus and generates a collection.
+
+    :param files: Scopus bib files open.
+    :return: the collection
+    """
+    return ScopusRisCollectionBuilder(*files).build()
+
+
+def read_wos(*files: TextIO) -> Collection:
+    """Take any number of wos text files and returns a collection.
+
+    :param files: WoS files open.
+    :return: the collection
+    """
+    return WosCollectionBuilder(*files).build()
+
+
+def read_any(file: TextIO) -> Collection:
+    """Try to read a file with the supported formats."""
+    for handler in (read_wos, read_scopus_ris, read_scopus_bib):
+        try:
+            return handler(file)
+        except BibXError as e:
+            logger.debug("Error: %s", e)
+        except ValueError:
+            logger.debug(
+                "Error: the %s function does not support this file", handler.__name__
+            )
+    message = "Unsupported file type"
+    raise ValueError(message)
---
+++ packages/bibx/bibx-0.3.0.tar.gz/bibx-0.3.0/src/bibx/_entities/collection_builders/base.py
@@ -0,0 +1,7 @@
+from typing import Protocol
+
+from bibx._entities.collection import Collection
+
+
+class CollectionBuilder(Protocol):
+    def build(self) -> Collection: ...
