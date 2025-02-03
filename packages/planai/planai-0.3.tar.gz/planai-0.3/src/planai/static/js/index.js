22481ca9c80cdf1d9155ab67a25b5dff04373259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:35 +0000

    Add package planai/0.3/planai-0.3.tar.gz

---
+++ packages/planai/planai-0.3.tar.gz/planai-0.3/src/planai/static/js/index.js
@@ -0,0 +1,30 @@
+import { setupEventSource } from './core.js';
+import { initTaskListeners } from './tasks.js';
+import { initTraceListeners } from './trace.js';
+import { initWorkerStatsListeners } from './workers.js';
+import { initStatusListeners } from './status.js';
+import { initTheme } from './theme.js';
+import { initTabs } from './utils.js';
+import { initMermaid } from './mermaid.js';
+import { initMemoryListeners } from './memory.js';
+
+setupEventSource();
+initTaskListeners();
+initTraceListeners();
+initWorkerStatsListeners();
+initStatusListeners();
+initTheme();
+initTabs();
+initMermaid();
+initMemoryListeners();
+
+// Quit button functionality
+document.getElementById('quit-button').addEventListener('click', function () {
+    fetch('/quit', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'ok') {
+                alert('Quit signal sent. The application will shut down soon.');
+            }
+        });
+});
