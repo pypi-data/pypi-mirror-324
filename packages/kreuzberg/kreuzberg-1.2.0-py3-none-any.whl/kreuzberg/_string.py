406e4c6227f95fa69f884f8f3b6bb607ccb48935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:06:58 +0000

    Add package kreuzberg/1.2.0/kreuzberg-1.2.0-py3-none-any.whl

---
+++ packages/kreuzberg/kreuzberg-1.2.0-py3-none-any.whl/kreuzberg/_string.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from contextlib import suppress
+
+from charset_normalizer import detect
+
+
+def safe_decode(byte_data: bytes, encoding: str | None = None) -> str:
+    """Decode a byte string safely, removing invalid sequences.
+
+    Args:
+        byte_data: The byte string to decode.
+        encoding: The encoding to use when decoding the byte string.
+
+    Returns:
+        The decoded string.
+    """
+    if not byte_data:
+        return ""
+
+    encodings = ["utf-8", "latin-1"]
+
+    if encoding:
+        with suppress(UnicodeDecodeError):
+            return byte_data.decode(encoding, errors="ignore")
+
+    if encoding := detect(byte_data).get("encoding"):
+        encodings.append(encoding)
+
+    for encoding in encodings:
+        with suppress(UnicodeDecodeError):
+            return byte_data.decode(encoding, errors="ignore")
+
+    # TODO: add test case
+    return byte_data.decode("latin-1", errors="replace")
+
+
+def normalize_spaces(text: str) -> str:
+    """Normalize the spaces in a string.
+
+    Args:
+        text: The text to sanitize.
+
+    Returns:
+        The sanitized text.
+    """
+    return " ".join(text.strip().split())
