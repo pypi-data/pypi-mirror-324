fd800b9a95d6207086ff66fa26dda5efff8aa5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:13 +0000

    Add package ocean-qap/0.0.1/ocean_qap-0.0.1.tar.gz

---
+++ packages/ocean-qap/ocean_qap-0.0.1.tar.gz/ocean_qap-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ocean_qap',
+    packages=find_packages(include=['ocean_qap']),
+    version='0.0.1',
+    description='Prototype library for solving QAP with the Ocean SDK',
+    author='Atharv Chowdhary',
+    install_requires=[
+        "matplotlib>=3.0.0",
+        "numpy>=1.19.0",
+        "networkx>=2.5",
+        "dwave-ocean-sdk>=5.5.0"
+    ]
+)
