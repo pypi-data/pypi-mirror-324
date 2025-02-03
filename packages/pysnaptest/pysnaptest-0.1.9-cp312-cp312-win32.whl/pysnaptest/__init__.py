4f4f866a09ba7106609597c024a1a85e2c96fd20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:36 +0000

    Add package pysnaptest/0.1.9/pysnaptest-0.1.9-cp312-cp312-win32.whl

---
+++ packages/pysnaptest/pysnaptest-0.1.9-cp312-cp312-win32.whl/pysnaptest/__init__.py
@@ -0,0 +1,9 @@
+# ruff: noqa: F401
+from .snapshot import (
+    snapshot,
+    assert_json_snapshot,
+    assert_csv_snapshot,
+    assert_snapshot,
+    assert_dataframe_snapshot,
+    assert_binary_snapshot,
+)
