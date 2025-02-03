71cd99890770c59c1213d38baea14ea8219997d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:00 +0000

    Add package epubcrush/2.0.7/epubcrush-2.0.7.tar.gz

---
+++ packages/epubcrush/epubcrush-2.0.7.tar.gz/epubcrush-2.0.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="epubcrush",
+    version="2.0.7",
+    author="Jon Craton",
+    author_email="jon@joncraton.com",
+    description="Compress EPUB files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jncraton/epubcrush",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "epubcrush=epubcrush:main",
+        ],
+    },
+    install_requires=[],
+)
