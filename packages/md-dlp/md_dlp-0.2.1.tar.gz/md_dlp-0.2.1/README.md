71777a21245ed97aabfab15e89d01913562575e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:48 +0000

    Add package md-dlp/0.2.1/md_dlp-0.2.1.tar.gz

---
+++ packages/md-dlp/md_dlp-0.2.1.tar.gz/md_dlp-0.2.1/README.md
@@ -0,0 +1,45 @@
+# md-dlp
+
+A tool for markdown data loss prevention.
+
+## Installation
+
+### Using pip (Recommended)
+
+```bash
+pip install md-dlp
+```
+
+### Using Homebrew (Coming Soon)
+
+> ⚠️ Homebrew installation is currently under development and not yet available. Please use pip installation for now.
+
+The following will be available once the Homebrew formula is ready:
+```bash
+# Add the tap
+brew tap markshawn2020/tools
+
+# Install md-dlp
+brew install md-dlp
+```
+
+## Usage
+
+```bash
+md-dlp [options] <file>
+```
+
+## Features
+
+- Prevent data loss in markdown files
+- Easy to use command-line interface
+- Cross-platform support
+- Available through both Homebrew and pip
+
+## License
+
+MIT License
+
+## Author
+
+MarkShawn (shawninjuly@gmail.com)
---
+++ packages/md-dlp/md_dlp-0.2.1.tar.gz/md_dlp-0.2.1/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="md-dlp",
+    version="0.2.1",
+    author="MarkShawn",
+    author_email="shawninjuly@gmail.com",
+    description="A tool for markdown data loss prevention",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/markshawn2020/md-dlp",
+    py_modules=["md_dlp"],
+    install_requires=[
+        "requests>=2.25.1",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'md-dlp=md_dlp:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
