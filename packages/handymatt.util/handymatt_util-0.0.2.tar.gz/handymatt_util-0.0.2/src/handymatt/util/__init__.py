8f36645b91d5220e58c4966173fe315226db3d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:07 +0000

    Add package handymatt.util/0.0.2/handymatt_util-0.0.2.tar.gz

---
+++ packages/handymatt.util/handymatt_util-0.0.2.tar.gz/handymatt_util-0.0.2/src/handymatt/util/__init__.py
@@ -0,0 +1,6 @@
+""" A collection of small miscellaneous libraries by MattThePerson (https://github.com/MattThePerson/handymatt) """
+from .string_parser import StringParser
+from .json_handler import JsonHandler
+from .bookmarks_getter import BookmarksGetter
+
+__all__ = ["StringParser", "JsonHandler", "BookmarksGetter"]
