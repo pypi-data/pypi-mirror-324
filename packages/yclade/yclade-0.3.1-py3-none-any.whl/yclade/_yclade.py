1da6f7c0b2319c2048a08328ed7daf1fcb405281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:06 +0000

    Add package yclade/0.3.1/yclade-0.3.1-py3-none-any.whl

---
+++ packages/yclade/yclade-0.3.1-py3-none-any.whl/yclade/_yclade.py
@@ -0,0 +1,16 @@
+"""Main functions for the yclade package."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from . import find, snps, tree, types
+
+
+def find_clade(
+    snp_string: str, version: str | None = None, data_dir: Path | None = None
+) -> list[types.CladeInfo]:
+    """Find the clade that matches the given SNP string."""
+    snp_results = snps.parse_snp_results(snp_string)
+    tree_data = tree.get_yfull_tree_data(version=version, data_dir=data_dir)
+    return find.get_ordered_clade_details(tree=tree_data, snps=snp_results)
