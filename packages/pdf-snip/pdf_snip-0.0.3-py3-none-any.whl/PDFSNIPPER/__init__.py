af9c9787afc10845e8a24bb966bbfb21180fc8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:38 +0000

    Add package pdf-snip/0.0.3/pdf_snip-0.0.3-py3-none-any.whl

---
+++ packages/pdf-snip/pdf_snip-0.0.3-py3-none-any.whl/PDFSNIPPER/__init__.py
@@ -0,0 +1,6 @@
+from .main import remove_first_pages 
+from .main import save_specific_pages
+from .main import save_pages_as_images
+from .main import split_pdf
+from .main import remove_last_pages
+from .main import remove_pages_outside_range
