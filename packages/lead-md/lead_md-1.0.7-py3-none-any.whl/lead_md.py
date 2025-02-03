a438ee0ccc8ec21e468c639583dd858e2e00cdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:10 +0000

    Add package lead-md/1.0.7/lead_md-1.0.7-py3-none-any.whl

---
+++ packages/lead-md/lead_md-1.0.7-py3-none-any.whl/lead_md.py
@@ -0,0 +1,88 @@
+#Lead, a markdown renderer
+#Copyright Pittab 2025
+#Producesyntaxed by EyeScary-Development
+
+#imports
+import producesyntaxed
+from typing import List, Any
+
+
+#Kinda self explanatory, makes bullet list strings and rerenders them for things like bold tags and strikethroughs
+def bulletlist(item):
+    splititem=item.split()
+    splititem.pop(0)
+    renderalt([str("- " + ' '.join(splititem))])
+
+#Processes bold text
+def bold(line):
+    line=line.split("*")
+    bolds = [line[i] for i in range(len(line)) if i % 2 != 0]
+    for item in line:
+        if item not in bolds:
+            renderalt([item])
+        else:
+            producesyntaxed.producesyntaxed(item, 'aqua', True, False)
+
+#Processes strikethroughs
+def strikethrough(line):
+    line=line.split("~~")
+    tostrike = [line[i] for i in range(len(line)) if i % 2 != 0]
+    for item in line:
+        if item not in tostrike:
+            renderalt([item])
+        else:
+            producesyntaxed.producesyntaxed(item, 'grey', True, False)
+
+#Processes headings TODO: maybe H6 and H5?
+def heading(line):
+    print()
+    line=line.split()
+    if line[0] == "#":
+        line.pop(0)
+        producesyntaxed.producesyntaxed(' '.join(line), 'blue2', False, True)
+    if line[0] == "##":
+        line.pop(0)
+        producesyntaxed.producesyntaxed(' '.join(line), 'blue', False, True)
+    if line[0] == "###":
+        line.pop(0)
+        producesyntaxed.producesyntaxed(' '.join(line), 'aqua', False, True)
+    if line[0] == "####":
+        line.pop(0)
+        print(' '.join(line))
+
+#Run this command to render le text
+def render(input_list: List[Any]):
+    for item in input_list:
+        match item:
+            case x if x.startswith("#"):
+                heading(item)
+            case x if x.startswith("*"):
+                if item.startswith ("* ") != True:
+                    bold(item)
+                else:
+                    bulletlist(item)
+            case x if "*" in x:
+                bold(item)
+            case x if "~~" in x:
+                strikethrough(item)
+            case _:
+                print(item, end='')
+        print()
+
+#render an line that has already passed through 1 render cycle
+def renderalt(input_list: List[Any]):
+    for item in input_list:
+        match item:
+            case x if x.startswith("#"):
+                heading(item)
+            case x if x.startswith("*"):
+                if item.startswith ("* ") != True:
+                    bold(item)
+                else:
+                    bulletlist(item)
+            case x if "*" in x:
+                bold(item)
+            case x if "~~" in x:
+                strikethrough(item)
+            case _:
+                print(item, end=' ')
