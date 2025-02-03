c4fae863886d9af6725cd5267d4ba57c68c435af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:49 +0000

    Add package cloudflare-dyndns/5.1/cloudflare_dyndns-5.1.tar.gz

---
+++ packages/cloudflare-dyndns/cloudflare_dyndns-5.1.tar.gz/cloudflare_dyndns-5.1/cloudflare_dyndns/types.py
@@ -0,0 +1,11 @@
+import ipaddress
+from typing import Literal, Union
+
+IPAddress = Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
+A = Literal["A"]
+AAAA = Literal["AAAA"]
+RecordType = Union[A, AAAA]
+
+
+def get_record_type(ip: IPAddress) -> RecordType:
+    return "A" if ip.version == 4 else "AAAA"
