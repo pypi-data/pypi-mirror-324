bdb696404ee1ca3222b28fd65c6b006592fb0c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:33 +0000

    Add package torchruntime/1.7.0/torchruntime-1.7.0.tar.gz

---
+++ packages/torchruntime/torchruntime-1.7.0.tar.gz/torchruntime-1.7.0/torchruntime/utils/torch_device_utils.py
@@ -0,0 +1,85 @@
+import sys
+from typing import Union
+
+if sys.version_info < (3, 9):
+    # polyfill for callable static methods
+    class CallableStaticMethod(staticmethod):
+        def __call__(self, *args, **kwargs):
+            return self.__func__(*args, **kwargs)
+
+    # Patch the built-in staticmethod with CallableStaticMethod
+    import builtins
+
+    builtins.staticmethod = CallableStaticMethod
+
+
+def _is_directml_platform_available():
+    import torch
+
+    try:
+        import torch_directml
+
+        torch.directml = torch_directml
+
+        return torch.directml.is_available()
+    except ImportError:
+        pass
+
+    return False
+
+
+def get_installed_torch_platform():
+    import torch
+    from platform import system as os_name
+
+    if _is_directml_platform_available():
+        return "directml", torch.directml
+
+    if torch.cuda.is_available():
+        return "cuda", torch.cuda
+    if hasattr(torch, "xpu") and torch.xpu.is_available():
+        return "xpu", torch.xpu
+    if hasattr(torch, "mps") and os_name() == "Darwin":
+        return "mps", torch.mps
+    if hasattr(torch, "mtia") and torch.mtia.is_available():
+        return "mtia", torch.mtia
+
+    return "cpu", torch.cpu
+
+
+def get_device_count() -> int:
+    torch_platform_name, torch_platform = get_installed_torch_platform()
+
+    return torch_platform.device_count()
+
+
+def get_device_name(device) -> str:
+    "Expects a torch.device as the argument"
+
+    torch_platform_name, torch_platform = get_installed_torch_platform()
+    if torch_platform_name not in ("xpu", "cuda", "directml"):
+        return f"{torch_platform_name}:{device.index}"
+
+    if torch_platform_name == "directml":
+        return torch_platform.device_name(device.index)
+
+    return torch_platform.get_device_name(device.index)
+
+
+def get_device(device: Union[int, str]):
+    import torch
+
+    if isinstance(device, str):
+        if ":" in device:
+            torch_platform_name, device_index = device.split(":")
+            device_index = int(device_index)
+        else:
+            torch_platform_name, device_index = device, 0
+    else:
+        torch_platform_name, _ = get_installed_torch_platform()
+        device_index = device
+
+    if torch_platform_name == "directml" and _is_directml_platform_available():
+        return torch.directml.device(device_index)
+
+    return torch.device(torch_platform_name, device_index)
