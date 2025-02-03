2677234881a5df9feaf26c6a760d01d21f439709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:10 +0000

    Add package hcli-hai/2.1.3/hcli_hai-2.1.3-py3-none-any.whl

---
+++ packages/hcli-hai/hcli_hai-2.1.3-py3-none-any.whl/hcli_hai/cli/model.py
@@ -0,0 +1,18 @@
+models = {
+    "claude-3-opus-20240229": {
+                   'model' : "claude-3-opus-20240229",
+                   'max_tokens' : 4096
+               },
+    "claude-3-haiku-20240307": {
+                   'model' : "claude-3-haiku-20240307",
+                   'max_tokens' : 2048
+               },
+    "claude-3-5-sonnet-20241022": {
+                   'model' : "claude-3-5-sonnet-20241022",
+                   'max_tokens' : 4096
+               },
+    "claude-3-5-sonnet-20240620": {
+                   'model' : "claude-3-5-sonnet-20240620",
+                   'max_tokens' : 4096
+               }
+}
