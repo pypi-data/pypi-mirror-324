0b08a87b58cb36c1ec8c712cf049fe0353bba160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:33 +0000

    Add package nonebot-plugin-memos/0.1.0b15/nonebot_plugin_memos-0.1.0b15-py3-none-any.whl

---
+++ packages/nonebot-plugin-memos/nonebot_plugin_memos-0.1.0b15-py3-none-any.whl/nonebot_plugin_memos/client.py
@@ -0,0 +1,150 @@
+from datetime import datetime, timezone
+from typing import Any, Type, TypeVar
+from base64 import b64decode, b64encode
+
+from nonebot.compat import TypeAdapter
+from httpx import AsyncClient, Response
+from filetype import guess_mime
+from pydantic import BaseModel
+from yarl import URL
+
+from .model import Memo, Visibility
+
+T = TypeVar("T", bound=BaseModel)
+
+TYPE_ADAPTER_CACHE: dict[int, TypeAdapter] = {}
+
+
+def get_type_adapter(model: Type[T]) -> TypeAdapter[T]:
+    model_hash = hash(model)
+    if model_hash not in TYPE_ADAPTER_CACHE:
+        TYPE_ADAPTER_CACHE[model_hash] = TypeAdapter(model)
+    return TYPE_ADAPTER_CACHE[model_hash]
+
+
+class UnSet:
+    pass
+
+
+class ApiClient:
+    def __init__(self, base_url: str, token: str):
+        self.base_url: URL = URL(base_url)
+        self.client: AsyncClient = AsyncClient(headers={"Authorization": f"Bearer {token}"})
+        self.raw_data: list[str] = [base_url, token]
+
+    def update_raw_data(self, base_url: str, token: str):
+        if self.raw_data != [base_url, token]:
+            self.base_url = URL(base_url)
+            self.client.headers["Authorization"] = f"Bearer {token}"
+            self.raw_data = [base_url, token]
+
+    async def getAuthStatus(self, token: str | None = None) -> Response:
+        return await self.client.post(
+            str(self.base_url / "api/v1/auth/status"),
+            headers={"Authorization": f"Bearer {token}" if token else self.client.headers.get("Authorization")},
+        )
+
+    async def checkAuthStatus(self, token: str | None = None) -> bool:
+        try:
+            response = await self.getAuthStatus(token)
+            return response.status_code == 200
+        except Exception:
+            return False
+
+    async def createMemo(self, content: str, visibility: str = "VISIBILITY_UNSPECIFIED") -> Response:
+        return await self.client.post(
+            str(self.base_url / "api/v1/memos"),
+            json={"content": content, "visibility": visibility},
+        )
+
+    async def createResource(
+        self,
+        *,
+        name: str = "",
+        uid: str = "",
+        createTime: str = "",
+        filename: str,
+        content: str | bytes,
+        externalLink: str = "",
+        type_: str | None = None,
+        size: str,
+        memo: str,
+    ):
+        if createTime == "":
+            # "2025-02-02T11:16:35.944Z"
+            createTime = datetime.now(tz=timezone.utc).isoformat()
+        if type_ is None:
+            if isinstance(content, str):
+                type_ = guess_mime(b64decode(content).decode()) or ""
+            else:
+                type_ = guess_mime(content) or ""
+        if isinstance(content, bytes):
+            content = b64encode(content).decode()
+        return await self.client.post(
+            str(self.base_url / "api/v1/resources"),
+            json={
+                "name": name,
+                "uid": uid,
+                "createTime": createTime,
+                "filename": filename,
+                "content": content,
+                "externalLink": externalLink,
+                "type": type_,
+                "size": size,
+                "memo": memo,
+            },
+        )
+
+    async def createMemoWithModel(self, content: str, visibility: str = "VISIBILITY_UNSPECIFIED") -> Memo:
+        response = await self.createMemo(content, visibility)
+        response.raise_for_status()
+        return get_type_adapter(Memo).validate_json(response.text)
+
+    def buildMemoUrl(self, memo_uid: str) -> str:
+        return str(self.base_url / f"m/{memo_uid}")
+
+    async def getMemoByUid(self, uid: str) -> Response:
+        return await self.client.get(str(self.base_url / f"api/v1/memos:by-uid/{uid}"))
+
+    async def getMemoById(self, memo_id: int) -> Response:
+        return await self.client.get(str(self.base_url / f"api/v1/memos/{memo_id}"))
+
+    async def getMemoByName(self, memo_name: str) -> Response:
+        return await self.client.get(str(self.base_url / f"api/v1/{memo_name}"))
+
+    async def getMemoByUidWithModel(self, uid: str) -> Memo:
+        response = await self.getMemoByUid(uid)
+        response.raise_for_status()
+        return get_type_adapter(Memo).validate_json(response.text)
+
+    async def getMemoByIdWithModel(self, memo_id: int) -> Memo:
+        response = await self.getMemoById(memo_id)
+        response.raise_for_status()
+        return get_type_adapter(Memo).validate_json(response.text)
+
+    async def getMemoByNameWithModel(self, memo_name: str) -> Memo:
+        response = await self.getMemoByName(memo_name)
+        response.raise_for_status()
+        return get_type_adapter(Memo).validate_json(response.text)
+
+    async def createComment(
+        self, memo_name: str, content: str, visibility: Visibility | Type[UnSet] = UnSet, resources: list | None = None
+    ) -> Response:
+        data: dict[str, Any] = {
+            "content": content,
+        }
+        if visibility is not UnSet:
+            data["visibility"] = visibility
+        if resources:
+            data["resources"] = resources
+        return await self.client.post(
+            str(self.base_url / f"api/v1/{memo_name}/comments"),
+            json=data,
+        )
+
+    async def createCommentWithModel(
+        self, memo_name: str, content: str, visibility: Visibility | Type[UnSet] = UnSet, resources: list | None = None
+    ) -> Memo:
+        response = await self.createComment(memo_name, content, visibility, resources)
+        response.raise_for_status()
+        return get_type_adapter(Memo).validate_json(response.text)
---
+++ packages/nonebot-plugin-memos/nonebot_plugin_memos-0.1.0b15-py3-none-any.whl/nonebot_plugin_memos/db.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+
+from nonebot_plugin_orm import Model
+from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy import JSON, ForeignKey, Integer, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Memo(Model):
+    __tablename__: str = "nonebot_plugin_memos_memo"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    user_id: Mapped[str] = mapped_column(nullable=False)
+    user_info: Mapped[str] = mapped_column(nullable=False)
+    session_persist_id: Mapped[int] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    token: Mapped[str] = mapped_column(nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+
+class Setting(Model):
+    __tablename__: str = "nonebot_plugin_memos_setting"
+
+    user_id: Mapped[str] = mapped_column(primary_key=True)
+    default_memo: Mapped[int | None] = mapped_column(
+        Integer,
+        ForeignKey(f"{str(Memo.__tablename__)}.{Memo.id.key}", ondelete="SET NULL"),
+        nullable=True,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+
+class MemoRecord(Model):
+    __tablename__: str = "nonebot_plugin_memos_memo_record"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    memo: Mapped[int] = mapped_column(Integer, ForeignKey(f"{str(Memo.__tablename__)}.{Memo.id.key}"), nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    uid: Mapped[str] = mapped_column(nullable=False)
+    session_persist_id: Mapped[int] = mapped_column(nullable=False)
+    message_id: Mapped[str] = mapped_column(nullable=False)
+    message_ids: Mapped[list[str]] = mapped_column(MutableList.as_mutable(JSON), nullable=False)
+    message: Mapped[str] = mapped_column(nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
