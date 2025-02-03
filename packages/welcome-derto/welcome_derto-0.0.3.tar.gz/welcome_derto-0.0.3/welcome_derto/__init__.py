8b6c7818bfee47ab24128923bb30cd8148991f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:37 +0000

    Add package welcome-derto/0.0.3/welcome_derto-0.0.3.tar.gz

---
+++ packages/welcome-derto/welcome_derto-0.0.3.tar.gz/welcome_derto-0.0.3/welcome_derto/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 10:05:41 2025
+
+@author: DiMartino
+"""
+
+from . import welcome_derto
+
+welcome_user = welcome_derto.welcome_user
+welcome_user_anfia = welcome_derto.welcome_user_anfia
