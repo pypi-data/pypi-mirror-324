4b20b8d6dd5c1882a8728277411520721b8bd30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:30 +0000

    Add package HydrogenLib-Next/0.1.7/hydrogenlib_next-0.1.7.tar.gz

---
+++ packages/HydrogenLib-Next/hydrogenlib_next-0.1.7.tar.gz/hydrogenlib_next-0.1.7/src/hydrolib/env_plus.py
@@ -0,0 +1,20 @@
+import os
+from collections import deque
+
+
+def split(string):
+    in_string = False
+    escope = False
+
+    temp = ''
+    results = deque()
+    for char in string:
+        ...
+
+
+
+
+class EnvionmentVarieble(list):
+    def __init__(self, value_string, split_char=';'):
+        ...
+
