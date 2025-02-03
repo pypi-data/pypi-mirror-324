e6bbeb23d522fea431a3f56934b73e15f7478b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:29 +0000

    Add package codecs-protocol/1.0.1/codecs_protocol-1.0.1.tar.gz

---
+++ packages/codecs-protocol/codecs_protocol-1.0.1.tar.gz/codecs_protocol-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codecs_protocol",
+    version="1.0.1", 
+    author="Chandan Sharma",
+    author_email="chandansharma7012@gmail.com",
+    description="A Python library for decoding AVL packets (Codec8 & Codec8E).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[], 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
