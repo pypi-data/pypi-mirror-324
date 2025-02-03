667f43f3dc2a303be2ddfce8c8cca51953473c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:17 +0000

    Add package badgerdb/0.7.0/badgerdb-0.7.0.tar.gz

---
+++ packages/badgerdb/badgerdb-0.7.0.tar.gz/badgerdb-0.7.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    description = f.read()   
+
+setup(
+    name="badgerdb",
+    author="Poorna Prakash SR",
+    author_email="poornaprakashsr@gmail.com",
+    description = "Python wrapper for BadgerDB",
+    version="0.7.0",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    long_description=description,
+    long_description_content_type='text/markdown',
+    package_data={
+        "badgerdb": ["libbadger.so"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
