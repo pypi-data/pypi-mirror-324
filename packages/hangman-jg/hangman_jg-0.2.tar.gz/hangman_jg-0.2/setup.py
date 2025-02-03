94148fc2b49e3833177e282cb9371b98a69ab739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:35 +0000

    Add package hangman-jg/0.2/hangman_jg-0.2.tar.gz

---
+++ packages/hangman-jg/hangman_jg-0.2.tar.gz/hangman_jg-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hangman-jg',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[], 
+    description="A quick and easy way to play hangman!",
+    author="Jack Grant",
+    author_email="classifiedcontentonyt@gmail.com",
+    url="https://github.com/astraixe-coding/hangman"
+)
