b9adf3dd2bfcf8d4eff071170703c9e68214daf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:22 +0000

    Add package jmz-tool/0.0.5/jmz-tool-0.0.5.tar.gz

---
+++ packages/jmz-tool/jmz-tool-0.0.5.tar.gz/jmz-tool-0.0.5/cryptojs/crypto.py
@@ -0,0 +1,22 @@
+import  execjs
+
+
+
+class CryptoJS:
+    def __init__(self):
+        pass
+
+    def desc(self):
+        with open('js/crypto.js', 'r', encoding='utf-8') as f:
+            js_code = f.read()
+        ctx = execjs.compile(js_code)
+        print(ctx.call('desc'))
+        print("一个简易工具提供常见加密解密功能如MD5,AES,SHA1等,使用该工具前请确保已安装node-js")
+
+    def md5(self, data):
+        pass
+    def sha1(self, data):
+        pass
+    def sha256(self, data):
+        pass
+
