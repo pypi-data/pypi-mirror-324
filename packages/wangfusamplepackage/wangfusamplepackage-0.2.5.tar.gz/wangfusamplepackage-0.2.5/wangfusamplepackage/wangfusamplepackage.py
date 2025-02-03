2cfccea8b00ac7ac40f994b45590ad4cba55154f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 05:06:03 +0000

    Add package wangfusamplepackage/0.2.5/wangfusamplepackage-0.2.5.tar.gz

---
+++ packages/wangfusamplepackage/wangfusamplepackage-0.2.5.tar.gz/wangfusamplepackage-0.2.5/wangfusamplepackage/wangfusamplepackage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK 
+
+import argparse
+import argcomplete
+
+def install():
+    print("install the application...")
+
+def update():
+    print("update the application...")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Manager my package")
+
+    # Define commands
+    parser.add_argument(
+        "command", choices=["install", "uninstall", "update"], help="Command to execute"
+    )
+    subparsers = parser.add_subparsers()
+
+    start_parser = subparsers.add_parser("install", help="Start the application")
+    start_parser.set_defaults(func=install)
+
+    restart_parser = subparsers.add_parser("update", help="update the application")
+    restart_parser.set_defaults(func=update)
+
+    argcomplete.autocomplete(parser)
+
+    args = parser.parse_args()
+    print(f"Executing {args.command}...")
+
+if __name__ == '__main__':
+    main()
