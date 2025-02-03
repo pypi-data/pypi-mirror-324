3150fd5a8852a4747367c7a9388427dcab8f52b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:53 +0000

    Add package daggerml-cli/0.0.12/daggerml_cli-0.0.12.tar.gz

---
+++ packages/daggerml-cli/daggerml_cli-0.0.12.tar.gz/daggerml_cli-0.0.12/src/daggerml_cli/contrib/lambda_adapter.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+import boto3
+
+
+def main():
+    response = boto3.client("lambda").invoke(
+        FunctionName=sys.argv[1],
+        InvocationType="RequestResponse",
+        LogType="Tail",
+        # extra json.dumps because lambda will json.loads
+        Payload=json.dumps(sys.stdin.read().strip()).encode(),
+    )
+    payload = json.loads(response["Payload"].read())
+    if payload["context"] is not None:
+        print(payload["context"], file=sys.stderr)
+    if payload["dump"] is not None:
+        print(payload["dump"])
