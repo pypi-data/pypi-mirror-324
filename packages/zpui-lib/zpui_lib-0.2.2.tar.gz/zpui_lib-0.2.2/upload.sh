29eea29b1dd0b350a79c2a6b8fb89c7e28a7d867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:06:56 +0000

    Add package zpui-lib/0.2.2/zpui_lib-0.2.2.tar.gz

---
+++ packages/zpui-lib/zpui_lib-0.2.2.tar.gz/zpui_lib-0.2.2/upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+rm -rf dist/*
+nano pyproject.toml
+git add pyproject.toml
+python3 -m build
+pip install -U --force-reinstall dist/*.whl
+python3 -m twine upload dist/*
+python3 -m build
