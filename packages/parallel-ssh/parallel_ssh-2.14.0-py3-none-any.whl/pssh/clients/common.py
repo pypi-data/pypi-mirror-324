d37bb9588dc624a6c356279a81771c2ac0159084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:08:53 +0000

    Add package parallel-ssh/2.14.0/parallel_ssh-2.14.0-py3-none-any.whl

---
+++ packages/parallel-ssh/parallel_ssh-2.14.0-py3-none-any.whl/pssh/clients/common.py
@@ -0,0 +1,41 @@
+# This file is part of parallel-ssh.
+#
+# Copyright (C) 2014-2022 Panos Kittenis and contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, version 2.1.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os
+
+from ..exceptions import PKeyFileError
+
+
+def _validate_pkey_path(pkey):
+    if pkey is None:
+        return
+    pkey = os.path.normpath(os.path.expanduser(pkey))
+    if not os.path.exists(pkey):
+        msg = "File %s does not exist. " \
+              "Please use either absolute or relative to user directory " \
+              "paths like '~/.ssh/my_key' for pkey parameter"
+        ex = PKeyFileError(msg, pkey)
+        raise ex
+    return pkey
+
+
+def _validate_pkey(pkey):
+    if pkey is None:
+        return
+    if isinstance(pkey, str):
+        return _validate_pkey_path(pkey)
+    return pkey
