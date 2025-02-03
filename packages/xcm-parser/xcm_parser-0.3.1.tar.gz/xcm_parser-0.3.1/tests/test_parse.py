2055d055dd5f0ebe3169fbfde7c60cb54aaeb561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:22 +0000

    Add package xcm-parser/0.3.1/xcm_parser-0.3.1.tar.gz

---
+++ packages/xcm-parser/xcm_parser-0.3.1.tar.gz/xcm_parser-0.3.1/tests/test_parse.py
@@ -0,0 +1,18 @@
+""" text_elevator_cd_pdf.py - test Starr and xUML notation Elevator class diagram pdf output"""
+
+import pytest
+from pathlib import Path
+from xcm_parser.class_model_parser import ClassModelParser
+
+diagrams = [
+    "elevator",
+    "intersection",
+    "sheet",
+    "ego"
+]
+
+@pytest.mark.parametrize("model", diagrams)
+def test_Starr_pdf(model):
+
+    result = ClassModelParser.parse_file(file_input=Path(f"models/{model}.xcm"), debug=False)
+    assert result
