cad2a959745aa94659632a80dc1a3dcfe44d7df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:09:08 +0000

    Add package asix/7.1/asix-7.1.tar.gz

---
+++ packages/asix/asix-7.1.tar.gz/asix-7.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asix",            # A neutral name for the library
+    version="7.1",
+    packages=find_packages(),
+    install_requires=[
+        "pygame>=2.1.0"
+    ],
+    author="Coataoc",
+    author_email="coataoctrader@gmail.com",
+    description="A collection of utilities for Pygame",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/CoataocCreate/ASIX",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
