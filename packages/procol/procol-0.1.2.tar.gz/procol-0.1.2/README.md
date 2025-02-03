24c62005e0acb9b0ef68669680e54e86c08710c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:23 +0000

    Add package procol/0.1.2/procol-0.1.2.tar.gz

---
+++ packages/procol/procol-0.1.2.tar.gz/procol-0.1.2/README.md
@@ -0,0 +1,3 @@
+# ProCol
+
+Python package extended in Rust to do expensive probability calculations. Currently only the [filling probability](https://stats.stackexchange.com/questions/660886/general-formula-for-the-filling-probability-with-respect-to-a-finite-collectio).
---
+++ packages/procol/procol-0.1.2.tar.gz/procol-0.1.2/src/lib.rs
@@ -0,0 +1,47 @@
+use pyo3::prelude::*;
+use itertools::Itertools;
+
+/// Calculate the probability that none of the specified events occur in any of the n trials.
+fn single_event_prob(n: usize, event_indices: &[usize], probabilities: &[Vec<f64>]) -> f64 {
+    (0..n).fold(1.0, |acc, i| {
+        acc * (1.0 - event_indices.iter().map(|&j| probabilities[i][j]).sum::<f64>())
+    })
+}
+
+/// Calculate the filling probability of observing each of the m events at least once
+/// among the n independent discrete random variables with given probabilities.
+#[pyfunction]
+fn filling_prob(n: usize, m: usize, probabilities: Vec<Vec<f64>>) -> PyResult<f64> {
+    if probabilities.len() != n {
+        return Err(pyo3::exceptions::PyValueError::new_err("Length of probabilities should be equal to n"));
+    }
+
+    for p in &probabilities {
+        if p.len() != m {
+            return Err(pyo3::exceptions::PyValueError::new_err("Each probability list should have a length of m"));
+        }
+    }
+
+    let mut filling_prob = 1.0;
+    
+    for k in 1..=m {
+        let mut sum_comb = 0.0;
+        
+        for event_indices in (0..m).combinations(k) {
+            sum_comb += single_event_prob(n, &event_indices, &probabilities);
+        }
+        
+        filling_prob -= (-1.0f64).powi((k + 1) as i32) * sum_comb;
+    }
+
+    filling_prob = f64::max(filling_prob, 0.0);
+    
+    Ok(filling_prob)
+}
+
+/// A module to be used in Python.
+#[pymodule]
+fn procol(module: &Bound<'_, PyModule>) -> PyResult<()> {
+    module.add_function(wrap_pyfunction!(filling_prob, module)?)?;
+    Ok(())
+}
