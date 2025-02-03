4f3719b96393afaa25fe64a849d84bb64826b6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:58 +0000

    Add package snowflake2dbt/0.0.2/snowflake2dbt-0.0.2.tar.gz

---
+++ packages/snowflake2dbt/snowflake2dbt-0.0.2.tar.gz/snowflake2dbt-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+setup(
+        name="snowflake2dbt", 
+        version=VERSION,
+        author="Narendra",
+        author_email="<narendra@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
