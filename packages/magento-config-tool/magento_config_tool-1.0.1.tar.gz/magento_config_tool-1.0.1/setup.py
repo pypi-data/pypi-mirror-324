f4c69979a03545478809a636e22da183d6aec26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:56 +0000

    Add package magento-config-tool/1.0.1/magento_config_tool-1.0.1.tar.gz

---
+++ packages/magento-config-tool/magento_config_tool-1.0.1.tar.gz/magento_config_tool-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="magento-config-tool",
+    version="1.0.1",
+    author="Yehor Shytikov",
+    author_email="yegorshytikov@email.com",
+    description="A terminal-based Magento configuration management tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/genaker/magento-config-tool",  # Update this
+    packages=find_packages(),
+    install_requires=[
+        "tabulate",
+        "sqlalchemy",
+        "mysql-connector-python",
+    ],
+    entry_points={
+        "console_scripts": [
+            "mage-conf=magento_config_tool.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Environment :: Console",
+    ],
+    python_requires=">=3.6",
+)
