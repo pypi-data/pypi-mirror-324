0d8adce3dc1d05bb56e2bb9ef49673b1429ee1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:45 +0000

    Add package swanlab/0.4.6/swanlab-0.4.6-py3-none-any.whl

---
+++ packages/swanlab/swanlab-0.4.6-py3-none-any.whl/swanlab/cli/commands/auth/login.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024/6/12 21:11
+@File: login.py
+@IDE: pycharm
+@Description:
+    登录模块
+"""
+import click
+from swankit.log import FONT
+
+from swanlab.api.auth import terminal_login
+from swanlab.package import has_api_key
+
+
+@click.command()
+@click.option(
+    "--relogin",
+    "-r",
+    is_flag=True,
+    default=False,
+    help="Relogin to the swanlab cloud, it will recover the token file.",
+)
+@click.option(
+    "--api-key",
+    "-k",
+    default=None,
+    type=str,
+    help="If you prefer not to engage in commands-line interaction to input the api key, "
+    "this will allow automatic login.",
+)
+def login(api_key: str, relogin: bool):
+    """Login to the swanlab cloud."""
+    if not relogin and has_api_key():
+        # 此时代表token已经获取，需要打印一条信息：已经登录
+        command = FONT.bold("swanlab login --relogin")
+        tip = FONT.swanlab("You are already logged in. Use `" + command + "` to force relogin.")
+        return print(tip)
+    # 进行登录，此时将直接覆盖本地token文件
+    login_info = terminal_login(api_key)
+    print(FONT.swanlab("Login successfully. Hi, " + FONT.bold(FONT.default(login_info.username))) + "!")
