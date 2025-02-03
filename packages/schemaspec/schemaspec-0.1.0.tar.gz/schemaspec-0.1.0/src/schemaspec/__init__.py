7ac7cd7cb68f3cfbd1e8b3171f796a452a2093ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:18 +0000

    Add package schemaspec/0.1.0/schemaspec-0.1.0.tar.gz

---
+++ packages/schemaspec/schemaspec-0.1.0.tar.gz/schemaspec-0.1.0/src/schemaspec/__init__.py
@@ -0,0 +1,4 @@
+from .core import Array, Enum, Integer, Schema, String
+from .service import Service, grpc, http
+
+__all__ = ["Schema", "String", "Integer", "Enum", "Array", "Service", "grpc", "http"]
