813810e5f2c2b6d9969c71abbaf67eb97d2690c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:51 +0000

    Add package swiftencrypt/0.1.0/swiftencrypt-0.1.0.tar.gz

---
+++ packages/swiftencrypt/swiftencrypt-0.1.0.tar.gz/swiftencrypt-0.1.0/readme.rst
@@ -0,0 +1,6 @@
+*Copyright (c) 2025 Sean Yeatts, Inc. All rights reserved.*
+
+SwiftEncrypt
+============
+
+A simple way to handle basic data encryption.
---
+++ packages/swiftencrypt/swiftencrypt-0.1.0.tar.gz/swiftencrypt-0.1.0/swiftencrypt/__init__.py
@@ -0,0 +1,16 @@
+"""
+SwiftEncrypt
+============
+
+Copyright (c) 2025 Sean Yeatts. All rights reserved.
+
+A simple way to handle basic data encryption.
+"""
+
+from __future__ import annotations
+
+__title__       = "swiftserialize"
+__author__      = "Sean Yeatts"
+__copyright__   = "Copyright (c) 2025 Sean Yeatts. All rights reserved."
+
+from .swiftencrypt import *
