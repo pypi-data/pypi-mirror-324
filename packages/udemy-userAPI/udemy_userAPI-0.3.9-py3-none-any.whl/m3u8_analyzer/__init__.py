c28067361310bbf39c9a8b6261c320387d1e8127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:07:02 +0000

    Add package udemy-userAPI/0.3.9/udemy_userAPI-0.3.9-py3-none-any.whl

---
+++ packages/udemy-userAPI/udemy_userAPI-0.3.9-py3-none-any.whl/m3u8_analyzer/__init__.py
@@ -0,0 +1,7 @@
+# m3u8_analyzer/__init__.py
+
+from .M3u8Analyzer import M3u8Analyzer, Wrapper,EncryptSuport
+
+__all__ = ['M3u8Analyzer', 'Wrapper','EncryptSuport']
+if __name__ == '__main__':
+    raise RuntimeError("no escope!")
