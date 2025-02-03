78d1fc509e45dedecf46554c30daf9775e1a6041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:08:20 +0000

    Add package dbiu-databases/1/dbiu_databases-1.tar.gz

---
+++ packages/dbiu-databases/dbiu_databases-1.tar.gz/dbiu_databases-1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+try:
+    originalFile = './dbiu_databases/bahn.db'  # modify this line to choose a database file
+    dbFile = './dbiu_databases/base.db'
+    bakFile = dbFile+'.bak'
+
+    if os.path.isfile(dbFile):
+        os.rename(dbFile, bakFile)
+    os.rename(originalFile, dbFile)
+
+
+
+    setup(
+        name='dbiu_databases',
+        version='1',  # modify this line to set a package version, one version for each DB
+        packages=find_packages(),
+        install_requires=[
+        ],
+        long_description=description,
+        long_description_content_type="text/markdown",
+        package_data={
+            'dbiu_databases': [
+                'base.db'
+                ]
+        },
+        include_package_data=True
+    )
+
+finally:
+    if os.path.isfile(dbFile):
+        os.rename(dbFile,originalFile)
+    if os.path.isfile(bakFile):
+        os.rename(bakFile,dbFile)
