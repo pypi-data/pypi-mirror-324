17dce1edae4f4bfae970a5f55c9db6fd3c745a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:09:02 +0000

    Add package cookit/0.9.1/cookit-0.9.1-py3-none-any.whl

---
+++ packages/cookit/cookit-0.9.1-py3-none-any.whl/cookit/loguru/common.py
@@ -0,0 +1,36 @@
+from collections.abc import Iterator
+from contextlib import contextmanager
+from functools import partial
+from typing import Union
+
+from loguru import logger
+
+from ..common import LazyGetterType, lazy_get
+
+
+@contextmanager
+def logged_suppress(
+    msg: "LazyGetterType[str, [Exception]]",
+    *t: type[Exception],
+    level: Union[int, str] = "ERROR",
+    log_stack: bool = True,
+    debug_stack: bool = False,
+) -> Iterator[None]:
+    try:
+        yield
+    except Exception as e:
+        if t and (not isinstance(e, t)):
+            raise
+        lg = logger.opt(exception=e) if log_stack else logger
+        lg.log(level, lazy_get(msg, e))
+        if debug_stack:
+            logger.opt(exception=e).debug("Stacktrace")
+        return
+
+
+warning_suppress = partial(
+    logged_suppress,
+    level="WARNING",
+    log_stack=False,
+    debug_stack=True,
+)
