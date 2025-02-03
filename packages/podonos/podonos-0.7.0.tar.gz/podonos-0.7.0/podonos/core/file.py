e21750ce5aa1c3175277c0b83195d003a2d6284b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 16:06:30 +0000

    Add package podonos/0.7.0/podonos-0.7.0.tar.gz

---
+++ packages/podonos/podonos-0.7.0.tar.gz/podonos-0.7.0/podonos/core/file.py
@@ -0,0 +1,56 @@
+from typing import List, Optional
+
+from podonos.core.base import *
+
+
+class File:
+    _path: str
+    _tags: List[str]
+    _script: Optional[str]
+
+    def __init__(self, path: str, model_tag: str, tags: List[str] = [], script: Optional[str] = None, is_ref: bool = False) -> None:
+        """
+        Args:
+            path: Path to the file to evaluate. Required.
+            model_tag: String that represents the model or group. Required.
+            tags: A list of string for file. Optional.
+            script: Script of the input audio in text. Optional.
+            is_ref: True if this file is to be a reference for an evaluation type that requires a reference.
+                    Optiona. Default is False.
+        """
+        log.check_ne(path, "")
+        log.check_ne(model_tag, "")
+        self._path = path
+        self._model_tag = model_tag
+        self._tags = self._set_tags(tags)
+        self._script = script
+        self._is_ref = is_ref
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def model_tag(self) -> str:
+        return self._model_tag
+
+    @property
+    def tags(self) -> List[str]:
+        return self._tags
+
+    @property
+    def script(self) -> Optional[str]:
+        return self._script
+
+    @property
+    def is_ref(self) -> Optional[bool]:
+        return self._is_ref
+
+    def _set_tags(self, tags: List[str]) -> List[str]:
+        unique_tags = []
+        seen = set()
+        for tag in tags:
+            if tag not in seen:
+                seen.add(tag)
+                unique_tags.append(tag)
+        return unique_tags
