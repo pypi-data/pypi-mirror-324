4f54c7b4587199850b6de1b02dfbd286c57189af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:08:33 +0000

    Add package easyDataShare/1.0/easydatashare-1.0.tar.gz

---
+++ packages/easyDataShare/easydatashare-1.0.tar.gz/easydatashare-1.0/dataShare/__init__.py
@@ -0,0 +1,1 @@
+from .main import easyShare
---
+++ packages/easyDataShare/easydatashare-1.0.tar.gz/easydatashare-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easyDataShare',
+    version='1.0',
+    packages=find_packages(),
+    description='Easiest dataSharing library',
+    
+)
