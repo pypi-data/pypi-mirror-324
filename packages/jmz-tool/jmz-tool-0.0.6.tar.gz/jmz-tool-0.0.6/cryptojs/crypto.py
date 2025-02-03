c67dbcbb59131a6a7aeff7a4ae56643b770b80c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 15:06:42 +0000

    Add package jmz-tool/0.0.6/jmz-tool-0.0.6.tar.gz

---
+++ packages/jmz-tool/jmz-tool-0.0.6.tar.gz/jmz-tool-0.0.6/cryptojs/crypto.py
@@ -0,0 +1,19 @@
+import  execjs
+
+
+
+class CryptoJS:
+    def __init__(self):
+        pass
+
+    def desc(self):
+        print('你好')
+        print("一个简易工具提供常见加密解密功能如MD5,AES,SHA1等,使用该工具前请确保已安装node-js")
+
+    def md5(self, data):
+        pass
+    def sha1(self, data):
+        pass
+    def sha256(self, data):
+        pass
+
---
+++ packages/jmz-tool/jmz-tool-0.0.6.tar.gz/jmz-tool-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jmz-tool",
+    version="0.0.6",
+    author="jinmingzhou",
+    author_email="17816765317@163.com",
+    description="一个简易工具提供常见加密解密功能如MD5,AES,SHA1等",
+    long_description=open("README.md",encoding='utf8').read(),
+    long_description_content_type="text/markdown",
+    url="",
+    download_url="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "PyExecJS2>=1.6.1",
+        'requests'
+        #"requests>=2.25.1",
+        #"numpy>=1.19.5",
+    ],
+    python_requires=">=3.10"
+)
