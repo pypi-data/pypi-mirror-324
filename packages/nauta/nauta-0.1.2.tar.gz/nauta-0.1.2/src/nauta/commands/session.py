492e6ab3c5ef827e5fa294fe07df48b6d42f3c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 04:08:34 +0000

    Add package nauta/0.1.2/nauta-0.1.2.tar.gz

---
+++ packages/nauta/nauta-0.1.2.tar.gz/nauta-0.1.2/src/nauta/commands/session.py
@@ -0,0 +1,59 @@
+from typing import Annotated
+
+import typer
+
+from nauta.client import NautaClient
+from nauta.database import get_account
+from nauta.secure import decrypt_password, generate_key
+
+
+def login():
+    """Inicia sesión en el nauta hogar"""
+
+    account = get_account(is_default=True)
+
+    if account:
+        password = decrypt_password(account.password, generate_key())
+        nauta_client = NautaClient(correo=account.email, password=password)
+        nauta_client.login()
+
+    else:
+        typer.echo(typer.style("Establece un usuario por defecto", fg="yellow"))
+
+
+def logout(
+    force: Annotated[bool, typer.Option(help="Forzar cierre de sesión")] = False
+):
+    """Cierra la sesión del nauta hogar"""
+
+    account = get_account(is_default=True)
+
+    if account:
+        password = decrypt_password(account.password, generate_key())
+        nauta_client = NautaClient(correo=account.email, password=password)
+        nauta_client.logout(force=force)
+
+    else:
+        typer.echo(typer.style("Establece un usuario por defecto", fg="yellow"))
+
+
+def time():
+    """Muestra el tiempo disponible"""
+
+    account = get_account(is_default=True)
+
+    if account:
+        password = decrypt_password(account.password, generate_key())
+        nauta_client = NautaClient(correo=account.email, password=password)
+        available_time = nauta_client.available_time()
+
+        if available_time:
+
+            typer.echo(
+                typer.style(
+                    f"Tiempo disponible: {available_time}", fg="cyan", bold=True
+                )
+            )
+
+    else:
+        typer.echo(typer.style("Establece un usuario por defecto", fg="yellow"))
---
+++ packages/nauta/nauta-0.1.2.tar.gz/nauta-0.1.2/src/nauta/models.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Account:
+    id: int
+    email: str
+    password: str
+    is_default: bool
+
+
+@dataclass
+class Session:
+    id: int
+    csrfhw: str
+    username: str
+    wlanuserip: str
+    attribute_uuid: str
+    created_at: int
