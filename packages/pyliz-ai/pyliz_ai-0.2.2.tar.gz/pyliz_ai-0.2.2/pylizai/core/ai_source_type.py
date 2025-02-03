4a01e6b03db6d014f7a44a4f242259e67690500a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:54 +0000

    Add package pyliz-ai/0.2.2/pyliz_ai-0.2.2.tar.gz

---
+++ packages/pyliz-ai/pyliz_ai-0.2.2.tar.gz/pyliz_ai-0.2.2/pylizai/core/ai_source_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+from pylizai.model.ai_model_list import AiModelList
+
+
+class AiSourceType(Enum):
+    OLLAMA_SERVER = "Ollama server",
+    LMSTUDIO_SERVER = "LMM studio server",
+    LOCAL_LLAMACPP = "Local (Llamacpp)",
+    LOCAL_WHISPER = "Local (Whisper)",
+    API_MISTRAL = "Mistral API"
+    API_GEMINI = "Gemini API"
+
+
