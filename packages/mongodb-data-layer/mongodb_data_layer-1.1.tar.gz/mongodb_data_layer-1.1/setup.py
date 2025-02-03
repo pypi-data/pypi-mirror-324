65d595ec5cc7b03f2ddbc062756266f84e189f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:20 +0000

    Add package mongodb-data-layer/1.1/mongodb_data_layer-1.1.tar.gz

---
+++ packages/mongodb-data-layer/mongodb_data_layer-1.1.tar.gz/mongodb_data_layer-1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'mongodb_data_layer/README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='mongodb-data-layer',
+    description='A data layer for mongodb',
+    version='1.1',
+    packages=find_packages(include=['mongodb_data_layer', 'mongodb_data_layer.*']),
+    install_requires=[
+        'pymongo',
+        'python-dotenv'
+    ],
+    author="Marjon Godito",
+    entry_points={
+        'console_scripts': [
+            'generate-model=mongodb_data_layer.generate_model:main'
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
