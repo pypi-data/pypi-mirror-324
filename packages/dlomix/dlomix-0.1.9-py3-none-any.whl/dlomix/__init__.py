b1af46d0842e1fb07ccf95a5f060c16f908112e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:18 +0000

    Add package dlomix/0.1.9/dlomix-0.1.9-py3-none-any.whl

---
+++ packages/dlomix/dlomix-0.1.9-py3-none-any.whl/dlomix/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.9"
+
+META_DATA = {
+    "author": "Wilhelm Lab",
+    "author_email": "o.shouman@tum.de",
+    "description": "Deep Learning for Proteomics",
+    "package_name": "DLOmix",
+    "copyright_text": "2025, Wilhelm Lab, TU Munich, School of Life Sciences",
+    "github_url": "https://github.com/wilhelm-lab/dlomix",
+}
