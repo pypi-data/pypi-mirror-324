072f3d42aba99ba2e80d53a3801caedd9dbb1814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 14:06:22 +0000

    Add package django-lispy-filter/0.1.3/django_lispy_filter-0.1.3.tar.gz

---
+++ packages/django-lispy-filter/django_lispy_filter-0.1.3.tar.gz/django_lispy_filter-0.1.3/src/django_lispy_filter/__init__.py
@@ -0,0 +1,9 @@
+"""
+Build a easy to use filter interface for Users to filter objects.
+"""
+
+__version__ = "0.1.3"
+
+from .lib import LispyFilterParser
+# TODO remove get_model_schema
+from .utils import get_models_schema
