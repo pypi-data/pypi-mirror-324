7f0ed3ce369095971e07f82ed96966268f2a4769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:00 +0000

    Add package lightcnn/0.1.9/lightcnn-0.1.9.tar.gz

---
+++ packages/lightcnn/lightcnn-0.1.9.tar.gz/lightcnn-0.1.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lightcnn",
+    version="0.1.9",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchvision",
+        "opencv-python",
+        "facenet-pytorch",
+        "scipy",
+        "pillow"
+    ],
+    author="Yernar Bekbolat",
+    author_email="dvayernar@gmail.com",
+    description="LightCNN for fast, accurate and lightweight face verification",
+    long_description="""
+    # LightCNN
+
+    Fast and accurate face recognition model optimized for Central Asian faces.
+
+    ## GitHub
+    For full documentation, examples, and source code:
+    https://github.com/YernarBekbolat/LightCNN-V4-PyTorch
+    """,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+    package_data={
+        "lightcnn": ["weights/*.pth.tar"],
+    },
+) 
