9dfebb151310580b4de7a99e9cf78fce9b843443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-03 03:07:47 +0000

    Add package nonebot-plugin-smallapi/1.1.5/nonebot_plugin_smallapi-1.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-smallapi/nonebot_plugin_smallapi-1.1.5-py3-none-any.whl/nonebot_plugin_smallapi/__init__.py
@@ -0,0 +1,27 @@
+from .config import LOAD_OK, config
+from nonebot.plugin import PluginMetadata
+
+if LOAD_OK:
+    try:
+        from .api_menu import *
+        from .api_pic import *
+        from .api_text import *
+        from .api_site import *
+    except Exception as e:
+        logger.error(f"Smallapi V1.1.5 Plugin load faild !")
+        logger.exception(e)
+    logger.success(f"Smallapi V1.1.5 Plugin is OK !")
+    logger.info("读取您的故梦API密钥为：" + config.token)
+
+    __plugin_meta__ = PluginMetadata(
+        name="小小API调用插件",
+        description="调用api来操福Q民～",
+        usage="发送“(API)图片/文字/站点系统”查看",
+        type="application",
+        homepage="https://github.com/chaichaisi/nonebot-plugin-smallapi",
+        extra={
+            "unique_name": "smallapi",
+            "author": "Chaichaisi <chaichaisi@qq.com>",
+            "version": "1.1.5",
+        },
+    )
