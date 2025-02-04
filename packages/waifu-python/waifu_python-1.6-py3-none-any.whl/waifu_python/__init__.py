78d368c0ee5ad323d7aed70eb15fa7788abe5cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:42 +0000

    Add package waifu-python/1.6/waifu_python-1.6-py3-none-any.whl

---
+++ packages/waifu-python/waifu_python-1.6-py3-none-any.whl/waifu_python/__init__.py
@@ -0,0 +1,6 @@
+from .waifu_im import WaifuIm
+from .waifu_pics import WaifuPics
+from .nekos_best import NekosBest
+from .nsfwbot import NSFWBot
+from .pics_re import PicRe
+from .purr_bot import PurrBot
