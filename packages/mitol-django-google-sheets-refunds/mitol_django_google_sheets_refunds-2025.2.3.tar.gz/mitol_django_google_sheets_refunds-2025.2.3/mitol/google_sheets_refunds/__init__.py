edf3f9477f3e0ee10594e62afda4402b7aff53a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:38 +0000

    Add package mitol-django-google-sheets-refunds/2025.2.3/mitol_django_google_sheets_refunds-2025.2.3.tar.gz

---
+++ packages/mitol-django-google-sheets-refunds/mitol_django_google_sheets_refunds-2025.2.3.tar.gz/mitol_django_google_sheets_refunds-2025.2.3/mitol/google_sheets_refunds/__init__.py
@@ -0,0 +1,6 @@
+"""mitol.google_sheets_refunds"""
+
+default_app_config = "mitol.google_sheets_refunds.apps.GoogleSheetsRefundsApp"
+
+__version__ = "2025.2.3"
+__distributionname__ = "mitol-django-google-sheets-refunds"
