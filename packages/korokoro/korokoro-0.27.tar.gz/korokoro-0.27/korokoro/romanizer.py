91e9b4433cd71c3dd14a1f33cb88d248adde830a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:42 +0000

    Add package korokoro/0.27/korokoro-0.27.tar.gz

---
+++ packages/korokoro/korokoro-0.27.tar.gz/korokoro-0.27/korokoro/romanizer.py
@@ -0,0 +1,127 @@
+import re
+import unicodedata
+
+from korokoro.syllable import Syllable
+from korokoro.pronouncer import Pronouncer
+
+'''
+### Transcribing vowels ###
+'''
+
+vowel = {
+    # 단모음 monophthongs
+    'ㅏ' : 'a',
+    'ㅓ' : 'eo',
+    'ㅗ' : 'o',
+    'ㅜ' : 'u',
+    'ㅡ' : 'eu',
+    'ㅣ' : 'i',
+    'ㅐ' : 'ae',
+    'ㅔ' : 'e',
+    'ㅚ' : 'oe',
+    'ㅟ' : 'wi',
+    
+    # 이중모음 diphthongs
+    'ㅑ' : 'ya',
+    'ㅕ' : 'yeo',
+    'ㅛ' : 'yo',
+    'ㅠ' : 'yu',
+    'ㅒ' : 'yae',
+    'ㅖ' : 'ye',
+    'ㅘ' : 'wa',
+    'ㅙ' : 'wae',
+    'ㅝ' : 'wo',
+    'ㅞ' : 'we',
+    'ㅢ' : 'ui', # [붙임 1] ‘ㅢ’는 ‘ㅣ’로 소리 나더라도 ‘ui’로 적는다.
+}
+
+'''
+### Transcribing consonants ###
+
+Consonants are defined in separate dicts, choseong and jongseong,
+for some characters are pronounced differently depending on 
+its position in the syllable.
+
+e.g. ㄱ, ㄷ, ㅂ, ㄹ are (g, d, b, r) in onset,
+                  but (k, t, p, l) in coda.
+e.g. ㅇ is a null sound when placed in onset, but becomes [ng] in coda.
+'''
+
+# 초성 Choseong (Syllable Onset)
+onset = {
+    # 파열음 stops/plosives
+    'ᄀ' : 'g',
+    'ᄁ' : 'kk',
+    'ᄏ' : 'k',
+    'ᄃ' : 'd',
+    'ᄄ' : 'tt',
+    'ᄐ' : 't',
+    'ᄇ' : 'b',
+    'ᄈ' : 'pp',
+    'ᄑ' : 'p',
+    # 파찰음 affricates
+    'ᄌ' : 'j',
+    'ᄍ' : 'jj',
+    'ᄎ' : 'ch',
+    # 마찰음 fricatives
+    'ᄉ' : 's',
+    'ᄊ' : 'ss',
+    'ᄒ' : 'h',
+    # 비음 nasals
+    'ᄂ' : 'n',
+    'ᄆ' : 'm',
+    # 유음 liquids
+    'ᄅ' : 'r',
+    # Null sound
+    'ᄋ' : '',
+}
+
+'''
+종성 Jongseong (Syllable Coda)
+
+"The 7 Jongseongs (7종성)"
+Only the seven consonants below may appear in coda position
+'''
+
+coda = {
+    # 파열음 stops/plosives
+    'ᆨ' : 'k',
+    'ᆮ' : 't',
+    'ᆸ' : 'p',
+    # 비음 nasals
+    'ᆫ' : 'n',
+    'ᆼ' : 'ng',
+    'ᆷ' : 'm',
+    # 유음 liquids
+    'ᆯ' : 'l',
+    
+    None: '',
+}
+    
+class Romanizer(object):
+    def __init__(self, text):
+        self.text = text
+
+    def romanize(self):
+        pronounced = Pronouncer(self.text).pronounced
+        hangul = r"[ㄱ-힣ᄀ-ㅣ]"
+        _romanized = ""
+        for char in pronounced:
+            if (re.match(hangul, char)):
+                s = Syllable(char)
+
+                if int(bool(s.medial)) + int(bool(s.final)) + int(bool(s.initial)) < 2: # lack to be complete word.
+                    bag = dict(vowel, **onset) # 종성만 있는 경우는 불가
+                    if char not in bag.keys():
+                        char = unicodedata.normalize('NFKC', char)
+                    _romanized += bag[char]
+                    if char == "ᄋ": # 자른 홑자음에 대해서도 이를 적용하기에는 대부분 이는 줄임말에 사용되기에 필요도가 떨어질 것이라고 생각
+                        _romanized += "ieung"
+                else:
+                    _romanized += onset[s.initial] + vowel[s.medial]
+                    if s.final in coda.keys(): # 맞춤법을 틀린 경우는 어쩔 수 없답니다 ^^
+                        _romanized += coda[s.final]
+            else:
+                _romanized += char
+
+        return _romanized
---
+++ packages/korokoro/korokoro-0.27.tar.gz/korokoro-0.27/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+  name = 'korokoro',
+  packages = ['korokoro'],
+  version = '0.27',
+  license='GNU GPLv3',
+  description = 'A Python pingpong table Korean VS Romantic',
+  author = 'seonghyeon yoon',
+  author_email = 'yoonhero06@naver.com',
+  url = 'https://github.com/yoonhero/korona',
+  keywords = ['Korean', 'Romanization', 'Transliteration'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
