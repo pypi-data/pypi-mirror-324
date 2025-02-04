da21b503ce960e79659cf21dbb5984050a83383b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:11 +0000

    Add package wbcore/1.44.3/wbcore-1.44.3.tar.gz

---
+++ packages/wbcore/wbcore-1.44.3.tar.gz/wbcore-1.44.3/wbcore/contrib/tags/viewsets/__init__.py
@@ -0,0 +1,8 @@
+from .menu import TAG_MENUITEM
+from .viewsets import (
+    TagGroupModelSerializer,
+    TagGroupRepresentationSerializer,
+    TagModelViewSet,
+    TagRepresentationViewSet,
+    TagTagGroupModelViewSet,
+)
