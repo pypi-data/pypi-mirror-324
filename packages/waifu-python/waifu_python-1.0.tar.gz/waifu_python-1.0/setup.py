ba652969ef420fa4907c6b7f2c38d7d3b958da14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:40 +0000

    Add package waifu-python/1.0/waifu_python-1.0.tar.gz

---
+++ packages/waifu-python/waifu_python-1.0.tar.gz/waifu_python-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="waifu-python",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["httpx"],
+    author="Misfit",
+    description="A project born out of boredom, designed to simplify and reduce the code related to the Waifu API.",
+    url="https://github.com/yourusername/Waifu-Python",
+    license="MIT"
+)
