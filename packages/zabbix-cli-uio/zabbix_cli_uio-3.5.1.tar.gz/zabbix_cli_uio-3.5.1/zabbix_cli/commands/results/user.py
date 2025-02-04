2a4b33ec577660c7461624aadd04dc341c2017f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:37 +0000

    Add package zabbix-cli-uio/3.5.1/zabbix_cli_uio-3.5.1.tar.gz

---
+++ packages/zabbix-cli-uio/zabbix_cli_uio-3.5.1.tar.gz/zabbix_cli_uio-3.5.1/zabbix_cli/commands/results/user.py
@@ -0,0 +1,6 @@
+"""User result types."""
+
+# NOTE: The user module was one of the first to be written, and thus
+# most of the result rendering was implemented in the Pyzabbix models
+# themselves instead of as result classes. That is why this module is
+# (mostly) empty.
