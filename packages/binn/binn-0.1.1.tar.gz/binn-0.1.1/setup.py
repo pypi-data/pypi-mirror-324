b933ac9405bf574c1ffe968750dd16aa4d03bd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:32 +0000

    Add package binn/0.1.1/binn-0.1.1.tar.gz

---
+++ packages/binn/binn-0.1.1.tar.gz/binn-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+requirements = [
+    "numpy",
+    "networkx",
+    "pandas",
+    "torch",
+    "shap",
+    "matplotlib",
+]
+
+setup(
+    author="Erik Hartman",
+    author_email="erik.hartman@hotmail.com",
+    name="binn",
+    version="0.1.1",
+    packages=find_packages(),
+    license="MIT",
+    long_description=open("README.md").read(),
+    install_requires=requirements,
+    url="https://github.com/InfectionMedicineProteomics/BINN",
+    include_package_data=True,
+)
