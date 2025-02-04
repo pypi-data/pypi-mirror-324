39147657665cd7f33d99c3d53846abbae88a2733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:06:37 +0000

    Add package weather_provider_api/2.63.58/weather_provider_api-2.63.58-py3-none-any.whl

---
+++ packages/weather_provider_api/weather_provider_api-2.63.58-py3-none-any.whl/weather_provider_api/scripts/update_era5sl_repository.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  SPDX-FileCopyrightText: 2019-2022 Alliander N.V.
+#  SPDX-License-Identifier: MPL-2.0
+
+from weather_provider_api.routers.weather.sources.cds.client.era5sl_repository import (
+    ERA5SLRepository,
+)
+
+
+def main():
+    era5sl_repo = ERA5SLRepository()
+    era5sl_repo.update()
+
+
+if __name__ == "__main__":
+    main()
