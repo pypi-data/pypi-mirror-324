2c403f68ea9ba38f829fdf72a461818cfb103b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:06:52 +0000

    Add package semantic-router/0.1.0.dev9/semantic_router-0.1.0.dev9-py3-none-any.whl

---
+++ packages/semantic-router/semantic_router-0.1.0.dev9-py3-none-any.whl/semantic_router/llms/cohere.py
@@ -0,0 +1,60 @@
+import os
+from typing import Any, List, Optional
+
+from pydantic import PrivateAttr
+
+from semantic_router.llms import BaseLLM
+from semantic_router.schema import Message
+
+
+class CohereLLM(BaseLLM):
+    _client: Any = PrivateAttr()
+
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        cohere_api_key: Optional[str] = None,
+    ):
+        if name is None:
+            name = os.getenv("COHERE_CHAT_MODEL_NAME", "command")
+        super().__init__(name=name)
+        self._client = self._initialize_client(cohere_api_key)
+
+    def _initialize_client(self, cohere_api_key: Optional[str] = None):
+        try:
+            import cohere
+        except ImportError:
+            raise ImportError(
+                "Please install Cohere to use CohereLLM. "
+                "You can install it with: "
+                "`pip install 'semantic-router[cohere]'`"
+            )
+        cohere_api_key = cohere_api_key or os.getenv("COHERE_API_KEY")
+        if cohere_api_key is None:
+            raise ValueError("Cohere API key cannot be 'None'.")
+        try:
+            client = cohere.Client(cohere_api_key)
+        except Exception as e:
+            raise ValueError(
+                f"Cohere API client failed to initialize. Error: {e}"
+            ) from e
+        return client
+
+    def __call__(self, messages: List[Message]) -> str:
+        if self._client is None:
+            raise ValueError("Cohere client is not initialized.")
+        try:
+            completion = self._client.chat(
+                model=self.name,
+                chat_history=[m.to_cohere() for m in messages[:-1]],
+                message=messages[-1].content,
+            )
+
+            output = completion.text
+
+            if not output:
+                raise Exception("No output generated")
+            return output
+
+        except Exception as e:
+            raise ValueError(f"Cohere API call failed. Error: {e}") from e
