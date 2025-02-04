ec68c119675e0d0e4563754451392139a5449c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:32 +0000

    Add package maplib/0.14.11/maplib-0.14.11.tar.gz

---
+++ packages/maplib/maplib-0.14.11.tar.gz/maplib-0.14.11/lib/triplestore/src/sparql/lazy_graph_patterns/project.rs
@@ -0,0 +1,39 @@
+use super::Triplestore;
+use crate::sparql::errors::SparqlError;
+use log::debug;
+use oxrdf::Variable;
+
+use query_processing::graph_patterns::project;
+use query_processing::pushdowns::Pushdowns;
+use representation::query_context::{Context, PathEntry};
+use representation::solution_mapping::{EagerSolutionMappings, SolutionMappings};
+use spargebra::algebra::GraphPattern;
+use std::collections::HashMap;
+
+impl Triplestore {
+    pub(crate) fn lazy_project(
+        &mut self,
+        inner: &GraphPattern,
+        variables: &Vec<Variable>,
+        solution_mappings: Option<SolutionMappings>,
+        context: &Context,
+        parameters: &Option<HashMap<String, EagerSolutionMappings>>,
+        mut pushdowns: Pushdowns,
+    ) -> Result<SolutionMappings, SparqlError> {
+        debug!("Processing project graph pattern");
+        let inner_context = context.extension_with(PathEntry::ProjectInner);
+        pushdowns.limit_to_variables(variables);
+        pushdowns.add_graph_pattern_pushdowns(inner);
+        let mut solution_mappings = self.lazy_graph_pattern(
+            inner,
+            solution_mappings,
+            &inner_context,
+            parameters,
+            pushdowns,
+        )?;
+
+        solution_mappings = project(solution_mappings, variables)?;
+
+        Ok(solution_mappings)
+    }
+}
