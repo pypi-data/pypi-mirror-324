69c351cbd2b3c62a49d47c3ae6a50952813809f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:06:52 +0000

    Add package jajucha2/1.2.1/jajucha2-1.2.1.tar.gz

---
+++ packages/jajucha2/jajucha2-1.2.1.tar.gz/jajucha2-1.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jajucha2',
+    version='1.2.1',
+    packages=find_packages(),  # Automatically finds the package directories
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
