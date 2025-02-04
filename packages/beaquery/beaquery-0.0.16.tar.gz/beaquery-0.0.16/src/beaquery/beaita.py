d55c9d8725f12d7431716bf06b5a9dce9fa7eee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:37 +0000

    Add package beaquery/0.0.16/beaquery-0.0.16.tar.gz

---
+++ packages/beaquery/beaquery-0.0.16.tar.gz/beaquery-0.0.16/src/beaquery/beaita.py
@@ -0,0 +1,61 @@
+#! env python
+#
+
+import argparse
+import os
+import sys
+import webbrowser
+
+try:
+    from beaquery import beaqueryq
+except Exception as e:
+    import beaqueryq
+
+def main():
+    argp = argparse.ArgumentParser(description='get BEA ITA data')
+
+    argp.add_argument('--dataset', default='ITA')
+    argp.add_argument('--indctr', required=True, help='ITA indicator')
+    argp.add_argument('--aoc', required=True, help='ITA area or country')
+    argp.add_argument('--freq', required=True,
+                     help='frequency M, Q, A or comma separated list')
+    argp.add_argument('--yr', required=True,
+                      help='year YYYY  X or all')
+
+    argp.add_argument('--format', default='json',
+                      choices=['json', 'XML'], help='result format')
+
+    argp.add_argument('--csvfn', \
+         help='name of file to store dataset CSV result')
+
+    argp.add_argument('--splitkey', default='TimeSeriesDescription',
+        help='table column name to use to split the table')
+    argp.add_argument('--xkey', default='Year',
+        help='table column name to use to plot the data')
+    argp.add_argument('--ykey', default='DataValue',
+        help='table column name to use to plot the data')
+    argp.add_argument('--unitskey', default='CL_UNIT',
+        help='table column name to use to label the data')
+    argp.add_argument('--htmlfn', \
+        help='name of file to store dataset HTML result')
+
+    args=argp.parse_args()
+
+    BN = beaqueryq.BEAQueryQ()
+    d = BN.getITAdata(args)
+    if d == None or type(d) == type({}) and 'Data' not in d.keys():
+        print('%s: no data' % os.path.basename(__file__), file=sys.stderr)
+    else:
+        if args.csvfn != None:
+            BN.store2csv(d, args.csvfn)
+        elif args.htmlfn != None:
+            h = BN.d2html(d, args)
+            with open(args.htmlfn, 'w') as fp:
+                print(h, file=fp)
+            webbrowser.open('file://%s' % args.htmlfn)
+        else:
+            BN.print2csv(d)
+
+
+if __name__ == '__main__':
+    main()
