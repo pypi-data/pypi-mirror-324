7115d6282228422c9a10b9a50194b1da38b842b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:59 +0000

    Add package martialLaw/0.0.1/martiallaw-0.0.1.tar.gz

---
+++ packages/martialLaw/martiallaw-0.0.1.tar.gz/martiallaw-0.0.1/README.md
@@ -0,0 +1,2 @@
+# martialLaw
+YoonSuckYall
---
+++ packages/martialLaw/martiallaw-0.0.1.tar.gz/martiallaw-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='martialLaw',
+    version='0.0.1',
+    description='[martialaw](YoonSuckYall) Joking lib that function what represented "Powerful Programming by closer Partion"',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/YoonSuckYall',
+    packages=find_packages(exclude=[]),
+    install_requires=['YoonSuckYall',],
+    keywords=['Yoon', "YoonSuckYall", "Martial", "Law"],
+    python_requires='>=3.4',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
