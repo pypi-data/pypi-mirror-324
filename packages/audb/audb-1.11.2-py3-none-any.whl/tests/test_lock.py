8b4d0ef95e36a964a5645a5bc42cf2aa340c79ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:38 +0000

    Add package audb/1.11.2/audb-1.11.2-py3-none-any.whl

---
+++ packages/audb/audb-1.11.2-py3-none-any.whl/tests/test_lock.py
@@ -0,0 +1,145 @@
+import threading
+import time
+
+import filelock
+
+import audeer
+
+from audb.core.lock import FolderLock
+
+
+event = threading.Event()
+
+
+def job(lock, wait, sleep):
+    if wait:
+        event.wait()  # wait for another thread to enter the lock
+    try:
+        with lock:
+            if not wait:
+                event.set()  # notify waiting threads to enter the lock
+            time.sleep(sleep)
+    except filelock.Timeout:
+        return 0
+    return 1
+
+
+def test_lock(tmpdir):
+    # create two lock folders
+
+    lock_folders = [audeer.mkdir(tmpdir, str(idx)) for idx in range(2)]
+
+    # lock 1 and 2
+
+    lock_1 = FolderLock(lock_folders[0])
+    lock_2 = FolderLock(lock_folders[1])
+
+    event.clear()
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, False, 0], {}),
+            ([lock_2, False, 0], {}),
+        ],
+        num_workers=2,
+    )
+    assert result == [1, 1]
+
+    # lock 1, 2 and 1+2
+
+    lock_1 = FolderLock(lock_folders[0])
+    lock_2 = FolderLock(lock_folders[1])
+    lock_12 = FolderLock(lock_folders)
+
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, False, 0], {}),
+            ([lock_2, False, 0], {}),
+            ([lock_12, False, 0], {}),
+        ],
+        num_workers=3,
+    )
+    assert result == [1, 1, 1]
+
+    # lock 1, then 1+2 + wait
+
+    lock_1 = FolderLock(lock_folders[0])
+    lock_12 = FolderLock(lock_folders)
+
+    event.clear()
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, False, 0.2], {}),
+            ([lock_12, True, 0], {}),
+        ],
+        num_workers=2,
+    )
+    assert result == [1, 1]
+
+    # lock 1, then 1+2 + timeout
+
+    lock_1 = FolderLock(lock_folders[0])
+    lock_12 = FolderLock(lock_folders, timeout=0)
+
+    event.clear()
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, False, 0.2], {}),
+            ([lock_12, True, 0], {}),
+        ],
+        num_workers=2,
+    )
+    assert result == [1, 0]
+
+    # lock 1+2, then 1 + wait
+
+    lock_1 = FolderLock(lock_folders[0])
+    lock_12 = FolderLock(lock_folders)
+
+    event.clear()
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, True, 0], {}),
+            ([lock_12, False, 0.2], {}),
+        ],
+        num_workers=2,
+    )
+    assert result == [1, 1]
+
+    # lock 1+2, then 1 + timeout
+
+    lock_1 = FolderLock(lock_folders[0], timeout=0)
+    lock_12 = FolderLock(lock_folders)
+
+    event.clear()
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, True, 0], {}),
+            ([lock_12, False, 0.2], {}),
+        ],
+        num_workers=2,
+    )
+    assert result == [0, 1]
+
+    # lock 1+2, then 1 + wait and 2 + timeout
+
+    lock_1 = FolderLock(lock_folders[0])
+    lock_2 = FolderLock(lock_folders[1], timeout=0)
+    lock_12 = FolderLock(lock_folders)
+
+    event.clear()
+    result = audeer.run_tasks(
+        job,
+        [
+            ([lock_1, True, 0], {}),
+            ([lock_2, True, 0], {}),
+            ([lock_12, 0, 0.2], {}),
+        ],
+        num_workers=3,
+    )
+    assert result == [1, 0, 1]
