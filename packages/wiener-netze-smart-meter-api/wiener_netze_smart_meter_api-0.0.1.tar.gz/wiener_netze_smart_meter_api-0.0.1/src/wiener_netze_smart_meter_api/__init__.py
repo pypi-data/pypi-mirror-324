4701b88ca826b6792deaf94561e6f47b60a0c6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:32 +0000

    Add package wiener-netze-smart-meter-api/0.0.1/wiener_netze_smart_meter_api-0.0.1.tar.gz

---
+++ packages/wiener-netze-smart-meter-api/wiener_netze_smart_meter_api-0.0.1.tar.gz/wiener_netze_smart_meter_api-0.0.1/src/wiener_netze_smart_meter_api/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025-present tschoerk <tschoerk@proton.me>
+#
+# SPDX-License-Identifier: MIT
+from .client import WNAPIClient
+
+__all__ = ["WNAPIClient"]
+
+# Optionally, you can define a package version here if you want:
+__version__ = "0.1.0"
