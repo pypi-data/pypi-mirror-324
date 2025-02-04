ed7159f9e3b08b58a12b76dadd2abcfc9ff22cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:05:38 +0000

    Add package mitol-django-google-sheets-deferrals/2025.2.3/mitol_django_google_sheets_deferrals-2025.2.3.tar.gz

---
+++ packages/mitol-django-google-sheets-deferrals/mitol_django_google_sheets_deferrals-2025.2.3.tar.gz/mitol_django_google_sheets_deferrals-2025.2.3/mitol/google_sheets_deferrals/__init__.py
@@ -0,0 +1,6 @@
+"""mitol.google_sheets_deferrals"""
+
+default_app_config = "mitol.google_sheets_deferrals.apps.GoogleSheetsDeferralsApp"
+
+__version__ = "2025.2.3"
+__distributionname__ = "mitol-django-google-sheets-deferrals"
