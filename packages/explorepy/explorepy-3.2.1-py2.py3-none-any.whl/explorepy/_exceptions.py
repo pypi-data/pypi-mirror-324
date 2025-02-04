5147969221cc750e28732a31ed0808e5337b238f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:41 +0000

    Add package explorepy/3.2.1/explorepy-3.2.1-py2.py3-none-any.whl

---
+++ packages/explorepy/explorepy-3.2.1-py2.py3-none-any.whl/explorepy/_exceptions.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+Customized exceptions module
+"""
+import sys
+
+
+class InputError(Exception):
+    """
+    User input exception
+    """
+    pass
+
+
+class DeviceNotFoundError(IOError):
+    """
+    Device not found exception
+    """
+    pass
+
+
+class UnsupportedBtHardwareError(Exception):
+    """
+    Exception for Bluetooth hardwares needing manual intervention
+    """
+    pass
+
+
+class FletcherError(Exception):
+    """
+    Fletcher value error
+    """
+    pass
+
+
+class ReconnectionFlowError(Exception):
+    """
+    Reconnection flow error, only thrown when device is reconnecting
+    """
+    pass
+
+
+class BleDisconnectionError(Exception):
+    """
+    Reconnection flow error, only thrown when device is reconnecting
+    """
+    pass
+
+
+if sys.platform == "darwin":
+    class BluetoothError(Exception):
+        """
+        mock exception class for mac OS
+        """
+        pass
