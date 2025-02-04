94eeb70f53b9c4e52eced13db852ad53f9abff60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:07:11 +0000

    Add package wbcore/1.44.3/wbcore-1.44.3-py2.py3-none-any.whl

---
+++ packages/wbcore/wbcore-1.44.3-py2.py3-none-any.whl/wbcore/contrib/tags/tests/tests.py
@@ -0,0 +1,17 @@
+import pytest
+from wbcore.test import GenerateTest, default_config
+
+config = {}
+for key, value in default_config.items():
+    config[key] = list(
+        filter(
+            lambda x: x.__module__.startswith("wbcore.contrib.tags") and x.__name__ not in ["TagTagGroupModelViewSet"],
+            value,
+        )
+    )
+
+
+@pytest.mark.django_db
+@GenerateTest(config)
+class TestProject:
+    pass
